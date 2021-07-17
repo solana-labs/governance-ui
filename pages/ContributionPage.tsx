dc511875965f849fbb0060f8432d7083cf1b111f
Steven Sarmiento <sarmiento.steven@gmail.com>
2021-07-16 17:57:02 -0700

    Add files via upload

--- pages/ContributionPage.tsx
+++ pages/ContributionPage.tsx
@@ -1,19 +1,21 @@
 import ContributionModal from '../components/ContributionModal'
-import PoolInfoCards from '../components/PoolInfoCards'
+import FooterSection from '../components/FooterSection'
 import HeroSection from '../components/HeroSection'
 import LandingContent from '../components/LandingContent'
+import NavBarBeta from '../components/NavBarBeta'
 
 const ContributionPage = () => {
   return (
     <>
+      <NavBarBeta />
       <HeroSection />
-      <PoolInfoCards />
       <LandingContent />
       <div className="flex justify-center">
         <div className="max-w-screen-md grid grid-cols-12 gap-4 w-full">
           <ContributionModal />
         </div>
       </div>
+      <FooterSection />
     </>
   )
 }
