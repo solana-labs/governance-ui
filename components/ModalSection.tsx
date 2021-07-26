90895f2413b5f0175225e2ba396104a5d177dfef
steven <sarmiento.steven@gmail.com>
2021-07-26 12:52:08 -0700

    change header margin to match upper sections

--- components/ModalSection.tsx
+++ components/ModalSection.tsx
@@ -5,11 +5,11 @@ const ModalSection = () => {
   return (
     <>
       <div className="pt-48 pb-48 px-4 bg-bg-wave bg-cover bg-bottom bg-no-repeat">
-        <div className="max-w-5xl mx-auto text-center">
-          <h2 className="mb-8 text-3xl md:text-4xl lg:text-4xl text-white font-bold font-heading">
+        <div className="max-w-5xl mx-auto text-center mb-12">
+          <h2 className="mb-4 text-3xl md:text-4xl lg:text-4xl text-white font-bold font-heading">
             Ready to contribute?
           </h2>
-          <p className="mb-8 text-xl md:text-2xl lg:text-2xl text-white text-opacity-50">
+          <p className="text-xl md:text-2xl lg:text-2xl text-white text-opacity-50">
             Join us and become a valued stakeholder in the Mango Dao.
           </p>
         </div>
