8f57b919335d0d354ad8a89c761ee755c2314f25
steven <sarmiento.steven@gmail.com>
2021-07-26 12:42:14 -0700

    minor style updates to text

--- components/PoolCountdown.tsx
+++ components/PoolCountdown.tsx
@@ -16,7 +16,7 @@ const PoolCountdown = (props: { className?: string; date: moment.Moment }) => {
     } else {
       return (
         <div className={`${props.className} font-bold text-white flex `}>
-          <ClockIcon className="w-6 h-6 mr-3" />
+          <ClockIcon className="w-7 h-7 mr-3" />
           <span className="text-xl">
             {/* <span className="bg-bkg-1 border border-bkg-4 mx-0.5 px-1.5 py-1 rounded"> */}
             {hours < 10 ? `0${hours}` : hours}
