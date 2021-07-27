7d33805e6b0ad4b9bef3b1ac02fcbac18eef2d17
Maximilian Schneider <mail@maximilianschneider.net>
2021-07-28 01:51:57 +0200

    fix timestamps for page reveal

--- hooks/usePool.tsx
+++ hooks/usePool.tsx
@@ -3,16 +3,19 @@ import useWalletStore from '../stores/useWalletStore'
 
 export default function usePool() {
   const pool = useWalletStore((s) => s.pool)
-  const startIdo = pool ? moment.unix(pool.startIdoTs.toNumber()) : undefined
 
+  const startIdo = pool ? moment.unix(pool.startIdoTs.toNumber()) : undefined
   const endIdo = pool ? moment.unix(pool.endIdoTs.toNumber()) : undefined
   const endDeposits = pool
     ? moment.unix(pool.endDepositsTs.toNumber())
     : undefined
 
-  // // override for testing
-  // const endDeposits = moment().add(1, 'days')
-  // const endIdo = moment().add(2, 'days')
+  /*
+  // override for announcement
+  const startIdo = moment.unix(1628553600)
+  const endDeposits = moment.unix(1628553600).add(1, 'days')
+  const endIdo = moment.unix(1628553600).add(2, 'days')
+  */
 
   return { pool, startIdo, endIdo, endDeposits }
 }
