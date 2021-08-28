2b2a568797c08961b8018981c098bd2b1e777b07
Sebastian.Bor <sebastian_bor@hotmail.com>
2021-08-28 15:30:14 +0200

    feat: use InstructionDescriptor to show instruction details

--- components/instructions/instructionCard.tsx
+++ components/instructions/instructionCard.tsx
@@ -1,6 +1,11 @@
 import { PublicKey } from '@solana/web3.js'
 import { AccountMetaData, ProposalInstruction } from '../../models/accounts'
-import { getAccountName, getProgramName } from './tools'
+import {
+  getAccountName,
+  getInstructionDescriptor,
+  getProgramName,
+  InstructionDescriptor,
+} from './tools'
 
 export default function InstructionCard({
   index,
@@ -9,17 +14,32 @@ export default function InstructionCard({
   index: number
   proposalInstruction: ProposalInstruction
 }) {
+  const instructionId = proposalInstruction.instruction.data[0]
+  const descriptor = getInstructionDescriptor(
+    proposalInstruction.instruction.programId,
+    instructionId
+  )
+
   return (
     <div>
-      <div>{`Instruction #${index}`}</div>
+      <div>
+        <span>{`Instruction #${index}`}</span>
+        {descriptor && <span>{` (${descriptor.name})`}</span>}
+      </div>
       <InstructionProgram
         programId={proposalInstruction.instruction.programId}
       ></InstructionProgram>
       {proposalInstruction.instruction.accounts.map((am, idx) => (
-        <InstructionAccount key={idx} index={idx} accountMeta={am} />
+        <InstructionAccount
+          key={idx}
+          index={idx}
+          accountMeta={am}
+          descriptor={descriptor}
+        />
       ))}
       <InstructionData
         data={proposalInstruction.instruction.data}
+        descriptor={descriptor}
       ></InstructionData>
     </div>
   )
@@ -29,8 +49,8 @@ export function InstructionProgram({ programId }: { programId: PublicKey }) {
   const programLabel = getProgramName(programId)
   return (
     <div>
-      <div>ProgramID:</div>
       <div>
+        <span>ProgramID:</span>
         <span> {programId.toBase58()}</span>
         {programLabel && <span>{` (${programLabel})`}</span>}
       </div>
@@ -41,26 +61,38 @@ export function InstructionProgram({ programId }: { programId: PublicKey }) {
 export function InstructionAccount({
   index,
   accountMeta,
+  descriptor,
 }: {
   index: number
   accountMeta: AccountMetaData
+  descriptor: InstructionDescriptor | undefined
 }) {
   const accountLabel = getAccountName(accountMeta.pubkey)
 
   return (
     <div>
       <span>{`Account #${index}`}</span>
+      {descriptor && <span>{` (${descriptor.accounts[index].name})`}</span>}
       <span> {accountMeta.pubkey.toBase58()}</span>
       {accountLabel && <span>{` (${accountLabel})`}</span>}
     </div>
   )
 }
 
-export function InstructionData({ data }: { data: Uint8Array }) {
+export function InstructionData({
+  data,
+  descriptor,
+}: {
+  data: Uint8Array
+  descriptor: InstructionDescriptor | undefined
+}) {
+  const getDataUI =
+    descriptor?.getDataUI ?? ((data, _accounts) => <>{JSON.stringify(data)}</>)
+
   return (
     <div>
       <span>Data:</span>
-      <span> {JSON.stringify(data)}</span>
+      <span> {getDataUI(data, descriptor.accounts)}</span>
     </div>
   )
 }
--- components/instructions/tools.tsx
+++ components/instructions/tools.tsx
@@ -18,3 +18,37 @@ export const ACCOUNT_NAMES = {
 export function getAccountName(accountPk: PublicKey) {
   return ACCOUNT_NAMES[accountPk.toBase58()]
 }
+
+export interface AccountDescriptor {
+  name: string
+  important?: boolean
+}
+
+export interface InstructionDescriptor {
+  name: string
+  accounts: AccountDescriptor[]
+  getDataUI: (data: Uint8Array) => JSX.Element
+}
+
+export const INSTRUCTION_DESCRIPTORS = {
+  TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA: {
+    3: {
+      name: 'Token: Transfer',
+      accounts: [
+        { name: 'Source', important: true },
+        { name: 'Destination', important: true },
+        { name: 'Authority' },
+      ],
+      getDataUI: (data: Uint8Array, _accounts: AccountDescriptor[]) => {
+        return <>{JSON.stringify(data)}</>
+      },
+    },
+  },
+}
+
+export function getInstructionDescriptor(
+  programId: PublicKey,
+  instructionId: number
+): InstructionDescriptor | undefined {
+  return INSTRUCTION_DESCRIPTORS[programId.toBase58()][instructionId]
+}
