29bfbb4352e7e4aee1e842e8bc3487b2d38511b2
microwavedcola1 <microwavedcola@gmail.com>
2021-09-20 17:22:38 +0200

    update voting state, e.g. in order to reflect latest state when going back to realm page
    Signed-off-by: microwavedcola1 <microwavedcola@gmail.com>

--- components/VoteCommentModal.tsx
+++ components/VoteCommentModal.tsx
@@ -35,7 +35,10 @@ const VoteCommentModal: FunctionComponent<VoteCommentModalProps> = ({
   const { proposal } = useWalletStore((s) => s.selectedProposal)
   const { fetchChatMessages } = useWalletStore((s) => s.actions)
   const { fetchVoteRecords } = useWalletStore((s) => s.actions)
-  const { realm } = useRealm()
+  const { realm, realmInfo } = useRealm()
+  const { fetchWalletTokenAccounts, fetchRealm } = useWalletStore(
+    (s) => s.actions
+  )
 
   const submitVote = async (vote: Vote) => {
     setSubmitting(true)
@@ -78,6 +81,7 @@ const VoteCommentModal: FunctionComponent<VoteCommentModalProps> = ({
 
     fetchChatMessages(proposal.pubkey)
     fetchVoteRecords(proposal)
+    await fetchRealm(realmInfo.programId, realmInfo.realmId)
   }
 
   const voteString = vote === 0 ? 'Approve' : 'Deny'
