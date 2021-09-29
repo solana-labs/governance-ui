c3985935bef55e242ec243b37b9a4417b9815868
Adrian Brzeziński <a.brzezinski94@gmail.com>
2021-09-29 14:24:11 +0200

    dao info fix and solona logo always in main nav

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
-import useRealm from '../hooks/useRealm'
-import { getResourcePathPart } from '../tools/core/resources'
 import ConnectWalletButton from './ConnectWalletButton'
 
 const NavBar = () => {
-  const { realm, realmInfo } = useRealm()
   const [showAltText, setShowAltText] = useState(false)
 
   const onLogoError = (e: any) => {
@@ -13,25 +10,21 @@ const NavBar = () => {
     setShowAltText(true)
   }
 
-  const realmName = realmInfo?.mainnetName ?? realm?.info.name ?? 'solana'
-
   return (
     <div className="bg-bkg-1 flex h-20 items-center justify-between px-6">
       <div className="flex justify-start lg:w-0 lg:flex-1">
-        <a href={realmInfo?.website ? `${realmInfo?.website}` : ''}>
-          {realmName && (
-            <>
-              <span className="sr-only">{realmName}</span>
-              <img
-                className="h-7"
-                src={`/realms/${getResourcePathPart(realmName)}/img/logo.svg`}
-                alt={realmName}
-                width="auto"
-                onError={onLogoError}
-              />
-              {showAltText && <span> {realmName}</span>}
-            </>
-          )}
+        <a href={'/'}>
+          <>
+            <span className="sr-only">{'Solana'}</span>
+            <img
+              className="h-7"
+              src="img/solanaLogo.svg"
+              alt={'Solana'}
+              width="auto"
+              onError={onLogoError}
+            />
+            {showAltText && <span>Solana</span>}
+          </>
         </a>
       </div>
       <ConnectWalletButton />
