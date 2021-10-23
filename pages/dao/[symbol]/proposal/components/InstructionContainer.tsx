d77a00c1fd60b1df4f35adab60481510e37a191b
Adrian Brzeziński <a.brzezinski94@gmail.com>
2021-10-23 11:15:20 +0200

    lint fix

--- pages/dao/[symbol]/proposal/components/InstructionContainer.tsx
+++ pages/dao/[symbol]/proposal/components/InstructionContainer.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import DryRunInstructionBtn from './DryRunInstructionBtn'
 
 const InstructionContainer = ({ children, idx, instructionsData }) => {
+  const currentInstruction = instructionsData ? instructionsData[idx] : null
   return (
     <div>
       {children}
       <div className="text-right">
         <DryRunInstructionBtn
           btnClassNames="mt-5 "
-          getInstructionDataFcn={instructionsData[idx].getInstruction}
+          getInstructionDataFcn={currentInstruction?.getInstruction}
         />
       </div>
     </div>
