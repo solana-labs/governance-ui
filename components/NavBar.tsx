d3cc16250cc2e9cc27928f5edec7d4f3246df72b
saml33 <slam.uke@gmail.com>
2021-10-24 19:49:47 +1100

    change logo

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,8 +11,8 @@ const NavBar = () => {
       <div className="col-span-12 xl:col-start-2 xl:col-span-10 flex h-20 items-center justify-between px-4 md:px-8 xl:px-4">
         <Link href={fmtUrlWithCluster('/realms')}>
           <div className="cursor-pointer flex items-center">
-            <img src="/img/logo.svg" className="h-10 mr-3" />
-            <h1 className="text-lg">Sierra</h1>
+            <img src="/img/solana-logo.svg" className="h-8 mr-3" />
+            {/* <h1 className="text-lg">Sierra</h1> */}
           </div>
         </Link>
         <ConnectWalletButton />
