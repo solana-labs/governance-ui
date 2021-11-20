90e238b78de6764aa80097e0cd10181f2695d710
Adrian Brzeziński <a.brzezinski94@gmail.com>
2021-11-20 17:15:49 +0100

    tres account compact wrapper

---
+++ components/TreasuryAccount/TreasuryAccountCompact.tsx
@@ -0,0 +1,9 @@
+const TreasuryAccountCompact = () => {
+  return (
+    <div className="bg-bkg-2 p-4 md:p-6 rounded-lg mt-5">
+      <h3 className="mb-4">Tresuary Accounts</h3>
+    </div>
+  )
+}
+
+export default TreasuryAccountCompact
