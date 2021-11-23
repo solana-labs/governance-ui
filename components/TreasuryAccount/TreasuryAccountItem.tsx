834b6943ed9efd1e02a73d64b8ac7c0c3e893e80
Adrian Brzeziński <a.brzezinski94@gmail.com>
2021-11-23 10:47:51 +0100

    set total price state

--- components/TreasuryAccount/TreasuryAccountItem.tsx
+++ components/TreasuryAccount/TreasuryAccountItem.tsx
@@ -1,77 +1,77 @@
 import { GovernanceAccountType } from '@models/accounts'
-import { getBigNumberAmount, numberWithCommas } from '@tools/sdk/units'
+import { numberWithCommas } from '@tools/sdk/units'
 import {
   getMintAccountLabelInfo,
   getTokenAccountLabelInfo,
   GovernedMultiTypeAccount,
 } from '@utils/tokens'
-import BN from 'bn.js'
 import { FC, useEffect, useState } from 'react'
 import priceService from 'utils/services/price'
 const TreasuryAccountItem: FC<{
   governedAccount: GovernedMultiTypeAccount
 }> = ({ governedAccount }) => {
   const [price, setPrice] = useState(0)
-  function returnItem(governedAccount: GovernedMultiTypeAccount) {
-    let account = ''
-    let tokenName = ''
-    let accountName = ''
-    let amount = ''
-    let supply = ''
-    const accountType = governedAccount.governance.info.accountType
-    if (accountType === GovernanceAccountType.MintGovernance) {
-      const mintLabelInfo = getMintAccountLabelInfo(governedAccount)
-      account = mintLabelInfo.account
-      tokenName = mintLabelInfo.tokenName
-      accountName = mintLabelInfo.mintAccountName
-      supply = mintLabelInfo.amount
-    } else {
-      const tokenLabelInfo = getTokenAccountLabelInfo(governedAccount)
-      account = tokenLabelInfo.tokenAccount
-      tokenName = tokenLabelInfo.tokenName
-      accountName = tokenLabelInfo.tokenAccountName
-      amount = tokenLabelInfo.amount
-    }
-    function getTotalPrice() {
-      const amountNumber = parseFloat(amount.split(',').join(''))
-      return amountNumber
-        ? numberWithCommas((amountNumber * price).toFixed(0))
-        : ''
-    }
-    useEffect(() => {
-      async function getTokenPrice() {
-        const price = await priceService.getTokenPriceToUSD(tokenName)
-        setPrice(price)
-      }
-      getTokenPrice()
-    }, [])
-    const totalPrice = getTotalPrice()
-    return account ? (
-      <div className="break-all text-fgd-1 bg-bkg-1 px-4 py-2 rounded-md w-full mb-5">
-        {accountName && <div className="mb-0.5">{accountName}</div>}
-        <div className="mb-2">{account}</div>
-        <div className="space-y-0.5 text-xs text-fgd-3">
-          {tokenName && (
-            <div className="flex items-center">
-              Token:{' '}
-              <img
-                className="flex-shrink-0 h-4 mx-1 w-4"
-                src={`/icons/${tokenName.toLowerCase()}.svg`}
-              />
-              {tokenName}
-            </div>
-          )}
-          {amount !== '' ? (
-            <div>Amount: {amount}</div>
-          ) : (
-            <div>Supply: {supply}</div>
-          )}
-          {totalPrice && `${totalPrice} $`}
-        </div>
-      </div>
-    ) : null
+  const [totalPrice, setTotalPrice] = useState('')
+  let account = ''
+  let tokenName = ''
+  let accountName = ''
+  let amount = ''
+  let supply = ''
+  const accountType = governedAccount.governance.info.accountType
+  if (accountType === GovernanceAccountType.MintGovernance) {
+    const mintLabelInfo = getMintAccountLabelInfo(governedAccount)
+    account = mintLabelInfo.account
+    tokenName = mintLabelInfo.tokenName
+    accountName = mintLabelInfo.mintAccountName
+    supply = mintLabelInfo.amount
+  } else {
+    const tokenLabelInfo = getTokenAccountLabelInfo(governedAccount)
+    account = tokenLabelInfo.tokenAccount
+    tokenName = tokenLabelInfo.tokenName
+    accountName = tokenLabelInfo.tokenAccountName
+    amount = tokenLabelInfo.amount
   }
-  return returnItem(governedAccount)
+  function handleSetTotalPrice() {
+    const amountNumber = parseFloat(amount.split(',').join(''))
+    const totalPrice = amountNumber
+      ? numberWithCommas((amountNumber * price).toFixed(0))
+      : ''
+    setTotalPrice(totalPrice)
+  }
+  useEffect(() => {
+    async function getTokenPrice() {
+      const price = await priceService.getTokenPriceToUSD(tokenName)
+      setPrice(price)
+    }
+    getTokenPrice()
+  }, [])
+  useEffect(() => {
+    handleSetTotalPrice()
+  }, [price, amount])
+  return account ? (
+    <div className="break-all text-fgd-1 bg-bkg-1 px-4 py-2 rounded-md w-full mb-5">
+      {accountName && <div className="mb-0.5">{accountName}</div>}
+      <div className="mb-2">{account}</div>
+      <div className="space-y-0.5 text-xs text-fgd-3">
+        {tokenName && (
+          <div className="flex items-center">
+            Token:{' '}
+            <img
+              className="flex-shrink-0 h-4 mx-1 w-4"
+              src={`/icons/${tokenName.toLowerCase()}.svg`}
+            />
+            {tokenName}
+          </div>
+        )}
+        {amount !== '' ? (
+          <div>Amount: {amount}</div>
+        ) : (
+          <div>Supply: {supply}</div>
+        )}
+        {totalPrice && `${totalPrice} $`}
+      </div>
+    </div>
+  ) : null
 }
 
 export default TreasuryAccountItem
--- components/TreasuryAccount/TreasuryAccountsCompactWrapper.tsx
+++ components/TreasuryAccount/TreasuryAccountsCompactWrapper.tsx
@@ -4,7 +4,7 @@ const TreasuryAccountsCompactWrapper = () => {
   return (
     <div className="bg-bkg-2 p-4 md:p-6 rounded-lg mt-5">
       <h3 className="mb-4">Treasury Accounts</h3>
-      <div className="max-h-full overflow-y-scroll">
+      <div className="max-h-full overflow-y-auto">
         <TreasuryAccountsItems></TreasuryAccountsItems>
       </div>
     </div>
