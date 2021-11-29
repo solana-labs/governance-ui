dc4e45085d21c7e8ad1d08a566e842c25a1dfde2
Adrian Brzeziński <a.brzezinski94@gmail.com>
2021-11-30 00:11:28 +0100

    webpack svg fix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const withTM = require('next-transpile-modules')(['react-markdown'])
 module.exports = withTM({
   target: 'serverless',
   webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
     if (!isServer) config.resolve.fallback.fs = false
     return config
   },
