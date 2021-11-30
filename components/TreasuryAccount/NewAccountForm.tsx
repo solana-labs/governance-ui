8191e98ed7c01abef0d8eb4f4c3a9906ec651c1f
Adrian Brzeziński <a.brzezinski94@gmail.com>
2021-11-30 23:15:40 +0100

    form fields and validation min max values inside create new account form

--- components/TreasuryAccount/NewAccountForm.tsx
+++ components/TreasuryAccount/NewAccountForm.tsx
@@ -1,5 +1,6 @@
 import Input from '@components/inputs/Input'
 import PreviousRouteBtn from '@components/PreviousRouteBtn'
+import { precision } from '@utils/formatting'
 import React, { useState } from 'react'
 
 interface NewTreasuryAccountForm {
@@ -24,6 +25,23 @@ const NewAccountForm = () => {
     setFormErrors({})
     setForm({ ...form, [propertyName]: value })
   }
+  const validateMinMax = (e) => {
+    const fieldName = e.target.name
+    const min = e.target.min || 0
+    const max = e.target.max || Number.MAX_SAFE_INTEGER
+    const value = form[fieldName]
+    //todo
+    const currentPrecision = 0 //precision(min)
+
+    handleSetForm({
+      value: parseFloat(
+        Math.max(Number(min), Math.min(Number(max), Number(value))).toFixed(
+          currentPrecision
+        )
+      ),
+      propertyName: fieldName,
+    })
+  }
   return (
     <div className="space-y-3">
       <PreviousRouteBtn />
@@ -43,6 +61,68 @@ const NewAccountForm = () => {
           })
         }
         error={formErrors['mintAddress']}
+      />
+      <Input
+        label="Min community tokens to create proposal"
+        value={form.minCommunityTokensToCreateProposal}
+        type="number"
+        name="minCommunityTokensToCreateProposal"
+        min={0.001}
+        max={10000}
+        onBlur={validateMinMax}
+        onChange={(evt) =>
+          handleSetForm({
+            value: evt.target.value,
+            propertyName: 'minCommunityTokensToCreateProposal',
+          })
+        }
+        error={formErrors['minCommunityTokensToCreateProposal']}
+      />
+      <Input
+        label="min instruction hold up time (days)"
+        value={form.minInstructionHoldUpTime}
+        type="number"
+        min={0}
+        name="minInstructionHoldUpTime"
+        onBlur={validateMinMax}
+        onChange={(evt) =>
+          handleSetForm({
+            value: evt.target.value,
+            propertyName: 'minInstructionHoldUpTime',
+          })
+        }
+        error={formErrors['minInstructionHoldUpTime']}
+      />
+      <Input
+        label="Max voting time (days)"
+        value={form.maxVotingTime}
+        name="maxVotingTime"
+        type="number"
+        min={1}
+        onBlur={validateMinMax}
+        onChange={(evt) =>
+          handleSetForm({
+            value: evt.target.value,
+            propertyName: 'maxVotingTime',
+          })
+        }
+        error={formErrors['maxVotingTime']}
+      />
+      <Input
+        label="Yes vote threshold (%)"
+        value={form.voteThreshold}
+        max={100}
+        min={1}
+        name="voteThreshold"
+        type="number"
+        onBlur={validateMinMax}
+        onChange={(evt) =>
+          handleSetForm({
+            value: evt.target.value,
+            propertyName: 'voteThreshold',
+          })
+        }
+        error={formErrors['voteThreshold']}
       />
     </div>
   )
