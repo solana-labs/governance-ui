06c8a5777ae5cc7fc55fce96807b98d7266fa5d3
Adrian Brzeziński <a.brzezinski94@gmail.com>
2021-11-30 10:20:57 +0100

    treasuary account add btn

--- components/TreasuryAccount/AccountsCompactWrapper.tsx
+++ components/TreasuryAccount/AccountsCompactWrapper.tsx
@@ -1,19 +1,21 @@
 import AccountsItems from './AccountsItems'
 import HoldTokensTotalPrice from './HoldTokensTotalPrice'
-import useGovernanceAssets from '@hooks/useGovernanceAssets'
 import { ViewState } from './Types'
 import useTreasuryAccountStore from 'stores/useTreasuryAccountStore'
 import useRealm from '@hooks/useRealm'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import AccountOverview from './AccountOverview'
 import SendTokens from './SendTokens'
 import DepositTokens from './DepositTokens'
+import Link from 'next/link'
+import { PlusIcon } from '@heroicons/react/solid'
+import useQueryContext from '@hooks/useQueryContext'
 
 const AccountsCompactWrapper = () => {
-  const { governedTokenAccounts } = useGovernanceAssets()
   const currentView = useTreasuryAccountStore((s) => s.compact.currentView)
   const { resetCompactViewState } = useTreasuryAccountStore()
   const { symbol } = useRealm()
+  const { fmtUrlWithCluster } = useQueryContext()
   const getCurrentView = () => {
     switch (currentView) {
       case ViewState.MainView:
@@ -23,6 +25,13 @@ const AccountsCompactWrapper = () => {
             <HoldTokensTotalPrice />
             <div className="max-h-full overflow-y-auto">
               <AccountsItems />
+              <Link href={fmtUrlWithCluster(`/treasury/new`)}>
+                <div className="bg-bkg-2 p-3 cursor-pointer default-transition flex flex-col items-center justify-center rounded-lg hover:bg-bkg-3 mt-3">
+                  <div className="bg-[rgba(255,255,255,0.06)] h-8 w-8 flex font-bold items-center justify-center rounded-full text-fgd-3">
+                    <PlusIcon />
+                  </div>
+                </div>
+              </Link>
             </div>
           </>
         )
@@ -37,11 +46,11 @@ const AccountsCompactWrapper = () => {
   useEffect(() => {
     resetCompactViewState()
   }, [symbol])
-  return governedTokenAccounts.length ? (
+  return (
     <div className="bg-bkg-2 p-4 md:p-6 rounded-lg mt-5">
       {getCurrentView()}
     </div>
-  ) : null
+  )
 }
 
 export default AccountsCompactWrapper
