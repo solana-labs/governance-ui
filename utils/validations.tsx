6beb894c8b577169cf7c6918b3e3769764c0605a
Adrian Brzeziński <a.brzezinski94@gmail.com>
2021-12-08 20:48:34 +0100

    token and mint validation refactor

--- utils/validations.tsx
+++ utils/validations.tsx
@@ -7,51 +7,96 @@ import { tryGetTokenAccount } from './tokens'
 import * as yup from 'yup'
 import { getMintNaturalAmountFromDecimal } from '@tools/sdk/units'
 import { BN } from '@project-serum/anchor'
+import { ConnectionContext } from 'stores/useWalletStore'
+
+const getValidateAccount = async (connection, pubKey: PublicKey) => {
+  const account = await connection.current.getParsedAccountInfo(pubKey)
+  if (!account || !account.value) {
+    throw 'Account not found'
+  }
+  return account
+}
+
+const getValidatePublicKey = (val: string) => {
+  const pubKey = tryParseKey(val)
+  if (pubKey) {
+    return pubKey
+  } else {
+    throw 'Provided value is not a public key'
+  }
+}
+
+const isValidSplTokenAccount = (account) => {
+  if (
+    !(
+      'parsed' in account.value.data &&
+      account.value.data.program === 'spl-token'
+    )
+  ) {
+    throw 'Invalid spl token account'
+  }
+  return true
+}
+
+const getValidateTokenAccount = (account) => {
+  try {
+    const tokenAccount = create(
+      account.value.data.parsed.info,
+      TokenAccountInfo
+    )
+    return tokenAccount
+  } catch {
+    throw 'Invalid spl token account'
+  }
+}
+
+const validateIsTokenAccSameMintAsGovernedAcc = async (
+  connection,
+  tokenAccount: TokenAccountInfo,
+  governedAccount: PublicKey
+) => {
+  const sourceAccMint = await tryGetTokenAccount(
+    connection.current,
+    governedAccount
+  )
+  if (
+    sourceAccMint &&
+    tokenAccount.mint.toBase58() !== sourceAccMint.account.mint.toBase58()
+  ) {
+    throw "Account mint doesn't match source account"
+  } else {
+    throw 'Invalid source account'
+  }
+}
+
+const validateDoseTokenAccountMatchMint = (
+  tokenAccount: TokenAccountInfo,
+  mint: PublicKey
+) => {
+  if (tokenAccount.mint.toBase58() !== mint.toBase58()) {
+    throw "Account mint doesn't match source account"
+  }
+}
 
 export const validateDestinationAccAddress = async (
-  connection,
+  connection: ConnectionContext,
   val: any,
   governedAccount?: PublicKey
 ) => {
-  const pubKey = tryParseKey(val)
-  if (pubKey) {
-    const account = await connection.current.getParsedAccountInfo(pubKey)
-    if (!account || !account.value) {
-      throw 'Account not found'
-    }
-    if (
-      !(
-        'parsed' in account.value.data &&
-        account.value.data.program === 'spl-token'
-      )
-    ) {
-      throw 'Invalid spl token account'
-    }
-
-    let tokenAccount
-
-    try {
-      tokenAccount = create(account.value.data.parsed.info, TokenAccountInfo)
-    } catch {
-      throw 'Invalid spl token account'
-    }
-
-    if (governedAccount) {
-      const sourceAccMint = await tryGetTokenAccount(
-        connection.current,
-        governedAccount
-      )
-      if (
-        tokenAccount.mint.toBase58() !== sourceAccMint?.account.mint.toBase58()
-      ) {
-        throw "Account mint doesn't match source account"
-      }
-    } else {
-      throw 'Source account not provided'
-    }
+  const pubKey = getValidatePublicKey(val)
+  const account = await getValidateAccount(connection, pubKey)
+  isValidSplTokenAccount(account)
+  const tokenAccount = getValidateTokenAccount(account)
+  if (governedAccount) {
+    validateIsTokenAccSameMintAsGovernedAcc(
+      connection,
+      tokenAccount,
+      governedAccount
+    )
   } else {
-    throw 'Provided value is not a valid account address'
+    throw 'Source account not provided'
   }
+  return true
 }
 
 export const validateDestinationAccAddressWithMint = async (
@@ -59,39 +104,16 @@ export const validateDestinationAccAddressWithMint = async (
   val: any,
   mintPubKey: PublicKey
 ) => {
-  const pubKey = tryParseKey(val)
-  if (pubKey) {
-    const account = await connection.current.getParsedAccountInfo(pubKey)
-    if (!account || !account.value) {
-      throw 'Account not found'
-    }
-    if (
-      !(
-        'parsed' in account.value.data &&
-        account.value.data.program === 'spl-token'
-      )
-    ) {
-      throw 'Invalid spl token account'
-    }
-
-    let tokenAccount
-
-    try {
-      tokenAccount = create(account.value.data.parsed.info, TokenAccountInfo)
-    } catch {
-      throw 'Invalid spl token account'
-    }
-
-    if (mintPubKey) {
-      if (tokenAccount.mint.toBase58() !== mintPubKey.toBase58()) {
-        throw "Account mint doesn't match source account"
-      }
-    } else {
-      throw 'Source account not provided'
-    }
+  const pubKey = getValidatePublicKey(val)
+  const account = await getValidateAccount(connection, pubKey)
+  isValidSplTokenAccount(account)
+  const tokenAccount = getValidateTokenAccount(account)
+  if (mintPubKey) {
+    validateDoseTokenAccountMatchMint(tokenAccount, mintPubKey)
   } else {
-    throw 'Provided value is not a valid account address'
+    throw 'Source account not provided'
   }
+  return true
 }
 
 export const validateBuffer = async (
