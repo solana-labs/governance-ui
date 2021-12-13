26b520be23e9e8a7430c9f86d4547f11107ef093
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-12-13 09:15:52 -0300

    chore: wip

--- models/serialisation.ts
+++ models/serialisation.ts
@@ -49,7 +49,6 @@ import { ProgramVersion } from './registry/api'
 
 // Temp. workaround to support u16.
 import { BorshAccountParser } from './core/serialisation'
-
 ;(BinaryReader.prototype as any).readU16 = function () {
   const reader = (this as unknown) as BinaryReader
   const value = reader.buf.readUInt16LE(reader.offset)
