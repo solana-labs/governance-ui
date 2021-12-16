88c5f008170ea56cdaff35498efd548c7472d6d8
John Rees <john@bitsushi.com>
2021-12-16 23:48:37 +0000

    update jest, rename .babelrc to babel.config.js
    renaming babel config is required for jest to be able to transform es6 libs (like solana/wallet-adapter-phantom) https://stackoverflow.com/a/54656593

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: [
+    [
+      'next/babel',
+      {
+        'preset-react': {
+          runtime: 'automatic',
+          importSource: '@emotion/react',
+        },
+      },
+    ],
+  ],
+  plugins: ['@emotion/babel-plugin', 'babel-plugin-macros'],
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+      ],
+    },
+  },
+}
