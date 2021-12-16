4b59f2f5656c92ffc7327a2e8732e78d31e00c93
John Rees <john@bitsushi.com>
2021-12-16 23:50:17 +0000

    test: update jest to handle tsconfig baseUrl/paths settings

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,25 @@
+const { resolve } = require('path')
+const { readdirSync } = require('fs')
+
+// XXX: hack to deal with tsconfig.baseUrl: "." and tsconfig.paths
+const directories = readdirSync(__dirname, { withFileTypes: true })
+  .filter((dirent) => dirent.isDirectory())
+  .map(({ name }) => name)
+  .filter((dir) => !dir.startsWith('.') && dir !== 'node_modules')
+  .reduce(
+    (acc, dir) => ({
+      ...acc,
+      [`^${dir}/(.*)$`]: resolve(__dirname, `./${dir}/$1`),
+      [`^@${dir}/(.*)$`]: resolve(__dirname, `./${dir}/$1`),
+    }),
+    {}
+  )
+
 module.exports = {
   roots: ['<rootDir>'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
-  testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
-  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+  testPathIgnorePatterns: ['<rootDir>/(node_modules|.next)/'],
+  transformIgnorePatterns: ['/node_modules/.+\\.(ts|tsx)$'],
   transform: {
     '^.+\\.(ts|tsx)$': 'babel-jest',
   },
@@ -13,5 +30,6 @@ module.exports = {
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+    ...directories,
   },
 }
