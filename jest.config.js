1798546152db2a2558570bae3f08aaf0569f6c1b
John Rees <john@bitsushi.com>
2021-12-18 14:37:16 +0000

    test: simplify jest/babel setup
    was able to be simplified after #149 was merged

--- jest.config.js
+++ jest.config.js
@@ -17,7 +17,6 @@ const directories = readdirSync(__dirname, { withFileTypes: true })
 
 module.exports = {
   roots: ['<rootDir>'],
-  testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
   testPathIgnorePatterns: ['<rootDir>/(node_modules|.next)/'],
   transformIgnorePatterns: ['/node_modules/.+\\.(ts|tsx)$'],
--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from 'react'
 import Home from '../../pages/index'
 import { render } from '@testing-library/react'
