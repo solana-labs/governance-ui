242c66f8443986d41f529cc81fa838487cfb90db
John Rees <john@bitsushi.com>
2021-12-28 23:45:08 +0000

    ci: add action to deploy docs to github pages

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,15 +6,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'SPL Governance UI',
+  url: 'https://solana-labs.github.io/governance-ui',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'solana-labs', // Usually your GitHub org/user name.
+  projectName: 'governance-ui', // Usually your repo name.
 
   presets: [
     [
