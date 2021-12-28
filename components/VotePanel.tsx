4b57e3a6047bccc9310b74ecb0304b930911c5c9
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-12-28 15:33:00 -0300

    chore: fix conditions to show vote panel

--- components/VotePanel.tsx
+++ components/VotePanel.tsx
@@ -140,7 +140,9 @@ const VotePanel = () => {
       {ProposalState.Cancelled === proposal?.info.state ||
       ProposalState.Succeeded === proposal?.info.state ||
       ProposalState.Draft === proposal?.info.state ||
-      (!isVoting && !isVoteCast) ? null : (
+      (!isVoting && !isVoteCast) ||
+      (connected && isVoteCast && !isWithdrawEnabled) ||
+      !connected ? null : (
         <div className="bg-bkg-2 p-4 md:p-6 rounded-lg space-y-6">
           <h2 className="mb-4 text-center">{actionLabel}</h2>
 
