2af504f4d0dd6494c28b2852127ddf705851b99f
André Mury <mury_gh@hotmail.com>
2021-12-29 15:26:40 -0300

    wip: council step bespoke realm

--- components/RealmWizard/components/Steps/BespokeCouncil.tsx
+++ components/RealmWizard/components/Steps/BespokeCouncil.tsx
@@ -1,13 +1,36 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react'
+import React, { useState } from 'react'
 import { RealmWizardStepComponentProps } from '@components/RealmWizard/interfaces/Realm'
 import Input from '@components/inputs/Input'
-
+import Switch from '@components/Switch'
+import { StyledLabel } from '@components/inputs/styles'
+import TeamWalletField from '../TeamWalletField'
 const BespokeCouncil: React.FC<RealmWizardStepComponentProps> = ({
   setForm,
   form,
   formErrors,
 }) => {
+  const [councilMintSwitch, setCouncilMintSwitch] = useState(false)
+  const handleInsertTeamWallet = (wallets: string[]) => {
+    let teamWallets: string[] = []
+    if (form?.teamWallets) {
+      teamWallets = form.teamWallets
+    }
+    wallets.forEach((wallet) => {
+      if (!teamWallets.find((addr) => addr === wallet)) {
+        teamWallets.push(wallet)
+        setForm({ teamWallets })
+      }
+    })
+  }
+
+  const handleRemoveTeamWallet = (index: number) => {
+    if (form?.teamWallets && form.teamWallets[index]) {
+      const teamWallets = form.teamWallets
+      teamWallets.splice(index, 1)
+      setForm({ teamWallets })
+    }
+  }
   return (
     <>
       <div className="border-b border-fgd-4 pb-4 pt-2">
@@ -16,14 +39,32 @@ const BespokeCouncil: React.FC<RealmWizardStepComponentProps> = ({
         </div>
       </div>
       <div className="pb-4 pr-10 mr-2">
-        <Input
-          label="Council Mint Id"
-          placeholder="(Optional) Council mint"
-          value={form?.councilMintId}
-          type="text"
-          error={formErrors['councilMintId'] || formErrors['councilMint']}
-          onChange={(evt) => setForm({ councilMintId: evt.target.value })}
-        />
+        <div className="flex justify-left items-center">
+          <Switch
+            className="mt-2 mb-2"
+            checked={councilMintSwitch}
+            onChange={() => setCouncilMintSwitch(!councilMintSwitch)}
+          />
+          <StyledLabel className="mt-1.5 ml-3">Use Council</StyledLabel>
+        </div>
+        {councilMintSwitch && (
+          <>
+            <Input
+              label="Council Mint Id"
+              placeholder="(Optional) Council mint"
+              value={form?.councilMintId}
+              type="text"
+              error={formErrors['councilMintId'] || formErrors['councilMint']}
+              onChange={(evt) => setForm({ councilMintId: evt.target.value })}
+            />
+
+            <TeamWalletField
+              onInsert={handleInsertTeamWallet}
+              onRemove={handleRemoveTeamWallet}
+              wallets={form.teamWallets}
+            />
+          </>
+        )}
       </div>
     </>
   )
