bda929b5b1113fdf12aa4d0192064749b0d51374
André Mury <mury_gh@hotmail.com>
2021-12-29 18:43:14 -0300

    feat: bespoke realm with two steps

--- actions/registerRealm.ts
+++ actions/registerRealm.ts
@@ -1,4 +1,5 @@
 import {
+  Connection,
   Keypair,
   PublicKey,
   Transaction,
@@ -21,25 +22,29 @@ import { withCreateAssociatedTokenAccount } from '@tools/sdk/splToken/withCreate
 import { withMintTo } from '@tools/sdk/splToken/withMintTo'
 import { chunks } from '@utils/helpers'
 import { WalletConnectionError } from '@solana/wallet-adapter-base'
-export async function registerRealm(
-  { connection, wallet, walletPubkey }: RpcContext,
-  programId: PublicKey,
-  programVersion: ProgramVersion,
-  name: string,
-  communityMint: PublicKey,
-  councilMint: PublicKey | undefined,
-  communityMintMaxVoteWeightSource: MintMaxVoteWeightSource,
-  minCommunityTokensToCreateGovernance: BN,
+import { withDepositGoverningTokens } from '@models/withDepositGoverningTokens'
+
+/**
+ * Prepares the council instructions
+ * @param connection
+ * @param walletPubkey
+ * @param councilMint
+ * @param councilWalletPks
+ */
+async function prepareCouncilInstructions(
+  connection: Connection,
+  walletPubkey: PublicKey,
+  councilMint?: PublicKey,
   councilWalletPks?: PublicKey[]
-): Promise<PublicKey> {
-  const instructions: TransactionInstruction[] = []
+) {
+  let councilMintPk: PublicKey | undefined = undefined
+  let walletAtaPk: PublicKey | undefined
   const councilMintInstructions: TransactionInstruction[] = []
   const councilMintSigners: Keypair[] = []
 
   // If the array of council wallets is not empty
   // then should create mints to the council
-  if (councilWalletPks) {
-    let councilMintPk: PublicKey
+  if (councilWalletPks && councilWalletPks.length) {
     // If councilMint is undefined, then
     // should create the council mint
     councilMintPk =
@@ -53,8 +58,6 @@ export async function registerRealm(
         0,
         walletPubkey
       ))
-
-    let walletAtaPk: PublicKey | undefined
 
     for (const teamWalletPk of councilWalletPks) {
       const ataPk = await withCreateAssociatedTokenAccount(
@@ -79,38 +82,95 @@ export async function registerRealm(
     }
   }
 
+  const councilMembersChunks = chunks(councilMintInstructions, 10)
+  // only walletPk needs to sign the minting instructions and it's a signer by default and we don't have to include any more signers
+  const councilSignersChunks = Array(councilMembersChunks.length).fill(
+    councilMintSigners
+  )
+
+  return {
+    councilMintPk,
+    walletAtaPk,
+    councilMembersChunks,
+    councilSignersChunks,
+  }
+}
+
+export async function registerRealm(
+  { connection, wallet, walletPubkey }: RpcContext,
+  programId: PublicKey,
+  programVersion: ProgramVersion,
+  name: string,
+  communityMint: PublicKey,
+  councilMint: PublicKey | undefined,
+  communityMintMaxVoteWeightSource: MintMaxVoteWeightSource,
+  minCommunityTokensToCreateGovernance: BN,
+  councilWalletPks?: PublicKey[]
+): Promise<PublicKey> {
+  const realmInstructions: TransactionInstruction[] = []
+  let tokenOwnerRecordPk: PublicKey | undefined = undefined
+
+  const {
+    councilMintPk,
+    walletAtaPk,
+    councilMembersChunks,
+    councilSignersChunks,
+  } = await prepareCouncilInstructions(
+    connection,
+    walletPubkey,
+    councilMint,
+    councilWalletPks
+  )
+
   const realmAddress = await withCreateRealm(
-    instructions,
+    realmInstructions,
     programId,
     programVersion,
     name,
     walletPubkey,
     communityMint,
     walletPubkey,
-    councilMint,
+    councilMintPk,
     communityMintMaxVoteWeightSource,
     minCommunityTokensToCreateGovernance,
     undefined
   )
 
-  const transaction = new Transaction()
-  transaction.add(...instructions)
+  // If the current wallet is in the team then deposit the council token
+  if (councilMintPk)
+    if (walletAtaPk) {
+      tokenOwnerRecordPk = await withDepositGoverningTokens(
+        realmInstructions,
+        programId,
+        realmAddress,
+        walletAtaPk,
+        councilMintPk,
+        walletPubkey,
+        walletPubkey,
+        walletPubkey
+      )
+    } else {
+      // Let's throw for now if the current wallet isn't in the team
+      // TODO: To fix it we would have to make it temp. as part of the team and then remove after the realm is created
+      throw new Error('Current wallet must be in the team')
+    }
 
-  const councilMembersChunks = chunks(councilMintInstructions, 10)
-  // only walletPk needs to sign the minting instructions and it's a signer by default and we don't have to include any more signers
-  const councilMembersSignersChunks = Array(councilMembersChunks.length).fill(
-    []
-  )
+  const transaction = new Transaction()
+  transaction.add(...realmInstructions)
+
   if (!wallet) throw WalletConnectionError
 
-  await sendTransactions(
-    connection,
-    wallet,
-    [...councilMembersChunks, instructions],
-    [...councilMembersSignersChunks, []],
-    SequenceType.Sequential
-  )
+  if (councilMembersChunks.length) {
+    await sendTransactions(
+      connection,
+      wallet,
+      [...councilMembersChunks, realmInstructions],
+      [...councilSignersChunks, []],
+      SequenceType.Sequential
+    )
+  } else {
+    await sendTransaction({ transaction, wallet, connection })
+  }
 
-  // await sendTransaction({ transaction, wallet, connection })
   return realmAddress
 }
