52f3a1cce7273e2cf26dd5477fb62e63a75295ef
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-12-31 15:22:45 -0300

    chore: add loading in account overview

--- components/TreasuryAccount/AccountOverview.tsx
+++ components/TreasuryAccount/AccountOverview.tsx
@@ -1,6 +1,7 @@
 import Button from '@components/Button'
 import { getExplorerUrl } from '@components/explorer/tools'
 import { getAccountName } from '@components/instructions/tools'
+import Loading from '@components/Loading'
 import Modal from '@components/Modal'
 import { ArrowLeftIcon } from '@heroicons/react/solid'
 import useGovernanceAssets from '@hooks/useGovernanceAssets'
@@ -11,7 +12,6 @@ import React, { useState } from 'react'
 import useTreasuryAccountStore from 'stores/useTreasuryAccountStore'
 import useWalletStore from 'stores/useWalletStore'
 import AccountLabel from './AccountHeader'
-import SendTokens from './SendTokens'
 import TreasuryPaymentForm from './TreasuryPaymentForm'
 import { ViewState } from './Types'
 
@@ -24,10 +24,10 @@ const AccountOverview = () => {
   const recentActivity = useTreasuryAccountStore(
     (s) => s.compact.recentActivity
   )
-
   const [openTreasuryPaymentModal, setOpenTreasuryPaymentModal] = useState(
     false
   )
+
   const {
     setCurrentCompactView,
     resetCompactViewState,
@@ -42,27 +42,41 @@ const AccountOverview = () => {
   }
   return (
     <>
-      <h3 className="mb-4 flex items-center">
-        <>
-          <ArrowLeftIcon
-            onClick={handleGoBackToMainView}
-            className="h-4 w-4 text-primary-light mr-2 hover:cursor-pointer"
-          />
-          {currentAccount?.token?.publicKey &&
-          getAccountName(currentAccount?.token?.publicKey) ? (
-            <div className="text-sm text-th-fgd-1">
-              {getAccountName(currentAccount.token?.publicKey)}
-            </div>
-          ) : (
-            <div className="text-xs text-th-fgd-1">
-              {abbreviateAddress(accountPublicKey as PublicKey)}
-            </div>
-          )}
-        </>
+      <h3 className="mb-8 flex items-center">
+        <ArrowLeftIcon
+          onClick={handleGoBackToMainView}
+          className="h-4 w-4 text-primary-light mr-2 hover:cursor-pointer"
+        />
+
+        {currentAccount?.token?.publicKey &&
+        getAccountName(currentAccount?.token?.publicKey) ? (
+          <p className="text-sm text-th-fgd-1">
+            {getAccountName(currentAccount.token?.publicKey)}
+          </p>
+        ) : (
+          <p className="text-xs text-th-fgd-1">
+            {abbreviateAddress(accountPublicKey as PublicKey)}
+          </p>
+        )}
       </h3>
-      <AccountLabel></AccountLabel>
+
+      <AccountLabel />
+
+      {openTreasuryPaymentModal && (
+        <Modal
+          background="bg-bkg-1"
+          sizeClassName="sm:max-w-3xl"
+          onClose={() => setOpenTreasuryPaymentModal(false)}
+          isOpen={openTreasuryPaymentModal}
+        >
+          <TreasuryPaymentForm
+            close={() => setOpenTreasuryPaymentModal(false)}
+          />
+        </Modal>
+      )}
+
       <div
-        className={`flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0 mb-4 ${
+        className={`flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0 mt-4 mb-8 ${
           !canUseTransferInstruction ? 'justify-center' : ''
         }`}
       >
@@ -86,44 +100,44 @@ const AccountOverview = () => {
           Send
         </Button>
       </div>
-      <div className="font-normal mr-1 text-xs text-fgd-3 mb-4">
-        Recent activity
-      </div>
-      <div>
-        {recentActivity.map((activity) => (
-          <a
-            href={
-              activity.signature
-                ? getExplorerUrl(connection.endpoint, activity.signature, 'tx')
-                : ''
-            }
-            target="_blank"
-            rel="noopener noreferrer"
-            className="border border-fgd-4 default-transition rounded-lg hover:bg-bkg-3 css-1ug690d-StyledCardWrapepr elzt7lo0 p-4 text-xs text-th-fgd-1 mb-2 flex"
-            key={activity.signature}
-          >
-            <div>{activity.signature.substring(0, 12)}...</div>
-            <div className="ml-auto text-fgd-3 text-xs flex flex-col">
-              {activity.blockTime
-                ? fmtUnixTime(new BN(activity.blockTime))
-                : null}
-            </div>
-          </a>
-        ))}
-      </div>
 
-      {openTreasuryPaymentModal && (
-        <Modal
-          background="bg-bkg-1 md:mt-0 mt-8"
-          sizeClassName="sm:max-w-3xl"
-          onClose={() => setOpenTreasuryPaymentModal(false)}
-          isOpen={openTreasuryPaymentModal}
-        >
-          <TreasuryPaymentForm
-            close={() => setOpenTreasuryPaymentModal(false)}
-          />
-        </Modal>
-      )}
+      <p className="font-normal mr-1 text-xs text-fgd-3 mb-4">
+        Recent activity
+      </p>
+
+      <div>
+        {recentActivity.length > 0 ? (
+          recentActivity.map((activity) => (
+            <a
+              href={
+                activity.signature
+                  ? getExplorerUrl(
+                      connection.endpoint,
+                      activity.signature,
+                      'tx'
+                    )
+                  : ''
+              }
+              target="_blank"
+              rel="noopener noreferrer"
+              className="border border-fgd-4 default-transition rounded-lg hover:bg-bkg-3 css-1ug690d-StyledCardWrapepr elzt7lo0 p-4 text-xs text-th-fgd-1 mb-2 flex"
+              key={activity.signature}
+            >
+              <p className="text-xs">{activity.signature.substring(0, 8)}...</p>
+
+              <p className="ml-auto text-fgd-3 text-xs flex flex-col">
+                {activity.blockTime
+                  ? fmtUnixTime(new BN(activity.blockTime))
+                  : null}
+              </p>
+            </a>
+          ))
+        ) : (
+          <div className="mx-auto mt-4">
+            <Loading />
+          </div>
+        )}
+      </div>
     </>
   )
 }
