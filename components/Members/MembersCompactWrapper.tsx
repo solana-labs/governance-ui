5b54205876b7799daf29362daa348f7b86d22893
Amanda Gonsalves <amandagonsalves75@gmail.com>
2021-12-31 14:57:04 -0300

    chore: remove unused code

--- components/Members/MembersCompactWrapper.tsx
+++ components/Members/MembersCompactWrapper.tsx
@@ -21,7 +21,11 @@ const MembersCompactWrapper = () => {
   } = useRealm()
   const { members, activeMembers } = useMembers()
   const connected = useWalletStore((s) => s.connected)
+<<<<<<< HEAD
   const activeMembersCount = activeMembers.length
+=======
+  const membersCount = members.length
+>>>>>>> e0316c0 (chore: remove unused code)
   const { resetCompactViewState } = useMembersListStore()
   const {
     canUseMintInstruction,
@@ -83,8 +87,14 @@ const MembersCompactWrapper = () => {
 
               <h3 className="mb-0">{totalVotesCast}</h3>
             </div>
+<<<<<<< HEAD
             <div style={{ maxHeight: '350px' }} className="w-full md:max-w-xs">
               <MembersItems activeMembers={activeMembers} />
+=======
+
+            <div className="max-w-xs">
+              <MembersItems />
+>>>>>>> e0316c0 (chore: remove unused code)
             </div>
 
             {openAddMemberModal && (
