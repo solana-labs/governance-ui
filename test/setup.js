7660c4bf34d97137721c37b18b1f4682a9bae4df
John Rees <john@bitsushi.com>
2022-01-04 00:54:07 +0000

    test: remove vcr tests and fixtures

--- test/setup.js
+++ test/setup.js
@@ -1,12 +1,6 @@
 import '@testing-library/jest-dom/extend-expect'
-import fetch from 'fetch-vcr'
-import { join } from 'path'
+import fetch from 'node-fetch'
 
 jest.mock('next/router', () => require('next-router-mock'))
-
-fetch.configure({
-  fixturePath: join(__dirname, '__vcrs__'),
-  // mode: 'record
-})
 
 global.fetch = fetch
