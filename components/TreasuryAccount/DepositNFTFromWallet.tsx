8ad0f234ac1210893c6982b2755c7f96a8acec1f
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-01-05 14:36:55 +0100

    file name fix

--- components/TreasuryAccount/DepositNFTFromWallet.tsx
+++ components/TreasuryAccount/DepositNFTFromWallet.tsx
@@ -16,7 +16,7 @@ import { PublicKey } from '@solana/web3.js'
 import { createATA } from '@utils/ataTools'
 import { getTokenAccountsByMint } from '@utils/tokens'
 import { sendTransaction } from '@utils/send'
-import NFTSelector, { NftSelectorFunctions } from '@components/Nft/NftSelector'
+import NFTSelector, { NftSelectorFunctions } from '@components/NFT/NFTSelector'
 
 const DepositNFTFromWallet = () => {
   const nftRef = useRef<NftSelectorFunctions>(null)
