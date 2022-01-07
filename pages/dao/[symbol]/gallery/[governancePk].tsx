9febb5674e3146cca56fdcae70b10daff206bdfc
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-01-07 13:44:52 +0100

    gallery tile

--- pages/dao/[symbol]/gallery/[governancePk].tsx
+++ pages/dao/[symbol]/gallery/[governancePk].tsx
@@ -57,8 +57,9 @@ const gallery = () => {
   return (
     <div className="grid grid-cols-12">
       <div className="bg-bkg-2 rounded-lg p-4 md:p-6 col-span-12 space-y-3">
-        <div className="flex flex-row">
+        <div className="flex flex-row items-center">
           <PreviousRouteBtn />
+          <h1 className="ml-3">Collectables</h1>
           <div className="ml-auto">
             <Select
               className="w-44 border-0"
