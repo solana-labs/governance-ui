6dad31280e1c7645b489c0db1620903894f730b4
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-01-22 12:22:27 +0000

    Update chat sdk to v2 (#272)

--- stores/useWalletStore.tsx
+++ stores/useWalletStore.tsx
@@ -18,6 +18,7 @@ import {
   getGovernanceAccounts,
   Governance,
   GovernanceAccountType,
+  GOVERNANCE_CHAT_PROGRAM_ID,
   Proposal,
   ProposalInstruction,
   Realm,
@@ -429,7 +430,11 @@ const useWalletStore = create<WalletStore>((set, get) => ({
         getGovernanceAccounts(connection, programId, SignatoryRecord, [
           pubkeyFilter(1, proposalPubKey)!,
         ]),
-        getGovernanceChatMessages(connection, proposalPubKey),
+        getGovernanceChatMessages(
+          connection,
+          GOVERNANCE_CHAT_PROGRAM_ID,
+          proposalPubKey
+        ),
         getGovernanceAccount(
           connection,
           proposal.account.tokenOwnerRecord,
@@ -482,6 +487,7 @@ const useWalletStore = create<WalletStore>((set, get) => ({
 
       const chatMessages = await getGovernanceChatMessages(
         connection,
+        GOVERNANCE_CHAT_PROGRAM_ID,
         proposalPubKey
       )
 
