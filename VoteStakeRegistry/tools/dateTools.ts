1c6b22211037afa7cd8c59a7993027774c38de82
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-01-24 09:58:06 +0100

    add hours to date format

--- VoteStakeRegistry/tools/dateTools.ts
+++ VoteStakeRegistry/tools/dateTools.ts
@@ -2,17 +2,23 @@ export const DAYS_PER_YEAR = 365
 export const SECS_PER_DAY = 86400
 export const DAYS_PER_MONTH = DAYS_PER_YEAR / 12
 export const SECS_PER_MONTH = DAYS_PER_MONTH * SECS_PER_DAY
+export const HOURS_PER_DAY = 24
+export const MINS_PER_HOUR = 60
 
 export function getFormattedStringFromDays(numberOfDays: number) {
   const years = Math.floor(numberOfDays / DAYS_PER_YEAR)
   const months = Math.floor((numberOfDays % DAYS_PER_YEAR) / DAYS_PER_MONTH)
   const days = Math.floor((numberOfDays % DAYS_PER_YEAR) % DAYS_PER_MONTH)
-
+  const hours = (numberOfDays - Math.floor(numberOfDays)) * HOURS_PER_DAY
+  const hoursInt = Math.floor(hours)
+  const minutes = Math.floor((hours - hoursInt) * MINS_PER_HOUR)
   const yearsDisplay = years > 0 ? years + (years == 1 ? ' yr ' : ' yrs ') : ''
   const monthsDisplay =
     months > 0 ? months + (months == 1 ? ' mth ' : ' mths ') : ''
   const daysDisplay = days > 0 ? days + (days == 1 ? ' d' : ' days') : ''
-  const text = yearsDisplay + monthsDisplay + daysDisplay
+  const hoursDisplay = hours > 0 ? ` ${hoursInt} h ${minutes} min` : ''
+  const text =
+    days <= 1 ? hoursDisplay : yearsDisplay + monthsDisplay + daysDisplay
   return text ? text : 0
 }
 
