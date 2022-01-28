506da7dc41b85b2b906899ff9c7af42431f38037
Thomas Proust <thomas.proust89@gmail.com>
2022-01-28 23:11:39 +0900

    wip

--- components/instructions/programs/uxdProtocol.tsx
+++ components/instructions/programs/uxdProtocol.tsx
@@ -1,9 +1,16 @@
-import { Connection } from '@solana/web3.js'
+import { Connection, PublicKey, TransactionInstruction } from '@solana/web3.js'
 import { struct, u8, u48 } from 'buffer-layout'
-import { AccountMetaData } from '@solana/spl-governance'
+import {
+  AccountMetaData,
+  serializeInstructionToBase64,
+} from '@solana/spl-governance'
+import { Wallet } from '@project-serum/sol-wallet-adapter'
+import { Program, Provider } from '@project-serum/anchor'
+import { uxdIdl } from '@uxdprotocol/uxd-client'
+import { u128 } from '@project-serum/borsh'
 
 export const UXD_PROGRAM_INSTRUCTIONS = {
-  UXGA4U6nmCkdpVnhrhRzgFWYtmY4uHpNzLn3h9nVXNa: {
+  UXD8m9cvwk4RcSxnX2HZ9VudQCEeDH6fRnB4CAP57Dr: {
     1: {
       name: 'UXD - Initialize Controller',
       accounts: [
@@ -35,21 +42,49 @@ export const UXD_PROGRAM_INSTRUCTIONS = {
         )
       },
     },
-    2: {
+    45: {
       name: 'UXD - Set Redeemable Global Supply Cap',
       accounts: ['authority', 'controller'],
       getDataUI: (
         _connection: Connection,
         data: Uint8Array,
-        _accounts: AccountMetaData[]
+        _accounts: AccountMetaData[],
+        programId: PublicKey
       ) => {
-        const dataLayout = struct([u48('redeemable_global_supply_cap')])
+        const dataLayout = struct([
+          u8('instruction'),
+          u128('redeemableGlobalSupplyCap'),
+        ])
+
+        const itx = new TransactionInstruction({
+          programId,
+          keys: _accounts,
+          data: Buffer.from(data),
+        })
+        const program = new Program(
+          uxdIdl,
+          programId,
+          new Provider(
+            _connection,
+            (null as unknown) as Wallet,
+            Provider.defaultOptions()
+          )
+        )
+        console.debug(
+          'itx',
+          program.coder.instruction.decode(
+            program.coder.instruction.encode(
+              'SetRedeemableGlobalSupplyCap',
+              serializeInstructionToBase64(itx)
+            )
+          )?.data['redeemableGlobalSupplyCap']
+        )
 
         const args = dataLayout.decode(Buffer.from(data)) as any
-        console.log('args', args)
+        console.debug('args', args)
         return (
           <>
-            <p>{args}</p>
+            <p>test</p>
           </>
         )
       },
