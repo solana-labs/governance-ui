aba4ecc55fad1c2755f5ad9c52481df54a936a75
Thomas Proust <thomas.proust89@gmail.com>
2022-01-30 08:18:53 +0900

    use UXP instead of UXD as DAO symbol

--- utils/tokens.tsx
+++ utils/tokens.tsx
@@ -113,21 +113,10 @@ export async function tryGetTokenAccount(
   publicKey: PublicKey
 ): Promise<TokenProgramAccount<TokenAccount> | undefined> {
   try {
-    if (
-      publicKey.toBase58() === '9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E'
-    ) {
-      console.debug('publicKey', publicKey.toBase58())
-    }
     const result = await connection.getAccountInfo(publicKey)
 
     if (!result?.owner.equals(TOKEN_PROGRAM_ID)) {
       return undefined
-    }
-
-    if (
-      publicKey.toBase58() === '9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E'
-    ) {
-      console.debug('result owned by token program', result)
     }
 
     const data = Buffer.from(result!.data)
