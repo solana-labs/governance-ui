6365b267e883f066da99cfd132d9b422be11c800
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-02-07 20:56:14 +0100

    fix instruction translation

--- components/instructions/programs/voteStakeRegistry.tsx
+++ components/instructions/programs/voteStakeRegistry.tsx
@@ -25,41 +25,42 @@ export const VOTE_STAKE_REGISTRY_INSTRUCTIONS = {
         data: Uint8Array,
         accounts: AccountMetaData[]
       ) => {
-        const options = Provider.defaultOptions()
-        const provider = new Provider(
-          connection,
-          new Wallet(Keypair.generate()),
-          options
-        )
-        const vsrClient = await VsrClient.connect(provider)
-        const decodedInstruction = vsrClient.program.coder.instruction.decode(
-          Buffer.from(data)
-        )
-        const existingVoter = await tryGetVoter(accounts[2].pubkey, vsrClient)
-        const deposit =
-          //@ts-ignore
-          existingVoter?.deposits[decodedInstruction?.data.depositEntryIndex]
-        const existingRegistrar = await tryGetRegistrar(
-          accounts[0].pubkey,
-          vsrClient
-        )
-        const mintPk =
-          existingRegistrar?.votingMints[deposit!.votingMintConfigIdx].mint
-        console.log(mintPk)
-        const mint = await tryGetMint(connection, mintPk!)
+        try {
+          const options = Provider.defaultOptions()
+          const provider = new Provider(
+            connection,
+            new Wallet(Keypair.generate()),
+            options
+          )
+          const vsrClient = await VsrClient.connect(provider)
+          const decodedInstruction = vsrClient.program.coder.instruction.decode(
+            Buffer.from(data)
+          )
+          const existingVoter = await tryGetVoter(accounts[2].pubkey, vsrClient)
+          const deposit =
+            //@ts-ignore
+            existingVoter?.deposits[decodedInstruction?.data.depositEntryIndex]
+          const existingRegistrar = await tryGetRegistrar(
+            accounts[0].pubkey,
+            vsrClient
+          )
+          const mintPk =
+            existingRegistrar?.votingMints[deposit!.votingMintConfigIdx].mint
+          const mint = await tryGetMint(connection, mintPk!)
 
-        return (
-          <>
-            {deposit ? (
+          return (
+            <div className="space-y-3">
+              <div>Wallet: {existingVoter?.voterAuthority.toBase58()}</div>
               <div>
-                clawback amount:{' '}
-                {fmtMintAmount(mint?.account, deposit.amountDepositedNative)}
+                Clawback amount:{' '}
+                {fmtMintAmount(mint?.account, deposit!.amountDepositedNative)}
               </div>
-            ) : (
-              <div>{JSON.stringify(data)}</div>
-            )}
-          </>
-        )
+            </div>
+          )
+        } catch (e) {
+          console.log(e)
+          return <div>{JSON.stringify(data)}</div>
+        }
       },
     },
     145: {
@@ -81,70 +82,78 @@ export const VOTE_STAKE_REGISTRY_INSTRUCTIONS = {
         data: Uint8Array,
         accounts: AccountMetaData[]
       ) => {
-        const options = Provider.defaultOptions()
-        const provider = new Provider(
-          connection,
-          new Wallet(Keypair.generate()),
-          options
-        )
-        const vsrClient = await VsrClient.connect(provider)
-        const decodedInstruction = vsrClient.program.coder.instruction.decode(
-          Buffer.from(data)
-        )
-        console.log(accounts[9].pubkey.toBase58())
-        const mintPk = accounts[9].pubkey
-        const mint = await tryGetMint(connection, mintPk!)
-        // @ts-ignore
-        const lockupKind = Object.keys(decodedInstruction.data.kind)[0]
-        // @ts-ignore
-        const periods = decodedInstruction.data.periods
-        //@ts-ignore
-        console.log(decodedInstruction)
-        return (
-          <>
-            {decodedInstruction ? (
-              <div className="space-y-3">
-                <div>Grant to: {accounts[8].pubkey.toBase58()}</div>
+        try {
+          const options = Provider.defaultOptions()
+          const provider = new Provider(
+            connection,
+            new Wallet(Keypair.generate()),
+            options
+          )
+          const vsrClient = await VsrClient.connect(provider)
+          const decodedInstruction = vsrClient.program.coder.instruction.decode(
+            Buffer.from(data)
+          )
+          const mintPk = accounts[9].pubkey
+          const mint = await tryGetMint(connection, mintPk!)
+          // @ts-ignore
+          const lockupKind = Object.keys(decodedInstruction.data.kind)[0]
+          // @ts-ignore
+          const periods = decodedInstruction.data.periods
+          return (
+            <>
+              {decodedInstruction ? (
+                <div className="space-y-3">
+                  <div>Grant to: {accounts[8].pubkey.toBase58()}</div>
 
-                <div>
-                  Amount: {/* @ts-ignore */}
-                  {fmtMintAmount(mint!.account, decodedInstruction.data.amount)}
+                  <div>
+                    Amount:
+                    {fmtMintAmount(
+                      mint!.account,
+                      // @ts-ignore
+                      decodedInstruction.data.amount
+                    )}
+                  </div>
+                  <div>
+                    <img
+                      className="w-5 h-5"
+                      src={
+                        tokenService.getTokenInfo(mintPk.toBase58())?.logoURI
+                      }
+                    ></img>
+                  </div>
+                  <div>Lock type: {lockupKind}</div>
+                  <div>
+                    Start date:{' '}
+                    {new Date(
+                      // @ts-ignore
+                      decodedInstruction.data.startTs.toNumber() * 1000
+                    ).toDateString()}
+                  </div>
+                  <div>
+                    End date:{' '}
+                    {new Date(
+                      //   @ts-ignore
+                      decodedInstruction.data.startTs.toNumber() * 1000 +
+                        (lockupKind === 'monthly'
+                          ? periods * DAYS_PER_MONTH * SECS_PER_DAY * 1000
+                          : periods * SECS_PER_DAY * 1000)
+                    ).toDateString()}
+                  </div>
+                  <div>
+                    Dao can clawback: {/* @ts-ignore */}
+                    {decodedInstruction.data.allowClawback ? 'Yes' : 'No'}
+                  </div>
+                  <div>Only grantee can execute instruction</div>
                 </div>
-                <div>
-                  <img
-                    className="w-5 h-5"
-                    src={tokenService.getTokenInfo(mintPk.toBase58())?.logoURI}
-                  ></img>
-                </div>
-                <div>Lock type: {lockupKind}</div>
-                <div>
-                  Start date:{' '}
-                  {new Date(
-                    // @ts-ignore
-                    decodedInstruction.data.startTs.toNumber() * 1000
-                  ).toDateString()}
-                </div>
-                <div>
-                  End date:{' '}
-                  {new Date(
-                    //   @ts-ignore
-                    decodedInstruction.data.startTs.toNumber() * 1000 +
-                      (lockupKind === 'monthly'
-                        ? periods * DAYS_PER_MONTH * SECS_PER_DAY * 1000
-                        : periods * SECS_PER_DAY * 1000)
-                  ).toDateString()}
-                </div>
-                <div>
-                  Dao can clawback: {/* @ts-ignore */}
-                  {decodedInstruction.data.allowClawback ? 'Yes' : 'No'}
-                </div>
-                <div>Only grantee can execute instruction</div>
-              </div>
-            ) : (
-              <div>{JSON.stringify(data)}</div>
-            )}
-          </>
-        )
+              ) : (
+                <div>{JSON.stringify(data)}</div>
+              )}
+            </>
+          )
+        } catch (e) {
+          console.log(e)
+          return <div>{JSON.stringify(data)}</div>
+        }
       },
     },
   },
