55d5692dc08165c6e1d73be8da5e7b6f29e6c97a
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-02-09 15:47:43 +0000

    fix: show CLI details on quick upgrade view

--- pages/dao/[symbol]/proposal/components/instructions/bpfUpgradeableLoader/ProgramUpgradeInfo.tsx
+++ pages/dao/[symbol]/proposal/components/instructions/bpfUpgradeableLoader/ProgramUpgradeInfo.tsx
@@ -3,10 +3,8 @@ import CommandLineInfo from '../../ComandLineInfo'
 
 export default function ProgramUpgradeInfo({
   governancePk,
-  cliVisible = false,
 }: {
   governancePk: PublicKey | undefined
-  cliVisible?: boolean
 }) {
   if (!governancePk) {
     return null
@@ -18,15 +16,11 @@ export default function ProgramUpgradeInfo({
 
       <CommandLineInfo info={governancePk?.toBase58()}></CommandLineInfo>
 
-      {cliVisible && (
-        <>
-          <div className="mb-2">Solana CLI</div>
-          <CommandLineInfo info="solana program write-buffer <PROGRAM_FILEPATH>"></CommandLineInfo>
-          <CommandLineInfo
-            info={`solana program set-buffer-authority --new-buffer-authority ${governancePk?.toBase58()} <BUFFER_PUBKEY>`}
-          ></CommandLineInfo>
-        </>
-      )}
+      <div className="mb-2">Solana CLI</div>
+      <CommandLineInfo info="solana program write-buffer <PROGRAM_FILEPATH>"></CommandLineInfo>
+      <CommandLineInfo
+        info={`solana program set-buffer-authority --new-buffer-authority ${governancePk?.toBase58()} <BUFFER_PUBKEY>`}
+      ></CommandLineInfo>
     </div>
   )
 }
