91192a9101356623051a49ac1691bc202990426f
Christian Kamm <mail@ckamm.de>
2022-02-21 23:17:34 +0100

    Add Mango related account identifiers; adjust existing ones (#370)
    * Account names: Add Mango related accounts
    
    * Program account names: Add consistent "Program" postfix

--- components/instructions/programs/names.ts
+++ components/instructions/programs/names.ts
@@ -1,21 +1,22 @@
 import { PublicKey } from '@solana/web3.js'
 
 export const GOVERNANCE_PROGRAM_NAMES = {
-  GqTPL6qRf5aUuqscLh8Rg2HTxPUXfhhAXDptTLhp1t2J: 'Mango Governance',
-  AVoAYTs36yB5izAaBkxRG67wL1AMwG3vo41hKtUSb8is: 'Serum Governance',
-  GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw: 'Governance',
-  '5sGZEdn32y8nHax7TxEyoHuPS3UXfPWtisgm8kqxat8H': 'Phantasia Governance',
+  GqTPL6qRf5aUuqscLh8Rg2HTxPUXfhhAXDptTLhp1t2J: 'Mango Governance Program',
+  AVoAYTs36yB5izAaBkxRG67wL1AMwG3vo41hKtUSb8is: 'Serum Governance Program',
+  GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw: 'Governance Program',
+  '5sGZEdn32y8nHax7TxEyoHuPS3UXfPWtisgm8kqxat8H':
+    'Phantasia Governance Program',
   smfjietFKFJ4Sbw1cqESBTpPhF4CwbMwN8kBEC1e5ui:
-    'Strangemood Foundation Governance',
+    'Strangemood Foundation Governance Program',
 }
 
 // Well known program names displayed on the instruction card
 export const PROGRAM_NAMES = {
   TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA: 'Token Program',
   '11111111111111111111111111111111': 'System Program',
-  mv3ekLzLbnVPNxjSKvqBpU3ZeZXPQdEC3bp5MDEBG68: 'Mango v3',
-  '9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin': 'Serum v3',
-  DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY: 'Serum v3 (devnet)',
+  mv3ekLzLbnVPNxjSKvqBpU3ZeZXPQdEC3bp5MDEBG68: 'Mango v3 Program',
+  '9xQeWvG816bUx9EPjHmaT23yvVM2ZWbrrpZb9PusVFin': 'Serum v3 Program',
+  DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY: 'Serum v3 Program (devnet)',
 
   BPFLoaderUpgradeab1e11111111111111111111111: 'BPF Upgradeable Loader',
 
@@ -26,7 +27,8 @@ export const PROGRAM_NAMES = {
 
   SysvarRent111111111111111111111111111111111: 'Sysvar: Rent',
   SysvarC1ock11111111111111111111111111111111: 'Sysvar: Clock',
-  '4Q6WW2ouZ6V3iaNm56MTd5n2tnTm4C5fiH8miFHnAFHo': 'Voter Stake Registry',
+  '4Q6WW2ouZ6V3iaNm56MTd5n2tnTm4C5fiH8miFHnAFHo':
+    'Mango Voter Stake Registry Program',
   ...GOVERNANCE_PROGRAM_NAMES,
 }
 
