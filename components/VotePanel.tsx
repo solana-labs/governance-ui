0929cd79871c0147eee79ad998dbea1105ada1c8
saml33 <30796577+saml33@users.noreply.github.com>
2022-02-25 10:57:56 +1100

    add approval progress to failed proposals and styling (#379)

--- components/VotePanel.tsx
+++ components/VotePanel.tsx
@@ -168,7 +168,7 @@ const VotePanel = () => {
     <>
       {isPanelVisible && (
         <div className="bg-bkg-2 p-4 md:p-6 rounded-lg space-y-4">
-          <h2 className="mb-4 text-center">{actionLabel}</h2>
+          <h3 className="mb-4 text-center">{actionLabel}</h3>
 
           <div className="items-center justify-center flex w-full gap-5">
             {isVoteCast && connected ? (
--- components/VoteResultStatus.tsx
+++ components/VoteResultStatus.tsx
@@ -1,21 +1,44 @@
 import { XCircleIcon, CheckCircleIcon } from '@heroicons/react/outline'
 
 type VoteResultStatusProps = {
+  progress: number
   votePassed: boolean | undefined
+  yesVotesRequired: number
 }
 
-const VoteResultStatus = ({ votePassed }: VoteResultStatusProps) => {
-  return (
-    <div className="border border-fgd-4 flex items-center p-3 rounded-md">
-      {votePassed ? (
-        <CheckCircleIcon className="h-6 mr-1.5 text-green w-6" />
-      ) : (
-        <XCircleIcon className="h-6 mr-1.5 text-red w-6" />
-      )}
-      <div>
-        <h4 className="mb-0">
-          {votePassed ? 'The proposal has passed' : 'The proposal has failed'}
-        </h4>
+const VoteResultStatus = ({
+  progress,
+  votePassed,
+  yesVotesRequired,
+}: VoteResultStatusProps) => {
+  return votePassed ? (
+    <div className="bg-bkg-1 flex items-center p-3 rounded-md">
+      <CheckCircleIcon className="h-5 mr-1.5 text-green w-5" />
+      <h4 className="mb-0">The proposal has passed</h4>
+    </div>
+  ) : (
+    <div className="bg-bkg-1 p-3 rounded-md">
+      <div className="flex items-center">
+        <XCircleIcon className="h-5 mr-1.5 text-red w-5" />
+        <div>
+          <h4 className="mb-0">The proposal has failed</h4>
+          <p className="mb-0 text-fgd-2">{`${yesVotesRequired?.toLocaleString(
+            undefined,
+            {
+              maximumFractionDigits: 0,
+            }
+          )} Yes votes were needed`}</p>
+        </div>
+      </div>
+      <div className="bg-bkg-4 h-2 flex flex-grow mt-2.5 rounded w-full">
+        <div
+          style={{
+            width: `${progress}%`,
+          }}
+          className={`${
+            progress >= 100 ? 'bg-green' : 'bg-fgd-3'
+          } flex rounded`}
+        ></div>
       </div>
     </div>
   )
