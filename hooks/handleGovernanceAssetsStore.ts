62956ad2951297e2728d04523acf45b1b59f6d79
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-03-01 18:33:54 +0100

    remove nft from token accounts

--- hooks/handleGovernanceAssetsStore.ts
+++ hooks/handleGovernanceAssetsStore.ts
@@ -124,7 +124,9 @@ export default function handleGovernanceAssetsStore() {
             transferAddress: tokenAcc.account.address,
             solAccount: null,
           }
-          governedTokenAccountsArray.push(obj)
+          if (mint?.account.decimals) {
+            governedTokenAccountsArray.push(obj)
+          }
         }
       }
       await tokenService.fetchTokenPrices(
