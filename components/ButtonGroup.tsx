6eb1afad89d2cae393a13bb05f779e577581fe92
saml33 <30796577+saml33@users.noreply.github.com>
2022-03-03 07:37:11 +1100

    Account UI review (#400)
    * filter by symbol
    
    * ui/ux tweaks
    
    * start unlock form

---
+++ components/ButtonGroup.tsx
@@ -0,0 +1,57 @@
+import { FunctionComponent } from 'react'
+
+interface ButtonGroupProps {
+  activeValue: string
+  className?: string
+  onChange: (x) => void
+  unit?: string
+  values: Array<string>
+  names?: Array<string>
+}
+
+const ButtonGroup: FunctionComponent<ButtonGroupProps> = ({
+  activeValue,
+  className,
+  unit,
+  values,
+  onChange,
+  names,
+}) => {
+  return (
+    <div className="bg-bkg-3 rounded-md">
+      <div className="flex relative">
+        {activeValue && values.includes(activeValue) ? (
+          <div
+            className={`absolute bg-bkg-4 default-transition h-full left-0 top-0 rounded-md transform`}
+            style={{
+              transform: `translateX(${
+                values.findIndex((v) => v === activeValue) * 100
+              }%)`,
+              width: `${100 / values.length}%`,
+            }}
+          />
+        ) : null}
+        {values.map((v, i) => (
+          <button
+            className={`${className} cursor-pointer default-transition font-normal px-2 py-1.5 relative rounded-md text-center text-xs w-1/2
+              ${
+                v === activeValue
+                  ? `text-primary-light`
+                  : `text-fgd-2 hover:text-primary-light`
+              }
+            `}
+            key={`${v}${i}`}
+            onClick={() => onChange(v)}
+            style={{
+              width: `${100 / values.length}%`,
+            }}
+          >
+            {names ? (unit ? names[i] + unit : names[i]) : unit ? v + unit : v}
+          </button>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default ButtonGroup
