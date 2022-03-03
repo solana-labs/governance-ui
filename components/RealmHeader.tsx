6f81f05ebc5c2885c63a39744f963e81c2934cb5
Kevin Mack <kmack418@gmail.com>
2022-03-03 09:40:14 -0500

    Base setup of Tokr Branding

--- components/RealmHeader.tsx
+++ components/RealmHeader.tsx
@@ -19,66 +19,31 @@ const RealmHeader = () => {
   const realmUrl = `https://${explorerHost}/#/realm/${realmInfo?.realmId.toBase58()}?programId=${realmInfo?.programId.toBase58()}`
 
   return (
-    <div className="pb-4">
+    <>
       {isBackNavVisible ? (
         <Link href={fmtUrlWithCluster('/realms')}>
-          <a className="default-transition flex items-center mb-2 md:mb-6 text-fgd-3 text-sm transition-all hover:text-fgd-1">
-            <ArrowLeftIcon className="h-4 w-4 mr-1 text-primary-light" />
-            Back
+          <a className="uppercase default-transition flex items-center mb-2 md:mb-6 text-fgd-3 text-sm transition-all hover:text-fgd-1">
+            &lt; Back
           </a>
         </Link>
       ) : null}
-      <div className="border-b border-fgd-4 flex flex-col md:flex-row md:items-center md:justify-between pb-3">
-        {realmDisplayName ? (
-          <a href={realmUrl} target="_blank" rel="noopener noreferrer">
-            <div className="flex items-center cursor-pointer">
-              <div className="flex flex-col md:flex-row items-center pb-3 md:pb-0">
-                {realmInfo?.ogImage ? (
-                  <img
-                    className="flex-shrink-0 mb-2 md:mb-0 w-8"
-                    src={realmInfo?.ogImage}
-                  ></img>
-                ) : (
-                  <div className="bg-[rgba(255,255,255,0.1)] h-14 w-14 flex font-bold items-center justify-center rounded-full text-fgd-3">
-                    {realmDisplayName?.charAt(0)}
-                  </div>
-                )}
-                <h1 className="ml-3">{realmDisplayName}</h1>
-              </div>
-              <ExternalLinkIcon
-                className={`flex-shrink-0 h-4 w-4 ml-2 text-primary-light`}
-              />
-            </div>
-          </a>
-        ) : (
-          <div className="animate-pulse bg-bkg-3 h-10 w-40 rounded-md" />
-        )}
-        <div className="flex items-center space-x-6">
-          {realmInfo?.website ? (
-            <a
-              className="default-transition flex items-center text-fgd-1 text-sm hover:text-primary-light"
-              href={realmInfo?.website}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <GlobeAltIcon className="mr-1.5 h-4 text-primary-light w-4" />
-              Website
-            </a>
-          ) : null}
-          {realmInfo?.twitter ? (
-            <a
-              className="default-transition flex items-center text-fgd-1 text-sm hover:text-primary-light"
-              href={`https://twitter.com/${realmInfo?.twitter}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TwitterIcon className="mr-1.5 h-4 text-primary-light w-4" />
-              Twitter
-            </a>
-          ) : null}
-        </div>
+      <div className="ml-4 -mb-5 relative z-10 m-width-full">
+        <a
+          href={realmUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-dark inline-block"
+        >
+          <span className="flex items-center cursor-pointer">
+            <span className="flex flex-col md:flex-row items-center pb-3 md:pb-0">
+              <span className="ml-4 pr-8 text-3xl uppercase">
+                {realmDisplayName}
+              </span>
+            </span>
+          </span>
+        </a>
       </div>
-    </div>
+    </>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,104 +21,83 @@ import useMarketStore from 'Strategies/store/marketStore'
 import handleGovernanceAssetsStore from '@hooks/handleGovernanceAssetsStore'
 
 function App({ Component, pageProps }) {
-  useHydrateStore()
-  useWallet()
-  useRouterHistory()
-  useVoteRegistry()
-  handleGovernanceAssetsStore()
-  const { loadMarket } = useMarketStore()
-  const { getOwnedDeposits, resetDepositState } = useDepositStore()
-  const { realm, realmInfo, symbol, ownTokenRecord } = useRealm()
-  const wallet = useWalletStore((s) => s.current)
-  const connection = useWalletStore((s) => s.connection)
-  const client = useVoteStakeRegistryClientStore((s) => s.state.client)
-  const realmName = realmInfo?.displayName ?? realm?.account?.name
+	useHydrateStore()
+	useWallet()
+	useRouterHistory()
+	useVoteRegistry()
+	handleGovernanceAssetsStore()
+	const { loadMarket } = useMarketStore()
+	const { getOwnedDeposits, resetDepositState } = useDepositStore()
+	const { realm, realmInfo, symbol, ownTokenRecord } = useRealm()
+	const wallet = useWalletStore((s) => s.current)
+	const connection = useWalletStore((s) => s.connection)
+	const client = useVoteStakeRegistryClientStore((s) => s.state.client)
+	const realmName = realmInfo?.displayName ?? realm?.account?.name
 
-  const title = realmName ? `${realmName}` : 'Solana Governance'
-  const description = `Discuss and vote on ${title} proposals.`
+	const title = realmName ? `${realmName}` : 'Solana Governance'
+	const description = `Discuss and vote on ${title} proposals.`
 
-  // Note: ?v==${Date.now()} is added to the url to force favicon refresh.
-  // Without it browsers would cache the last used and won't change it for different realms
-  // https://stackoverflow.com/questions/2208933/how-do-i-force-a-favicon-refresh
-  const faviconSelector = symbol ?? 'SOLANA'
-  const faviconUrl = `/realms/${getResourcePathPart(
-    faviconSelector as string
-  )}/favicon.ico?v=${Date.now()}`
-  useEffect(() => {
-    loadMarket(connection, connection.cluster)
-  }, [connection.cluster])
-  useEffect(() => {
-    if (
-      realm?.account.config.useCommunityVoterWeightAddin &&
-      realm.pubkey &&
-      wallet?.connected &&
-      client
-    ) {
-      getOwnedDeposits({
-        realmPk: realm!.pubkey,
-        communityMintPk: realm!.account.communityMint,
-        walletPk: wallet!.publicKey!,
-        client: client!,
-        connection: connection.current,
-      })
-    } else if (!wallet?.connected) {
-      resetDepositState()
-    }
-  }, [
-    realm?.pubkey.toBase58(),
-    ownTokenRecord?.pubkey.toBase58(),
-    wallet?.connected,
-    client,
-  ])
-  return (
-    <div className="relative">
-      <Head>
-        <title>{title}</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=PT+Mono&display=swap"
-          rel="stylesheet"
-        />
+	// Note: ?v==${Date.now()} is added to the url to force favicon refresh.
+	// Without it browsers would cache the last used and won't change it for different realms
+	// https://stackoverflow.com/questions/2208933/how-do-i-force-a-favicon-refresh
+	const faviconSelector = symbol ?? 'SOLANA'
+	const faviconUrl = `/realms/${getResourcePathPart(faviconSelector as string)}/favicon.ico?v=${Date.now()}`
+	useEffect(() => {
+		loadMarket(connection, connection.cluster)
+	}, [connection.cluster])
+	useEffect(() => {
+		if (realm?.account.config.useCommunityVoterWeightAddin && realm.pubkey && wallet?.connected && client) {
+			getOwnedDeposits({
+				realmPk: realm!.pubkey,
+				communityMintPk: realm!.account.communityMint,
+				walletPk: wallet!.publicKey!,
+				client: client!,
+				connection: connection.current,
+			})
+		} else if (!wallet?.connected) {
+			resetDepositState()
+		}
+	}, [realm?.pubkey.toBase58(), ownTokenRecord?.pubkey.toBase58(), wallet?.connected, client])
+	return (
+		<div className="relative">
+			<Head>
+				<title>{title}</title>
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=PT+Mono&display=swap" rel="stylesheet" />
 
-        {faviconUrl && <link rel="icon" href={faviconUrl} />}
+				{faviconUrl && <link rel="icon" href={faviconUrl} />}
 
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        {realmInfo?.keywords && (
-          <meta name="keywords" content={realmInfo.keywords} />
-        )}
+				{realmInfo?.keywords && <meta name="keywords" content={realmInfo.keywords} />}
 
-        <meta name="description" content={description} />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
+				<meta name="description" content={description} />
+				<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+				<meta name="msapplication-TileColor" content="#ffffff" />
+				<meta name="theme-color" content="#ffffff" />
 
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        {realmInfo?.ogImage && (
-          <meta property="og:image" content={realmInfo.ogImage} />
-        )}
-        <meta name="twitter:card" content="summary" />
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content={title} />
+				<meta property="og:description" content={description} />
+				{realmInfo?.ogImage && <meta property="og:image" content={realmInfo.ogImage} />}
+				<meta name="twitter:card" content="summary" />
 
-        {realmInfo?.twitter && (
-          <meta name="twitter:site" content={realmInfo.twitter} />
-        )}
-      </Head>
-      <ErrorBoundary>
-        <ThemeProvider defaultTheme="Mango">
-          <WalletIdentityProvider appName={'Realms'}>
-            <NavBar />
-            <Notifications />
-            <PageBodyContainer>
-              <Component {...pageProps} />
-            </PageBodyContainer>
-          </WalletIdentityProvider>
-        </ThemeProvider>
-      </ErrorBoundary>
-      <Footer />
-    </div>
-  )
+				{realmInfo?.twitter && <meta name="twitter:site" content={realmInfo.twitter} />}
+			</Head>
+			<ErrorBoundary>
+				<ThemeProvider defaultTheme="Mango">
+					<WalletIdentityProvider appName={'Realms'}>
+						<NavBar />
+						<Notifications />
+						<PageBodyContainer>
+							<Component {...pageProps} />
+						</PageBodyContainer>
+					</WalletIdentityProvider>
+				</ThemeProvider>
+			</ErrorBoundary>
+			{/* <Footer /> */}
+		</div>
+	)
 }
 
 export default App
