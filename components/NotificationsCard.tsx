0c086886d2a82f7c6c529193f27ccf02b27e52c0
Juni Kim <nubstick27@users.noreply.github.com>
2022-03-04 13:21:08 -0800

    Update NotificationsCard to use SDK
    The new client hook wraps much more functionality and exposes stuff nicely.
    
    A lot of the business logic that was previously in the NotificationsCard is now moved into the SDK.

--- components/NotificationsCard.tsx
+++ components/NotificationsCard.tsx
@@ -2,326 +2,112 @@ import useRealm from '../hooks/useRealm'
 import useWalletStore from '../stores/useWalletStore'
 import Button from './Button'
 import Input from './inputs/Input'
-import React, {
-  FunctionComponent,
-  useCallback,
-  useEffect,
-  useState,
-} from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
 import {
   ChatAltIcon,
   MailIcon,
   PaperAirplaneIcon,
 } from '@heroicons/react/solid'
 import {
-  useCreateAlert,
-  useCreateEmailTarget,
-  useCreateSmsTarget,
-  useCreateTargetGroup,
-  useCreateTelegramTarget,
-  useGetFilters,
-  useGetSourceGroups,
-  useGetTargetGroups,
-  useLoginFromDao,
-  useNotifiJwt,
-  useUpdateTargetGroup,
+  BlockchainEnvironment,
+  GqlError,
+  MessageSigner,
+  useNotifiClient,
 } from '@notifi-network/notifi-react-hooks'
-
-function bufferToBase64(buf) {
-  const binstr = Array.prototype.map
-    .call(buf, function (ch) {
-      return String.fromCharCode(ch)
-    })
-    .join('')
-  return btoa(binstr)
-}
 
 const NotificationsCard = () => {
   const { councilMint, mint, realm } = useRealm()
   const [isLoading, setLoading] = useState<boolean>(false)
   const [hasUnsavedChanges, setUnsavedChanges] = useState<boolean>(false)
   const [errorMessage, setErrorMessage] = useState<string>('')
-  const [email, setEmail] = useState<string>('')
-  const [phone, setPhone] = useState<string>('')
-  const [telegram, setTelegram] = useState<string>('')
-  const [sourceGroup, setSourceGroup] = useState<string>('')
-  const [filter, setFilter] = useState<string>('')
-  const [targetGroup, setTargetGroup] = useState<string>('')
-  const [storedEmail, setStoredEmail] = useState<string>('')
-  const [storedSms, setStoredSms] = useState<string>('')
-  const [storedTelegram, setStoredTelegram] = useState<string>('')
-  const [storedEmailId, setStoredEmailId] = useState<string>('')
-  const [storedSmsId, setStoredSmsId] = useState<string>('')
-  const [storedTelegramId, setStoredTelegramId] = useState<string>('')
   const enableTelegramInput = false
 
   const wallet = useWalletStore((s) => s.current)
   const connected = useWalletStore((s) => s.connected)
+  const {
+    data,
+    logIn,
+    isAuthenticated,
+    updateAlert,
+    fetchData,
+  } = useNotifiClient({
+    daoAddress: realm?.pubkey?.toBase58() ?? '',
+    walletPublicKey: wallet?.publicKey?.toString() ?? '',
+    env: BlockchainEnvironment.MainNetBeta,
+  })
+  const [email, setEmail] = useState<string>(data?.emailAddress ?? '')
+  const [phone, setPhone] = useState<string>(data?.phoneNumber ?? '')
+  const [telegram, setTelegram] = useState<string>(data?.telegramId ?? '')
 
-  const getTargetGroups = useGetTargetGroups()
-  const getExistingTargetGroup = useCallback(() => {
-    getTargetGroups()
-      .then((resp) => {
-        const targetGroup = resp[0]
-        console.log(targetGroup)
-        if (targetGroup != null) {
-          setTargetGroup(targetGroup.id ?? '')
-          if (targetGroup.emailTargets.length > 0) {
-            const emailTarget = targetGroup.emailTargets[0]
-            setEmail(emailTarget.emailAddress ?? '')
-            setStoredEmail(emailTarget.emailAddress ?? '')
-            setStoredEmailId(emailTarget.id ?? '')
-          }
-          if (targetGroup.smsTargets.length > 0) {
-            const smsTarget = targetGroup.smsTargets[0]
-            setPhone(smsTarget.phoneNumber ?? '')
-            setStoredSms(smsTarget.phoneNumber ?? '')
-            setStoredSmsId(smsTarget.id ?? '')
-          }
-          if (targetGroup.telegramTargets.length > 0) {
-            const telegramTarget = targetGroup.telegramTargets[0]
-            setTelegram(telegramTarget.telegramId ?? '')
-            setStoredTelegram(telegramTarget.telegramId ?? '')
-            setStoredTelegramId(telegramTarget.id ?? '')
-          }
-        }
-      })
-      .catch((err) => {
-        console.log('Request failed: ' + JSON.stringify(err))
-      })
-  }, [getTargetGroups])
-
-  const getSourceGroups = useGetSourceGroups()
-  const getSourceGroup = useCallback(() => {
-    getSourceGroups()
-      .then((resp) => {
-        const sourceGroup = resp[0]
-        console.log(sourceGroup)
-        setSourceGroup(sourceGroup.id ?? '')
-      })
-      .catch((err) => {
-        console.log('Request failed: ' + JSON.stringify(err))
-      })
-  }, [getSourceGroups])
-
-  const getFilters = useGetFilters()
-  const getFilter = useCallback(() => {
-    getFilters()
-      .then((resp) => {
-        const filter = resp[0]
-        console.log(filter)
-        setFilter(filter.id ?? '')
-      })
-      .catch((err) => {
-        console.log('Request failed: ' + JSON.stringify(err))
-      })
-  }, [getFilters])
-
-  const doCreateAlert = useCreateAlert()
-  const createAlert = (tgId: string) => {
-    doCreateAlert({
-      sourceGroupId: sourceGroup,
-      filterId: filter,
-      targetGroupId: tgId,
-    })
-      .then((resp) => {
-        // TODO: find out why we set filter here
-        const filter = resp.filter[0]
-        console.log(filter)
-        setFilter(filter.id ?? '')
-      })
-      .catch((err) => {
-        console.log('Request failed: ' + JSON.stringify(err))
-      })
-  }
-
-  const doCreateEmailTarget = useCreateEmailTarget()
-  const createEmailTarget = async (): Promise<string> => {
-    const emailTarget = await doCreateEmailTarget({
-      name: email,
-      value: email,
-    })
-
-    console.log('createEmailTarget: ' + emailTarget.id)
-    setStoredEmailId(emailTarget.id ?? '')
-
-    return emailTarget.id!
-  }
-
-  const doCreateSmsTarget = useCreateSmsTarget()
-  const createSmsTarget = async (): Promise<string> => {
-    const smsTarget = await doCreateSmsTarget({
-      name: phone,
-      value: phone,
-    })
-
-    console.log(smsTarget)
-    return smsTarget.id!
-  }
-
-  const doCreateTelegramTarget = useCreateTelegramTarget()
-  const createTelegramTarget = async (): Promise<string> => {
-    const telegramTarget = await doCreateTelegramTarget({
-      name: telegram,
-      value: telegram,
-    })
-
-    console.log(telegramTarget)
-
-    const telegramLink =
-      'https://t.me/NotifiNetworkBot?start=' + telegramTarget.telegramId!
-    setTelegram(telegramLink)
-    setStoredTelegram(telegramLink)
-    window.open(telegramLink)
-
-    return telegramTarget.id!
-  }
-
-  const doUpdateTargetGroup = useUpdateTargetGroup()
-  const updateTargetGroup = async function (
-    emailId: string,
-    smsId: string,
-    telegramId: string
-  ): Promise<string> {
-    const emailTargetIds: string[] = []
-    if (emailId !== '') {
-      emailTargetIds.push(emailId)
-    }
-
-    const smsTargetIds: string[] = []
-    if (smsId !== '') {
-      smsTargetIds.push(smsId)
-    }
-
-    const telegramTargetIds: string[] = []
-    if (telegramId !== '') {
-      telegramTargetIds.push(telegramId)
-    }
-
-    const resp = await doUpdateTargetGroup({
-      targetGroupId: targetGroup,
-      name: `${realm?.account.name} notifications`,
-      emailTargetIds,
-      smsTargetIds,
-      telegramTargetIds,
-    })
-
-    console.log(resp)
-    return resp.id!
-  }
-
-  const createTargetGroup = useCreateTargetGroup()
-  const createNewTargetGroup = async function (
-    emailId: string,
-    smsId: string,
-    telegramId: string
-  ): Promise<string> {
-    const emailTargetIds: string[] = []
-    if (emailId !== '') {
-      emailTargetIds.push(emailId)
-    }
-
-    const smsTargetIds: string[] = []
-    if (smsId !== '') {
-      smsTargetIds.push(smsId)
-    }
-
-    const telegramTargetIds: string[] = []
-    if (telegramId !== '') {
-      telegramTargetIds.push(telegramId)
-    }
-
-    const resp = await createTargetGroup({
-      name: `${realm?.account.name} notifications`,
-      emailTargetIds,
-      smsTargetIds,
-      telegramTargetIds,
-    })
-
-    console.log(resp)
-    return resp.id!
-  }
+  useEffect(() => {
+    // Update state when server data changes
+    setEmail(data?.emailAddress ?? '')
+    setPhone(data?.phoneNumber ?? '')
+    setTelegram(data?.telegramId ?? '')
+  }, [data])
 
   const handleError = (errors: { message: string }[]) => {
-    const message = errors.length > 0 ? errors[0]?.message : 'Unknown error'
-    setErrorMessage(message)
+    const error = errors.length > 0 ? errors[0] : null
+    if (error instanceof GqlError) {
+      setErrorMessage(
+        `${error.message}: ${error.getErrorMessages().join(', ')}`
+      )
+    } else {
+      setErrorMessage(error?.message ?? 'Unknown error')
+    }
     setLoading(false)
   }
 
-  const { jwtRef, setJwt } = useNotifiJwt()
-  const logInFromDao = useLoginFromDao()
-  const handleSave = async function () {
+  const handleRefresh = async function () {
     setLoading(true)
     // user is not authenticated
-    if (!jwtRef.current && wallet && wallet.publicKey) {
-      console.log(wallet)
-      console.log(jwtRef.current)
-      const ticks = Math.round(Date.now() / 1000)
-      const p = await (wallet as any).signMessage(
-        new TextEncoder().encode(
-          `${wallet?.publicKey}` + realm?.pubkey + ticks.toString()
-        ),
-        'utf8'
-      )
-
+    if (!isAuthenticated() && wallet && wallet.publicKey) {
       try {
-        const resp = logInFromDao({
-          walletPublicKey: wallet.publicKey.toString(),
-          daoAddress: realm?.pubkey.toBase58() || '',
-          timestamp: ticks,
-          signature: bufferToBase64(p),
-        })
-
-        console.log(resp)
-
-        getExistingTargetGroup()
-        getFilter()
-        getSourceGroup()
+        await logIn(wallet as MessageSigner)
       } catch (e) {
         handleError([e])
       }
       setLoading(false)
     }
 
-    if (connected && jwtRef.current) {
+    // Update fields
+    if (connected && isAuthenticated()) {
+      console.log('Refreshing values')
+      try {
+        const { emailAddress, phoneNumber, telegramId } = await fetchData()
+        setEmail(emailAddress ?? '')
+        setPhone(phoneNumber ?? '')
+        setTelegram(telegramId ?? '')
+      } catch (e) {
+        handleError([e])
+      }
+    }
+    setLoading(false)
+  }
+
+  const handleSave = async function () {
+    setLoading(true)
+    // user is not authenticated
+    if (!isAuthenticated() && wallet && wallet.publicKey) {
+      try {
+        await logIn(wallet as MessageSigner)
+      } catch (e) {
+        handleError([e])
+      }
+    }
+
+    if (connected && isAuthenticated()) {
       console.log('Sending')
       console.log(email)
-      console.log(storedEmail)
-      console.log(storedEmailId)
-      let emailId = storedEmailId
-      let smsId = storedSmsId
-      let telegramId = storedTelegramId
       try {
-        if (email != storedEmail) {
-          console.log('creating email')
-          emailId = await createEmailTarget()
-          setStoredEmailId(emailId)
-        }
-
-        if (phone != storedSms) {
-          smsId = await createSmsTarget()
-          setStoredSmsId(smsId)
-        }
-
-        if (telegram != storedTelegram) {
-          telegramId = await createTelegramTarget()
-          setStoredTelegramId(telegramId)
-        }
-
-        if (targetGroup) {
-          // Update
-          console.log('updating target group')
-          await updateTargetGroup(emailId, smsId, telegramId)
-        } else {
-          // New
-          console.log('creating new target group')
-          const tgId = await createNewTargetGroup(emailId, smsId, telegramId)
-          setTargetGroup(tgId)
-          console.log('creating new alert')
-          await createAlert(tgId)
-        }
+        await updateAlert({
+          name: `${realm?.account.name} notifications`,
+          emailAddress: email === '' ? null : email,
+          phoneNumber: phone.length < 12 ? null : phone,
+          telegramId: telegram === '' ? null : telegram,
+        })
+        setUnsavedChanges(false)
       } catch (e) {
-        setJwt(null)
         handleError([e])
       }
     }
@@ -354,13 +140,7 @@ const NotificationsCard = () => {
     setUnsavedChanges(true)
   }
 
-  useEffect(() => {
-    if (connected && jwtRef.current != null) {
-      getExistingTargetGroup()
-      getFilter()
-      getSourceGroup()
-    }
-  }, [jwtRef, connected])
+  const disabled = isAuthenticated() && !hasUnsavedChanges
 
   return (
     <div className="bg-bkg-2 p-4 md:p-6 rounded-lg">
@@ -381,7 +161,7 @@ const NotificationsCard = () => {
               {errorMessage.length > 0 ? (
                 <div className="text-sm text-red">{errorMessage}</div>
               ) : (
-                !jwtRef.current && (
+                !isAuthenticated() && (
                   <div className="text-sm text-fgd-3">
                     When prompted, sign the transaction.
                   </div>
@@ -433,19 +213,28 @@ const NotificationsCard = () => {
                 />
               </InputRow>
             )}
-
-            <div className="flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0 mt-4 justify-end">
-              {hasUnsavedChanges && (
+            <div className="flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0 mt-4 items-center justify-between">
+              {
                 <Button
-                  tooltipMessage="Save settings for notifications"
+                  tooltipMessage={
+                    disabled
+                      ? 'No unsaved changes!'
+                      : isAuthenticated()
+                      ? 'Save settings for notifications'
+                      : 'Fetch stored values for existing accounts'
+                  }
                   className="sm:w-1/2"
-                  disabled={!hasUnsavedChanges}
-                  onClick={handleSave}
+                  disabled={disabled}
+                  onClick={
+                    hasUnsavedChanges || isAuthenticated()
+                      ? handleSave
+                      : handleRefresh
+                  }
                   isLoading={isLoading}
                 >
-                  Save
+                  {hasUnsavedChanges || isAuthenticated() ? 'Save' : 'Refresh'}
                 </Button>
-              )}
+              }
             </div>
           </>
         )
