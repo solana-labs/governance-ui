f0c57abdfce9f40965897a5e5a5fc08a3090a1af
Kevin Mack <kmack418@gmail.com>
2022-03-07 09:52:11 -0500

    adding some temp data to make testing easier

--- pages/dao/[symbol]/proposal/new.tsx
+++ pages/dao/[symbol]/proposal/new.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useEffect, useLayoutEffect, useState } from 'react'
 import * as yup from 'yup'
 import { ArrowLeftIcon, PlusCircleIcon, XCircleIcon } from '@heroicons/react/outline'
 import { getInstructionDataFromBase64, Governance, GovernanceAccountType, ProgramAccount, RpcContext } from '@solana/spl-governance'
@@ -67,6 +67,7 @@ function extractGovernanceAccountFromInstructionsData(instructionsData: Componen
 }
 
 const New = () => {
+	const _prepopulateForDemos = false;
 	const router = useRouter()
 	const client = useVoteStakeRegistryClientStore((s) => s.state.client)
 	const { fmtUrlWithCluster } = useQueryContext()
@@ -638,7 +639,165 @@ const New = () => {
 				],
 			},
 		*/
-	}, [propertyData])
+	}, [propertyData]);
+
+	useLayoutEffect(() => {
+		if (_prepopulateForDemos) {
+			setMetaplexDataObj({
+				name: 'White House',
+				symbol: 'tokr_',
+				description: 'The White House is the official residence and workplace of the president of the United States. It is located at 1600 Pennsylvania Avenue NW in Washington, D.C., and has been the residence of every U.S. president since John Adams in 1800. The term "White House" is often used as a metonym for the president and his advisers.',
+				image: 'https://ipfs.io/ipfs/QmPZR8h2CUY8CZW77oLDQ51ctPqZEbZXKVaC5xYQgPPbzX?filename=white-house_photo.png',
+				attributes: [
+					{
+						trait_type: 'name',
+						value: 'White House',
+					},
+					{
+						trait_type: 'description',
+						value: 'The White House is the official residence and workplace of the president of the United States. It is located at 1600 Pennsylvania Avenue NW in Washington, D.C., and has been the residence of every U.S. president since John Adams in 1800. The term "White House" is often used as a metonym for the president and his advisers.',
+					},
+					{
+						trait_type: 'property_address',
+						value: '1600 Pennsylvania Avenue NW, Washington, DC 20500',
+					},
+					{
+						trait_type: 'lat_long',
+						value: '38.898819, -77.036690',
+					},
+					{
+						trait_type: 'acres',
+						value: '18',
+					},
+					{
+						trait_type: 'land_record_auditor',
+						value: 'lorem ipsum',
+					},
+					{
+						trait_type: 'deed_record_recorder',
+						value: 'lorem ipsum',
+					},
+					{
+						trait_type: 'mortgage_record_recorder',
+						value: 'lorem ipsum',
+					},
+					{
+						trait_type: 'legal_description',
+						value: 'Legal description of the White House pulled from official record',
+					},
+					{
+						trait_type: 'mortgage_record',
+						value: 'lorem ipsum',
+					},
+					{
+						trait_type: 'title_method',
+						value: 'Sole ownership',
+					},
+					{
+						trait_type: 'title_held_by',
+						value: 'The People',
+					},
+					{
+						trait_type: 'ein',
+						value: '123445555',
+					},
+					{
+						trait_type: 'transfer_restrictions',
+						value: 'None',
+					},
+					{
+						trait_type: 'marketing_name',
+						value: 'White House',
+					},
+					{
+						trait_type: 'type',
+						value: 'Building',
+					},
+					{
+						trait_type: 'sq_ft',
+						value: '54,900',
+					},
+					{
+						trait_type: 'property_description',
+						value: 'The White House is the official residence and workplace of the president of the United States. It is located at 1600 Pennsylvania Avenue NW in Washington, D.C., and has been the residence of every U.S. president since John Adams in 1800. The term "White House" is often used as a metonym for the president and his advisers.',
+					},
+					{
+						trait_type: 'deed',
+						value: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+					},
+					{
+						trait_type: 'mortgage',
+						value: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+					},
+					{
+						trait_type: 'title_insurance',
+						value: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+					},
+					{
+						trait_type: 'articles_of_organization',
+						value: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+					},
+					{
+						trait_type: 'certificate_of_organization_from_secretary_of_state',
+						value: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+					},
+					{
+						trait_type: 'operating_agreement',
+						value: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+					},
+					{
+						trait_type: 'membership_interest_transfer_agreement',
+						value: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+					},
+					{
+						trait_type: 'ein_letter_from_irs',
+						value: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+					},
+					{
+						trait_type: 'appraisal',
+						value: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+					},
+					{
+						trait_type: 'submitted_by',
+						value: 'John Adams',
+					},
+				],
+			});
+
+			setPropertyData({
+				name: 'White House',
+				description: 'The White House is the official residence and workplace of the president of the United States. It is located at 1600 Pennsylvania Avenue NW in Washington, D.C., and has been the residence of every U.S. president since John Adams in 1800. The term "White House" is often used as a metonym for the president and his advisers.',
+				property_address: '1600 Pennsylvania Avenue NW, Washington, DC 20500',
+				lat_long: '38.898819, -77.036690',
+				acres: '18',
+				land_record_auditor: 'lorem ipsum',
+				deed_record_recorder: 'lorem ipsum',
+				mortgage_record_recorder: 'lorem ipsum',
+				legal_description: 'Legal description of the White House pulled from official record',
+				mortgage_record: 'lorem ipsum',
+				title_method: 'Sole ownership',
+				title_held_by: 'The People',
+				ein: '123445555',
+				transfer_restrictions: 'None',
+				marketing_name: 'White House',
+				type: 'Building',
+				sq_ft: '59,900',
+				property_description: 'The White House is the official residence and workplace of the president of the United States. It is located at 1600 Pennsylvania Avenue NW in Washington, D.C., and has been the residence of every U.S. president since John Adams in 1800. The term "White House" is often used as a metonym for the president and his advisers.',
+				deed: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+				mortgage: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+				title_insurance: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+				articles_of_organization: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+				certificate_of_organization_from_secretary_of_state: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+				operating_agreement: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+				membership_interest_transfer_agreement: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+				ein_letter_from_irs: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+				appraisal: 'https://ipfs.io/ipfs/QmaHoFcUyHdd1ZE55gtqeJAKqif7k53sLusyaaPzXA6n8N?filename=tokr_screenshot_1.png',
+				submitted_by: 'John Adams',
+				image: 'https://ipfs.io/ipfs/QmPZR8h2CUY8CZW77oLDQ51ctPqZEbZXKVaC5xYQgPPbzX?filename=white-house_photo.png',
+				uri: '',
+			})
+		}
+	}, [_prepopulateForDemos])
 
 	return (
 		<div>
