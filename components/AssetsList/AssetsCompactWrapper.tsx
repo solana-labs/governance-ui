a2d4fe6a2fc3c6ae4deb3d75b262c96c7e7b4764
saml33 <30796577+saml33@users.noreply.github.com>
2022-03-17 03:25:27 +1100

    improve dao page layout (#454)

--- components/AssetsList/AssetsCompactWrapper.tsx
+++ components/AssetsList/AssetsCompactWrapper.tsx
@@ -3,11 +3,9 @@ import AssetsList from './AssetsList'
 import { ChevronRightIcon } from '@heroicons/react/solid'
 import useRealm from '@hooks/useRealm'
 import useQueryContext from '@hooks/useQueryContext'
-import { useRouter } from 'next/router'
-import { LinkButton } from '@components/Button'
+import Link from 'next/link'
 
 const AssetsCompactWrapper = () => {
-  const router = useRouter()
   const { symbol } = useRealm()
   const { fmtUrlWithCluster } = useQueryContext()
 
@@ -15,16 +13,14 @@ const AssetsCompactWrapper = () => {
     <div className="bg-bkg-2 p-4 md:p-6 rounded-lg">
       <div className="flex items-center justify-between pb-4">
         <h3 className="mb-0">Assets</h3>
-        <LinkButton
-          className={`flex items-center text-primary-light`}
-          onClick={() => {
-            const url = fmtUrlWithCluster(`/dao/${symbol}/assets`)
-            router.push(url)
-          }}
-        >
-          View
-          <ChevronRightIcon className="flex-shrink-0 h-6 w-6" />
-        </LinkButton>
+        <Link href={fmtUrlWithCluster(`/dao/${symbol}/assets`)}>
+          <a
+            className={`default-transition flex items-center text-fgd-2 text-sm transition-all hover:text-fgd-3`}
+          >
+            View
+            <ChevronRightIcon className="flex-shrink-0 h-6 w-6" />
+          </a>
+        </Link>
       </div>
       <div className="overflow-y-auto" style={{ maxHeight: '350px' }}>
         <AssetsList panelView />
