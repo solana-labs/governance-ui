c826f4c0569f7388b029883f3d5471be592b51b8
Kevin Mack <kmack418@gmail.com>
2022-03-16 21:00:42 -0400

    LEgal text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -37,6 +37,9 @@ const Footer = () => {
 					</li>
 				</ul>
 			</div>
+			<div className="container mx-auto text-xs my-6">
+				<p className="text-legal text-justify">The Current Owner, Tokr DAO, and any Tokr affiliates or contributors to the open source software and systems involved in the Tokr Protocol and the minting of this rNFT hereby disclaim any representation or warranty relating to the sufficiency or adequacy of the title to the real estate owned by the entity specified in the rNFT metadata, and, by purchasing this rNFT, you hereby acknowledge that you are not relying on any such representations or warranties. Linked in the metadata is a copy of the Owner's Title Insurance Policy that was obtained at the time of acquisition (or subsequently as amended in the metadata, if applicable). The metadata and documentation submitted as part of rNFT certification and verification process is intended to make data collection easier to assist you in conducting your own due diligence. It is strongly encouraged that you conduct your own research and additional due diligence as it relates to the sufficiency and adequacy of the title to such real estate prior to acquiring this rNFT, which may include obtaining a title insurance policy. Any validations or certifications made by the Current Owner or Tokr DAO and any affiliates or contributors relating to the rNFT do not relate to the title of such real estate.</p>
+			</div>
 		</div>
 	)
 }
