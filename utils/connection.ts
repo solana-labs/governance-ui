84fc9f1ac432d7afa6ad98c4dc54e7aee78657ae
gregm711 <miller.7285osu@gmail.com>
2022-03-17 19:31:08 -0400

    Connection for mainnet

--- utils/connection.ts
+++ utils/connection.ts
@@ -3,36 +3,34 @@ import { Connection } from '@solana/web3.js'
 import type { EndpointInfo } from '../@types/types'
 
 const ENDPOINTS: EndpointInfo[] = [
-//   {
-//     name: 'mainnet',
-//     url: process.env.MAINNET_RPC || 'https://ssc-dao.genesysgo.net/',
-//   },
-  {
-    name: 'devnet',
-    url:
-      process.env.DEVNET_RPC ||
-      'https://psytrbhymqlkfrhudd.dev.genesysgo.net:8899',
-  }
-//   ,
-//   {
-//     name: 'localnet',
-//     url: 'http://127.0.0.1:8899',
-//   },
+	{
+		name: 'mainnet',
+		url: process.env.MAINNET_RPC || 'https://ssc-dao.genesysgo.net/',
+	},
+	{
+		name: 'devnet',
+		url: process.env.DEVNET_RPC || 'https://psytrbhymqlkfrhudd.dev.genesysgo.net:8899',
+	},
+	//   ,
+	//   {
+	//     name: 'localnet',
+	//     url: 'http://127.0.0.1:8899',
+	//   },
 ]
 
 console.log('deployed ENDPOINTS:', ENDPOINTS)
 
 export interface ConnectionContext {
-  cluster: EndpointTypes
-  current: Connection
-  endpoint: string
+	cluster: EndpointTypes
+	current: Connection
+	endpoint: string
 }
 
 export function getConnectionContext(cluster: string): ConnectionContext {
-  const ENDPOINT = ENDPOINTS.find((e) => e.name === cluster) || ENDPOINTS[0]
-  return {
-    cluster: ENDPOINT!.name as EndpointTypes,
-    current: new Connection(ENDPOINT!.url, 'recent'),
-    endpoint: ENDPOINT!.url,
-  }
+	const ENDPOINT = ENDPOINTS.find((e) => e.name === cluster) || ENDPOINTS[0]
+	return {
+		cluster: ENDPOINT!.name as EndpointTypes,
+		current: new Connection(ENDPOINT!.url, 'recent'),
+		endpoint: ENDPOINT!.url,
+	}
 }
