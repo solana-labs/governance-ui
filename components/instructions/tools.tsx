0ff9b477f02cfbd36d300027eea52c1d5f38e5c3
Kevin Mack <kmack418@gmail.com>
2022-03-17 12:30:32 -0400

    Updated DAOs for demo

--- components/instructions/tools.tsx
+++ components/instructions/tools.tsx
@@ -29,7 +29,7 @@ export const DEFAULT_GOVERNANCE_PROGRAM_ID = 'A46hUedKdttuKKU3bCA4c1pX47X7TjVGDU
 // export const DEFAULT_TEST_GOVERNANCE_PROGRAM_ID = 'ToKriDd3YyUxPjbDamXpAZKQMAFMGjhzbgGiGjGbCxt' // MAINNET+DEVNET (new)
 export const DEFAULT_TEST_GOVERNANCE_PROGRAM_ID = 'A46hUedKdttuKKU3bCA4c1pX47X7TjVGDUH7gJUhoPRZ' // scrappy DEVNET
 
-export const TOKR_DAO = '4YWe67bC9B4YWy6MCqMDBSe2xekFGSMVeqEzCx3Ziebd' // Kmack Test DAO 1
+export const TOKR_DAO = 'ALkNW35wP58efK1k4xpaa9GdbZJSuFraBJqjjzNqoncr'
 
 // Well known account names displayed on the instruction card
 export const ACCOUNT_NAMES = {
