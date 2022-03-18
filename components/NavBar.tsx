e0bfb63ff082e0fe90e4dfeb7213d6afc2a8aedc
Kevin Mack <kmack418@gmail.com>
2022-03-18 10:14:52 -0400

    Remove the logic to hide Wallet Connect

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,23 +9,28 @@ const NavBar = props => {
 
 	return (
 		<>
-		{ !props.web3 && <>
-			<a href="https://phantom.app/download" target="_blank" className="bg-green text-dark block w-full text-center py-2 px-16">
-				<span className="text-black">tokr_ Realms</span> requires a Web3 crypto wallet
-			</a>
-		</> }
-		<div className="grid grid-cols-12 mb-3 border-b border-green">
-			<div className="col-span-12 xl:col-start-2 xl:col-span-10 flex h-20 items-center justify-between px-4 md:px-8 xl:px-4">
-				<a href="/realms" onClick={e => {
-					router.push(fmtUrlWithCluster(`/realms`))
-					e.preventDefault();
-				}}>
-					<span className="cursor-pointer flex items-center">
-						<h1 className="text-lg">tokr_</h1>
-					</span>
-				</a>
-				<ul className="flex ite ms-center space-x-4">
-					{/*
+			{!props.web3 && (
+				<>
+					<a href="https://phantom.app/download" target="_blank" className="bg-green text-dark block w-full text-center py-2 px-16">
+						<span className="text-black">tokr_ Realms</span> requires a Web3 crypto wallet
+					</a>
+				</>
+			)}
+			<div className="grid grid-cols-12 mb-3 border-b border-green">
+				<div className="col-span-12 xl:col-start-2 xl:col-span-10 flex h-20 items-center justify-between px-4 md:px-8 xl:px-4">
+					<a
+						href="/realms"
+						onClick={(e) => {
+							router.push(fmtUrlWithCluster(`/realms`))
+							e.preventDefault()
+						}}
+					>
+						<span className="cursor-pointer flex items-center">
+							<h1 className="text-lg">tokr_</h1>
+						</span>
+					</a>
+					<ul className="flex ite ms-center space-x-4">
+						{/*
 					 <li className="flex items-center">
 						<Link href="#">
 							<span className="nav-link uppercase flex items-center">
@@ -34,39 +39,48 @@ const NavBar = props => {
 						</Link>
 					</li>
 					*/}
-					{ props.web3 === true && <li className="flex items-center">
-						<a href="/realms/new" onClick={e => {
-							router.push(fmtUrlWithCluster(`/realms/new`))
-							e.preventDefault();
-						}}>
-							<span className="nav-link flex items-center">
-								<span>Create DAO</span>
-							</span>
-						</a>
-					</li> }
-					<li className="flex items-center">
-						<a href="/realms" onClick={e => {
-							router.push(fmtUrlWithCluster(`/realms`))
-							e.preventDefault();
-						}}>
-							<span className="nav-link flex items-center">
-								<span>DAOs</span>
-							</span>
-						</a>
-					</li>
-					<li className="flex items-center">
-						<a href="https://tokr.gitbook.io/tokr-main-docs/" target="_blank">
-							<span className="nav-link uppercase flex items-center">
-								<span>Docs</span>
-							</span>
-						</a>
-					</li>
-					{ props.web3 === true && <li className="pl-4 flex items-center">
-						<ConnectWalletButtonAlternative />
-					</li> }
-				</ul>
+						{props.web3 === true && (
+							<li className="flex items-center">
+								<a
+									href="/realms/new"
+									onClick={(e) => {
+										router.push(fmtUrlWithCluster(`/realms/new`))
+										e.preventDefault()
+									}}
+								>
+									<span className="nav-link flex items-center">
+										<span>Create DAO</span>
+									</span>
+								</a>
+							</li>
+						)}
+						<li className="flex items-center">
+							<a
+								href="/realms"
+								onClick={(e) => {
+									router.push(fmtUrlWithCluster(`/realms`))
+									e.preventDefault()
+								}}
+							>
+								<span className="nav-link flex items-center">
+									<span>DAOs</span>
+								</span>
+							</a>
+						</li>
+						<li className="flex items-center">
+							<a href="https://tokr.gitbook.io/tokr-main-docs/" target="_blank">
+								<span className="nav-link uppercase flex items-center">
+									<span>Docs</span>
+								</span>
+							</a>
+						</li>
+
+						<li className="pl-4 flex items-center">
+							<ConnectWalletButtonAlternative />
+						</li>
+					</ul>
+				</div>
 			</div>
-		</div>
 		</>
 	)
 }
