3c6f1b20f9d3f21b61ce8fb2a12a6835a0f54d31
Jason Chitla <jasonchitla@hotmail.com>
2022-03-18 22:58:06 -0400

    fix validation

--- utils/validations.tsx
+++ utils/validations.tsx
@@ -224,26 +224,10 @@ export const getFriktionDepositSchema = ({ form }) => {
   })
 }
 
-export const getFriktionWithdrawSchema = ({ form }) => {
+export const getFriktionWithdrawSchema = () => {
   return yup.object().shape({
     governedTokenAccount: yup.object().required('Source account is required'),
-    amount: yup
-      .number()
-      .typeError('Amount is required')
-      .test(
-        'amount',
-        'Transfer amount must be less than the source account available amount',
-        async function (val: number) {
-          if (val && !form.governedTokenAccount) {
-            return this.createError({
-              message: `Please select source account to validate the amount`,
-            })
-          }
-          return this.createError({
-            message: `Amount is required`,
-          })
-        }
-      ),
+    amount: yup.number().typeError('Amount is required'),
   })
 }
 
