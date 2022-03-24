6f8d58cbb0ebcf9483154229874645eb4cc1afde
saml33 <30796577+saml33@users.noreply.github.com>
2022-03-24 20:04:23 +1100

    add empty states to treasury and assets panels (#474)

--- components/AssetsList/AssetsCompactWrapper.tsx
+++ components/AssetsList/AssetsCompactWrapper.tsx
@@ -3,28 +3,78 @@ import AssetsList from './AssetsList'
 import { ChevronRightIcon } from '@heroicons/react/solid'
 import useRealm from '@hooks/useRealm'
 import useQueryContext from '@hooks/useQueryContext'
+import useWalletStore from 'stores/useWalletStore'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { TerminalIcon } from '@heroicons/react/outline'
+import useGovernanceAssets from '@hooks/useGovernanceAssets'
+import { GovernanceAccountType } from '@solana/spl-governance'
+import EmptyState from '@components/EmptyState'
+import {
+  NEW_PROGRAM_VIEW,
+  renderAddNewAssetTooltip,
+} from 'pages/dao/[symbol]/assets'
 
 const AssetsCompactWrapper = () => {
-  const { symbol } = useRealm()
+  const router = useRouter()
   const { fmtUrlWithCluster } = useQueryContext()
+  const {
+    symbol,
+    realm,
+    ownVoterWeight,
+    toManyCommunityOutstandingProposalsForUser,
+    toManyCouncilOutstandingProposalsForUse,
+  } = useRealm()
+  const connected = useWalletStore((s) => s.connected)
+  const canCreateGovernance = realm
+    ? ownVoterWeight.canCreateGovernance(realm)
+    : null
+
+  const newAssetToolTip = renderAddNewAssetTooltip(
+    connected,
+    canCreateGovernance,
+    toManyCommunityOutstandingProposalsForUser,
+    toManyCouncilOutstandingProposalsForUse
+  )
+
+  const goToNewAssetForm = () => {
+    router.push(fmtUrlWithCluster(`/dao/${symbol}${NEW_PROGRAM_VIEW}`))
+  }
+  const { getGovernancesByAccountTypes } = useGovernanceAssets()
+  const programGovernances = getGovernancesByAccountTypes([
+    GovernanceAccountType.ProgramGovernanceV1,
+    GovernanceAccountType.ProgramGovernanceV2,
+  ])
 
   return (
     <div className="bg-bkg-2 p-4 md:p-6 rounded-lg">
       <div className="flex items-center justify-between pb-4">
-        <h3 className="mb-0">Assets</h3>
-        <Link href={fmtUrlWithCluster(`/dao/${symbol}/assets`)}>
-          <a
-            className={`default-transition flex items-center text-fgd-2 text-sm transition-all hover:text-fgd-3`}
-          >
-            View
-            <ChevronRightIcon className="flex-shrink-0 h-6 w-6" />
-          </a>
-        </Link>
+        <h3 className="mb-0">Programs</h3>
+        {programGovernances.length > 0 ? (
+          <Link href={fmtUrlWithCluster(`/dao/${symbol}/assets`)}>
+            <a
+              className={`default-transition flex items-center text-fgd-2 text-sm transition-all hover:text-fgd-3`}
+            >
+              View
+              <ChevronRightIcon className="flex-shrink-0 h-6 w-6" />
+            </a>
+          </Link>
+        ) : null}
       </div>
-      <div className="overflow-y-auto" style={{ maxHeight: '350px' }}>
-        <AssetsList panelView />
-      </div>
+      {programGovernances.length > 0 ? (
+        <div className="overflow-y-auto" style={{ maxHeight: '350px' }}>
+          <AssetsList panelView />
+        </div>
+      ) : (
+        <EmptyState
+          desc="No programs found"
+          disableButton={!!newAssetToolTip}
+          buttonText="New Program"
+          icon={<TerminalIcon />}
+          onClickButton={goToNewAssetForm}
+          toolTipContent={newAssetToolTip}
+        />
+      )}
     </div>
   )
 }
--- pages/dao/[symbol]/assets/index.tsx
+++ pages/dao/[symbol]/assets/index.tsx
@@ -8,7 +8,7 @@ import useWalletStore from 'stores/useWalletStore'
 import PreviousRouteBtn from '@components/PreviousRouteBtn'
 import { LinkButton } from '@components/Button'
 import { PlusCircleIcon } from '@heroicons/react/outline'
-const NEW_PROGRAM_VIEW = `/program/new`
+export const NEW_PROGRAM_VIEW = `/program/new`
 
 const Assets = () => {
   const router = useRouter()
@@ -28,15 +28,22 @@ const Assets = () => {
     ? ownVoterWeight.canCreateGovernance(realm)
     : null
 
-  const addNewAssetTooltip = !connected
-    ? 'Connect your wallet to create new asset'
-    : !canCreateGovernance
-    ? "You don't have enough governance power to create a new asset"
-    : toManyCommunityOutstandingProposalsForUser
-    ? 'You have too many community outstanding proposals. You need to finalize them before creating a new asset.'
-    : toManyCouncilOutstandingProposalsForUse
-    ? 'You have too many council outstanding proposals. You need to finalize them before creating a new asset.'
-    : ''
+  const newAssetToolTip = renderAddNewAssetTooltip(
+    connected,
+    canCreateGovernance,
+    toManyCommunityOutstandingProposalsForUser,
+    toManyCouncilOutstandingProposalsForUse
+  )
+
+  // const addNewAssetTooltip = !connected
+  //   ? 'Connect your wallet to create new asset'
+  //   : !canCreateGovernance
+  //   ? "You don't have enough governance power to create a new asset"
+  //   : toManyCommunityOutstandingProposalsForUser
+  //   ? 'You have too many community outstanding proposals. You need to finalize them before creating a new asset.'
+  //   : toManyCouncilOutstandingProposalsForUse
+  //   ? 'You have too many council outstanding proposals. You need to finalize them before creating a new asset.'
+  //   : ''
   return (
     <div className="bg-bkg-2 rounded-lg p-4 md:p-6">
       <div className="grid grid-cols-12 gap-6">
@@ -45,12 +52,12 @@ const Assets = () => {
             <PreviousRouteBtn />
           </div>
           <div className="flex items-center justify-between mb-4">
-            <h1 className="mb-0">Assets</h1>
-            <Tooltip contentClassName="ml-auto" content={addNewAssetTooltip}>
+            <h1 className="mb-0">Programs</h1>
+            <Tooltip contentClassName="ml-auto" content={newAssetToolTip}>
               <LinkButton
                 onClick={goToNewAssetForm}
                 className={`flex items-center text-primary-light ${
-                  addNewAssetTooltip
+                  newAssetToolTip
                     ? 'cursor-not-allowed pointer-events-none opacity-60'
                     : 'cursor-pointer'
                 }`}
@@ -68,3 +75,20 @@ const Assets = () => {
 }
 
 export default Assets
+
+export const renderAddNewAssetTooltip = (
+  connected,
+  canCreateGovernance,
+  toManyCommunityOutstandingProposalsForUser,
+  toManyCouncilOutstandingProposalsForUse
+) => {
+  return !connected
+    ? 'Connect your wallet to create new asset'
+    : !canCreateGovernance
+    ? "You don't have enough governance power to create a new asset"
+    : toManyCommunityOutstandingProposalsForUser
+    ? 'You have too many community outstanding proposals. You need to finalize them before creating a new asset.'
+    : toManyCouncilOutstandingProposalsForUse
+    ? 'You have too many council outstanding proposals. You need to finalize them before creating a new asset.'
+    : ''
+}
