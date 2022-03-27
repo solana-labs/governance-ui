b2b7dd84b215b16f155e15209fe15ef59ce4463e
Masaya Funakoshi <masayafunakoshi1@gmail.com>
2022-03-26 21:36:08 -0400

    added components for each tab

---
+++ pages/dao/[symbol]/params/components/StatsView.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { capitalize } from '@utils/helpers'
+import { tryParsePublicKey } from '@tools/core/pubkey'
+import { getAccountName } from '@components/instructions/tools'
+
+const StatsView = () => {
+  return <div>StatsView</div>
+}
+
+const DisplayField = ({ label, val, padding = false, bg = false }) => {
+  const pubkey = tryParsePublicKey(val)
+  const name = pubkey ? getAccountName(pubkey) : ''
+  return (
+    <div
+      className={`flex flex-col mb-2 ${bg ? 'bg-bkg-1' : ''} ${
+        padding ? 'py-1' : ''
+      }`}
+    >
+      <div className="text-xs text-fgd-3">{capitalize(label)}</div>
+      <div className="text-sm break-all">
+        {pubkey && name ? (
+          <>
+            <div className="text-xs">{name}</div>
+            <div>{val}</div>
+          </>
+        ) : (
+          <div>{val}</div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default StatsView
