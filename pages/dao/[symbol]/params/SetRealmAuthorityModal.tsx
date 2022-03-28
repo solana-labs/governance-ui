68067923dd1e2314b94e5c8a1582543178b2baf5
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-03-28 21:07:10 +0200

    use 2% of mint supply instand of 10000 tokens when creating gov

--- pages/dao/[symbol]/params/SetRealmAuthorityModal.tsx
+++ pages/dao/[symbol]/params/SetRealmAuthorityModal.tsx
@@ -3,6 +3,15 @@ import { useState } from 'react'
 import Button from '@components/Button'
 import useGovernedMultiTypeAccounts from '@hooks/useGovernedMultiTypeAccounts'
 import GovernedAccountSelect from '../proposal/components/GovernedAccountSelect'
+import {
+  SetRealmAuthorityAction,
+  withSetRealmAuthority,
+} from '@solana/spl-governance'
+import useRealm from '@hooks/useRealm'
+import useWalletStore from 'stores/useWalletStore'
+import { Transaction, TransactionInstruction } from '@solana/web3.js'
+import { sendTransaction } from '@utils/send'
+import { GovernedMultiTypeAccount } from '@utils/tokens'
 
 const SetRealmAuthorityModal = ({
   closeModal,
@@ -11,12 +20,38 @@ const SetRealmAuthorityModal = ({
   closeModal: () => void
   isOpen: boolean
 }) => {
+  const { realmInfo, realm } = useRealm()
+  const wallet = useWalletStore((s) => s.current)
+  const connection = useWalletStore((s) => s.connection)
+  const { fetchRealm } = useWalletStore((s) => s.actions)
   const { governedMultiTypeAccounts } = useGovernedMultiTypeAccounts()
-  const [governance, setGovernnace] = useState(null)
+  const [account, setAccount] = useState<GovernedMultiTypeAccount | null>(null)
   const [settingAuthority, setSettingAuthority] = useState(false)
-  const handleSetAuthority = () => {
+  const handleSetAuthority = async () => {
     setSettingAuthority(true)
+    const instructions: TransactionInstruction[] = []
+    withSetRealmAuthority(
+      instructions,
+      realmInfo!.programId,
+      realmInfo!.programVersion!,
+      realm!.pubkey!,
+      wallet!.publicKey!,
+      account!.governance.pubkey,
+      SetRealmAuthorityAction.SetChecked
+    )
+    const transaction = new Transaction({ feePayer: wallet!.publicKey })
+    transaction.add(...instructions)
+    await sendTransaction({
+      transaction: transaction,
+      wallet,
+      connection: connection.current,
+      signers: [],
+      sendingMessage: `Setting authority`,
+      successMessage: `Authority set`,
+    })
+    await fetchRealm(realmInfo!.programId, realmInfo!.realmId)
     setSettingAuthority(false)
+    closeModal()
   }
   return (
     <Modal sizeClassName="sm:max-w-3xl" onClose={closeModal} isOpen={isOpen}>
@@ -28,16 +63,15 @@ const SetRealmAuthorityModal = ({
           label="Governance"
           governedAccounts={governedMultiTypeAccounts}
           onChange={(value) => {
-            setGovernnace(value)
+            setAccount(value)
           }}
-          value={governance}
-          governance={governance}
+          value={account}
         />
       </div>
       <div className="border-t border-fgd-4 flex justify-end mt-6 pt-6 space-x-4">
         <Button
           isLoading={settingAuthority}
-          disabled={settingAuthority || !governance}
+          disabled={settingAuthority || !account}
           onClick={() => handleSetAuthority()}
         >
           Set authority
