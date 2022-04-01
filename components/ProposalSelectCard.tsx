fc5f17dbfdce34bec48da82a972a85fc77e16122
saml33 <slam.uke@gmail.com>
2022-04-01 13:36:34 +1100

    add multi-vote mode ui

---
+++ components/ProposalSelectCard.tsx
@@ -0,0 +1,88 @@
+import { CheckIcon } from '@heroicons/react/solid'
+import ProposalStateBadge from './ProposalStatusBadge'
+import { Proposal, ProposalState } from '@solana/spl-governance'
+import ApprovalQuorum from './ApprovalQuorum'
+import useProposalVotes from '../hooks/useProposalVotes'
+import ProposalTimeStatus from './ProposalTimeStatus'
+import { PublicKey } from '@solana/web3.js'
+import VoteResults from './VoteResults'
+
+type ProposalCardProps = {
+  proposalPk: PublicKey
+  proposal: Proposal
+  setSelectedProposals: any
+  selectedProposals: Proposal[]
+}
+
+const ProposalSelectCard = ({
+  proposalPk,
+  proposal,
+  setSelectedProposals,
+  selectedProposals,
+}: ProposalCardProps) => {
+  const { yesVoteProgress, yesVotesRequired } = useProposalVotes(proposal)
+
+  const checked = !!selectedProposals.find(
+    // @ts-ignore
+    (p) => p.proposalPk.toString() === proposalPk.toString()
+  )
+
+  const toggleCheckbox = () => {
+    if (checked) {
+      const proposals = selectedProposals.filter(
+        // @ts-ignore
+        (p) => p.proposalPk.toString() !== proposalPk.toString()
+      )
+      setSelectedProposals(proposals)
+    } else {
+      setSelectedProposals([...selectedProposals, { proposal, proposalPk }])
+    }
+  }
+
+  return (
+    <button
+      className={`border ${
+        checked ? 'border-primary-light' : 'border-fgd-4'
+      } default-transition rounded-lg text-left w-full hover:bg-bkg-3`}
+      onClick={() => toggleCheckbox()}
+    >
+      <div className="p-4">
+        <div className="flex items-start justify-between">
+          <h3 className="text-fgd-1">{proposal.name}</h3>
+          <div className="flex items-center pl-4 pt-1">
+            <ProposalStateBadge
+              proposalPk={proposalPk}
+              proposal={proposal}
+              open={false}
+            />
+            <div
+              className={`bg-bkg-1 border ${
+                checked ? 'border-primary-light' : 'border-fgd-4'
+              } flex items-center justify-center ml-3 h-6 rounded-md w-6`}
+            >
+              {checked ? (
+                <CheckIcon className="h-5 text-primary-light w-5" />
+              ) : null}
+            </div>
+          </div>
+        </div>
+        <ProposalTimeStatus proposal={proposal} />
+      </div>
+      {proposal.state === ProposalState.Voting && (
+        <div className="border-t border-fgd-4 flex flex-col lg:flex-row mt-2 p-4">
+          <div className="pb-3 lg:pb-0 lg:border-r lg:border-fgd-4 lg:pr-4 w-full lg:w-1/2">
+            <VoteResults isListView proposal={proposal} />
+          </div>
+          <div className="lg:pl-4 w-full lg:w-1/2">
+            <ApprovalQuorum
+              progress={yesVoteProgress}
+              yesVotesRequired={yesVotesRequired}
+            />
+          </div>
+        </div>
+      )}
+    </button>
+  )
+}
+
+export default ProposalSelectCard
--- pages/dao/[symbol]/index.tsx
+++ pages/dao/[symbol]/index.tsx
@@ -1,5 +1,5 @@
 import useRealm from 'hooks/useRealm'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import ProposalFilter from 'components/ProposalFilter'
 import {
   Governance,
@@ -7,16 +7,20 @@ import {
   Proposal,
   ProposalState,
 } from '@solana/spl-governance'
+import useWalletStore from 'stores/useWalletStore'
 import NewProposalBtn from './proposal/components/NewProposalBtn'
 import { PublicKey } from '@solana/web3.js'
 import TokenBalanceCardWrapper from '@components/TokenBalance/TokenBalanceCardWrapper'
-import ApproveAllBtn from './proposal/components/ApproveAllBtn'
 import dynamic from 'next/dynamic'
 import PaginationComponent from '@components/Pagination'
 import Tabs from '@components/Tabs'
 import AboutRealm from '@components/AboutRealm'
 import Input from '@components/inputs/Input'
 import { SearchIcon } from '@heroicons/react/outline'
+import Switch from '@components/Switch'
+import ProposalSelectCard from '@components/ProposalSelectCard'
+import Checkbox from '@components/inputs/Checkbox'
+import Button from '@components/Button'
 const AccountsCompactWrapper = dynamic(
   () => import('@components/TreasuryAccount/AccountsCompactWrapper')
 )
@@ -91,6 +95,12 @@ const REALM = () => {
   const [proposalSearch, setProposalSearch] = useState('')
   const [filteredProposals, setFilteredProposals] = useState(displayedProposals)
   const [activeTab, setActiveTab] = useState('Proposals')
+  const [multiVoteMode, setMultiVoteMode] = useState(false)
+  const [selectedProposals, setSelectedProposals] = useState<any[]>([])
+  const ownVoteRecordsByProposal = useWalletStore(
+    (s) => s.ownVoteRecordsByProposal
+  )
+  const connected = useWalletStore((s) => s.connected)
 
   const allProposals = Object.entries(proposals).sort((a, b) =>
     compareProposals(b[1].account, a[1].account, governances)
@@ -134,8 +144,84 @@ const REALM = () => {
     )
   }
 
+  const toggleMultiVoteMode = () => {
+    setMultiVoteMode(!multiVoteMode)
+  }
+
+  const votingProposals = useMemo(
+    () =>
+      allProposals.filter(
+        ([k, v]) =>
+          v.account.state === ProposalState.Voting &&
+          !ownVoteRecordsByProposal[k]
+      ),
+    [allProposals]
+  )
+
+  useEffect(() => {
+    if (multiVoteMode) {
+      setFilteredProposals(votingProposals)
+    } else {
+      const proposals =
+        filters.length > 0
+          ? allProposals.filter(([, v]) => !filters.includes(v.account.state))
+          : allProposals
+      setFilteredProposals(proposals)
+    }
+  }, [multiVoteMode])
+
+  const allVotingProposalsSelected =
+    selectedProposals.length === votingProposals.length
+
+  const toggleSelectAll = () => {
+    if (allVotingProposalsSelected) {
+      setSelectedProposals([])
+    } else {
+      setSelectedProposals(
+        votingProposals.map(([k, v]) => ({
+          proposal: v.account,
+          proposalPk: new PublicKey(k),
+        }))
+      )
+    }
+  }
+
   return (
     <>
+      <div
+        className={`bottom-0 bg-bkg-3 flex flex-col justify-center fixed h-24 px-4 md:px-6 lg:px-8 transform transition-all duration-300 ease-in-out w-full left-1/2 -translate-x-1/2 z-10 ${
+          multiVoteMode ? 'translate-y-0' : 'translate-y-full'
+        }`}
+      >
+        <div className="flex items-center justify-between">
+          <div>
+            <h4 className="mb-2">
+              {`${selectedProposals.length} Proposal${
+                selectedProposals.length === 1 ? '' : 's'
+              } Selected`}
+            </h4>
+            <Checkbox
+              checked={allVotingProposalsSelected}
+              label={allVotingProposalsSelected ? 'Deselect All' : 'Select All'}
+              onChange={() => toggleSelectAll()}
+            />
+          </div>
+          <div className="flex items-center space-x-3">
+            <Button
+              className="whitespace-nowrap"
+              disabled={selectedProposals.length === 0}
+            >
+              Vote Yes
+            </Button>
+            <Button
+              className="whitespace-nowrap"
+              disabled={selectedProposals.length === 0}
+            >
+              Vote No
+            </Button>
+          </div>
+        </div>
+      </div>
       <div className="grid grid-cols-12 gap-4">
         {realm ? (
           <>
@@ -174,45 +260,70 @@ const REALM = () => {
                 />
                 {activeTab === 'Proposals' && (
                   <>
-                    <div className="flex flex-col-reverse lg:flex-row lg:items-center lg:justify-between pb-3 lg:space-x-4">
-                      <div className="flex items-center justify-between space-x-3 w-full">
-                        <h4 className="font-normal mb-0 text-fgd-2">{`${
-                          filteredProposals.length
-                        } Proposal${
-                          filteredProposals.length === 1 ? '' : 's'
-                        }`}</h4>
-                        <div className="flex space-x-4">
-                          <ApproveAllBtn />
-                          <NewProposalBtn />
-                        </div>
-                      </div>
-                      <div className="flex items-center pb-4 lg:pb-0 space-x-3">
+                    <div className="pb-3">
+                      <div className="flex items-center pb-4 space-x-3">
                         <Input
-                          className="pl-8 w-full lg:w-44"
+                          className="pl-8 w-full"
                           type="text"
                           placeholder="Search Proposals"
                           value={proposalSearch}
+                          noMaxWidth
                           onChange={(e) => {
                             setProposalSearch(e.target.value)
                           }}
                           prefix={<SearchIcon className="h-5 w-5 text-fgd-3" />}
                         />
                         <ProposalFilter
+                          disabled={multiVoteMode}
                           filters={filters}
                           setFilters={setFilters}
                         />
+                      </div>
+                      <div className="flex flex-col-reverse lg:flex-row items-center justify-between lg:space-x-3 w-full">
+                        <h4 className="font-normal mb-0 text-fgd-2 lg:whitespace-nowrap">{`${
+                          filteredProposals.length
+                        } Proposal${
+                          filteredProposals.length === 1 ? '' : 's'
+                        }`}</h4>
+                        <div className="flex items-center justify-between lg:justify-end pb-3 lg:pb-0 lg:space-x-3 w-full">
+                          {realm.account.votingProposalCount > 1 &&
+                          connected ? (
+                            <div className="flex items-center">
+                              <p className="mb-0 mr-1 text-fgd-3">
+                                Multi-vote Mode
+                              </p>
+                              <Switch
+                                checked={multiVoteMode}
+                                onChange={() => {
+                                  toggleMultiVoteMode()
+                                }}
+                              />
+                            </div>
+                          ) : null}
+                          <NewProposalBtn />
+                        </div>
                       </div>
                     </div>
                     <div className="space-y-3">
                       {filteredProposals.length > 0 ? (
                         <>
-                          {paginatedProposals.map(([k, v]) => (
-                            <ProposalCard
-                              key={k}
-                              proposalPk={new PublicKey(k)}
-                              proposal={v.account}
-                            />
-                          ))}
+                          {paginatedProposals.map(([k, v]) =>
+                            multiVoteMode ? (
+                              <ProposalSelectCard
+                                key={k}
+                                proposalPk={new PublicKey(k)}
+                                proposal={v.account}
+                                selectedProposals={selectedProposals}
+                                setSelectedProposals={setSelectedProposals}
+                              />
+                            ) : (
+                              <ProposalCard
+                                key={k}
+                                proposalPk={new PublicKey(k)}
+                                proposal={v.account}
+                              />
+                            )
+                          )}
                           <PaginationComponent
                             ref={pagination}
                             totalPages={Math.ceil(
