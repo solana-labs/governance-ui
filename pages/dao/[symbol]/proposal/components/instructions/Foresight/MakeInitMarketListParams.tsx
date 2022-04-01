8c5e2019dacbfe18c7d06ce9d4566861aa0f8fd6
Kevin Heavey <kevinheavey123@gmail.com>
2022-04-01 20:34:34 +0100

    save unsaved file

--- pages/dao/[symbol]/proposal/components/instructions/Foresight/MakeInitMarketListParams.tsx
+++ pages/dao/[symbol]/proposal/components/instructions/Foresight/MakeInitMarketListParams.tsx
@@ -6,7 +6,7 @@ import * as yup from 'yup'
 import { isFormValid } from '@utils/formValidation'
 import {
   UiInstruction,
-  ForesightMakeInitMarketListParams,
+  ForesightHasMarketListId,
 } from '@utils/uiTypes/proposalCreationTypes'
 import { NewProposalContext } from '../../../new'
 import { Governance } from '@solana/spl-governance'
@@ -37,7 +37,7 @@ const MakeInitMarketListParams = ({
   )
   const shouldBeGoverned = index !== 0 && governance
   const programId: PublicKey | undefined = realmInfo?.programId
-  const [form, setForm] = useState<ForesightMakeInitMarketListParams>({
+  const [form, setForm] = useState<ForesightHasMarketListId>({
     governedAccount: filteredTokenAccounts[0],
     marketListId: '',
   })
