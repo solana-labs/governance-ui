1cbe4af4ab97c3c9499f63a1a2bbe03b91242e83
Kevin Heavey <kevinheavey123@gmail.com>
2022-04-03 19:17:19 +0100

    refactor init category

--- pages/dao/[symbol]/proposal/components/instructions/Foresight/MakeInitCategoryParams.tsx
+++ pages/dao/[symbol]/proposal/components/instructions/Foresight/MakeInitCategoryParams.tsx
@@ -1,26 +1,21 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import React, { useContext, useEffect, useState } from 'react'
-import useRealm from '@hooks/useRealm'
-import { PublicKey } from '@solana/web3.js'
+import React, { useState } from 'react'
 import * as yup from 'yup'
-import { isFormValid } from '@utils/formValidation'
-import {
-  UiInstruction,
-  ForesightHasCategoryId,
-} from '@utils/uiTypes/proposalCreationTypes'
-import { NewProposalContext } from '../../../new'
+import { ForesightHasCategoryId } from '@utils/uiTypes/proposalCreationTypes'
 import { Governance } from '@solana/spl-governance'
 import { ProgramAccount } from '@solana/spl-governance'
-import useWalletStore from 'stores/useWalletStore'
-import { serializeInstructionToBase64 } from '@solana/spl-governance'
-import Input from '@components/inputs/Input'
-import GovernedAccountSelect from '../../GovernedAccountSelect'
 import {
   governance as foresightGov,
-  consts as foresightConsts,
+  types as foresightTypes,
 } from '@foresight-tmp/foresight-sdk'
-import useGovernanceAssets from '@hooks/useGovernanceAssets'
-import { GovernedMultiTypeAccount } from '@utils/tokens'
+import {
+  commonAssets,
+  ForesightCategoryIdInput,
+  ForesightGovernedAccountSelect,
+  ForesightUseEffects,
+  getSchema,
+  makeHandleSetFormWithErrors,
+} from './utils'
 
 const MakeInitCategoryParams = ({
   index,
@@ -29,98 +24,63 @@ const MakeInitCategoryParams = ({
   index: number
   governance: ProgramAccount<Governance> | null
 }) => {
-  const wallet = useWalletStore((s) => s.current)
-  const { realmInfo } = useRealm()
-  const { governedTokenAccountsWithoutNfts } = useGovernanceAssets()
-  const filteredTokenAccounts = governedTokenAccountsWithoutNfts.filter((x) =>
-    x.transferAddress?.equals(foresightGov.DEVNET_TREASURY)
-  )
-  const shouldBeGoverned = index !== 0 && governance
-  const programId: PublicKey | undefined = realmInfo?.programId
+  const {
+    wallet,
+    filteredTokenAccounts,
+    formErrors,
+    setFormErrors,
+    handleSetInstructions,
+  } = commonAssets()
   const [form, setForm] = useState<ForesightHasCategoryId>({
     governedAccount: filteredTokenAccounts[0],
     categoryId: '',
   })
-  const [formErrors, setFormErrors] = useState({})
-  const { handleSetInstructions } = useContext(NewProposalContext)
-  const handleSetForm = ({ propertyName, value }) => {
-    setFormErrors({})
-    setForm({ ...form, [propertyName]: value })
-  }
-  const validateInstruction = async (): Promise<boolean> => {
-    const { isValid, validationErrors } = await isFormValid(schema, form)
-    setFormErrors(validationErrors)
-    return isValid
-  }
-  async function getInstruction(): Promise<UiInstruction> {
-    const isValid = await validateInstruction()
-    let serializedInstruction = ''
-    if (isValid && programId && wallet?.publicKey) {
-      const program = foresightGov.readonlyProgram(
-        new PublicKey(foresightConsts.DEVNET_PID)
-      )
-      const { ix: initCategoryIx } = await foresightGov.genInitCategoryIx(
-        Buffer.from(form.categoryId.padEnd(20)),
-        program,
-        form.governedAccount.transferAddress!
-      )
-
-      serializedInstruction = serializeInstructionToBase64(initCategoryIx)
-    }
-    const obj: UiInstruction = {
-      serializedInstruction: serializedInstruction,
-      isValid,
-      governance: form.governedAccount.governance,
-    }
-    return obj
-  }
-  useEffect(() => {
-    handleSetForm({
-      propertyName: 'programId',
-      value: programId?.toString(),
-    })
-  }, [realmInfo?.programId])
-
-  useEffect(() => {
-    handleSetInstructions(
-      { governedAccount: form.governedAccount?.governance, getInstruction },
-      index
-    )
-  }, [form])
-  const schema = yup.object().shape({
-    governedAccount: yup
-      .object()
-      .nullable()
-      .required('Program governed account is required'),
+  const handleSetForm = makeHandleSetFormWithErrors(
+    form,
+    setForm,
+    setFormErrors
+  )
+  const schema = getSchema({
     categoryId: yup.string().required(),
   })
+  async function ixCreator(
+    form: ForesightHasCategoryId,
+    program: foresightTypes.PredictionMarketProgram
+  ) {
+    const { ix } = await foresightGov.genInitCategoryIx(
+      Buffer.from(form.categoryId.padEnd(20)),
+      program,
+      form.governedAccount.transferAddress!
+    )
+    return ix
+  }
+
+  const inputProps = {
+    form,
+    handleSetForm,
+    formErrors,
+  }
+  ForesightUseEffects(
+    handleSetForm,
+    form,
+    handleSetInstructions,
+    ixCreator,
+    wallet,
+    schema,
+    setFormErrors,
+    index
+  )
 
   return (
     <>
-      <GovernedAccountSelect
-        label="Program"
-        governedAccounts={filteredTokenAccounts as GovernedMultiTypeAccount[]}
-        onChange={(value) => {
-          handleSetForm({ value, propertyName: 'governedAccount' })
-        }}
-        value={form.governedAccount}
-        error={formErrors['governedAccount']}
-        shouldBeGoverned={shouldBeGoverned}
+      <ForesightGovernedAccountSelect
+        filteredTokenAccounts={filteredTokenAccounts}
+        form={form}
+        handleSetForm={handleSetForm}
+        index={index}
         governance={governance}
-      ></GovernedAccountSelect>
-      <Input
-        label="Category ID"
-        value={form.categoryId}
-        type="text"
-        min={0}
-        onChange={(evt) =>
-          handleSetForm({
-            value: evt.target.value,
-            propertyName: 'categoryId',
-          })
-        }
-        error={formErrors['categoryIdd']}
       />
+      <ForesightCategoryIdInput {...inputProps} />
     </>
   )
 }
