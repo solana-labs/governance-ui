77a2e07aa1a8278ed4018f7961696a56035ebaa0
Corinne Bösch <corinne.boesch@gmail.com>
2022-04-14 13:23:11 +0100

    new realms design DaoCommunity

---
+++ components_2/DaoCommunity.tsx
@@ -0,0 +1,59 @@
+const DaoCommunity = () => {
+  return (
+    <div className="py-28 px-56">
+      <div className="text-blue">
+        <h1>A DAO is a community working</h1>
+        <h1>together to make decisions</h1>
+      </div>
+      <div>
+        <p>
+          DAOs (decentralized autonomous organizations) are an effective and
+          safe way to work with like-minded folks around the globe.
+        </p>
+        <br />
+        <p>
+          Think of them like an internet-native business that's collectively
+          owned and managed by its members. They have built-in treasuries that
+          no one has the authority to access without the approval of the group.
+          Decisions are governed by proposals and voting to ensure everyone in
+          the organization has a voice.
+        </p>
+      </div>
+      <div>
+        <p>Industries DAOs Impact:</p>
+      </div>
+      <div>
+        <div className="m-5">
+          <p className="font-bold">Defi</p>
+          <p>Enable equitable financial markets</p>
+        </div>
+        <div className="m-5">
+          <p className="font-bold">Gaming</p>
+          <p>Unlock new models of gameplay and digital content</p>
+        </div>
+        <div className="m-5">
+          <p className="font-bold">Creators</p>
+          <p>No middlemen, own your work</p>
+        </div>
+        <div className="m-5">
+          <p className="font-bold">Corporations</p>
+          <p>Reinvent loyalty programs</p>
+        </div>
+        <div className="m-5">
+          <p className="font-bold">Investing</p>
+          <p>Inclusive group access to capital allocation</p>
+        </div>
+        <div className="m-5">
+          <p className="font-bold">Communities</p>
+          <p>Build shared resources</p>
+        </div>
+        <div className="m-5">
+          <p className="font-bold">Startups</p>
+          <p>Reward risk-taking contributors to new products and ideas</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DaoCommunity
--- pages/solana/index.tsx
+++ pages/solana/index.tsx
@@ -2,6 +2,8 @@ import KickstartSolana from '../../components_2/KickstartSolana'
 import SolanaPerks from '../../components_2/SolanaPerks'
 import DaoTypes from '../../components_2/DaoTypes'
 import RealmsOptions from '../../components_2/RealmsOptions'
+import SplGov from '../../components_2/SplGov'
+import DaoCommunity from '../../components_2/DaoCommunity'
 import Footer from '../../components_2/Footer'
 
 const Solana = () => {
@@ -11,6 +13,10 @@ const Solana = () => {
       <SolanaPerks />
       <DaoTypes />
       <RealmsOptions />
+      <div className="px-56">
+        <SplGov />
+      </div>
+      <DaoCommunity />
       <Footer />
     </div>
   )
