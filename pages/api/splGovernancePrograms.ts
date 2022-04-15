414bd96f53ae748a369abe76163441b3b7f6e515
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-04-15 14:40:30 +0100

    chore: update comments

--- pages/api/splGovernancePrograms.ts
+++ pages/api/splGovernancePrograms.ts
@@ -2,6 +2,7 @@ import { getCertifiedRealmInfos } from '@models/registry/api'
 import { getConnectionContext } from '@utils/connection'
 import { NextApiRequest, NextApiResponse } from 'next'
 
+// Returns unique spl-governance program ids
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
   res
     .status(200)
