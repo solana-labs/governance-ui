c6609492cd15775bed3c8a8338c2cad13e21e528
Connor O'Hara <connor@switchboard.xyz>
2022-04-18 13:35:10 -0400

    added some switchboard things

--- components/TokenBalance/SwitchboardPermissionCard.tsx
+++ components/TokenBalance/SwitchboardPermissionCard.tsx
@@ -1,19 +1,19 @@
 import Button from '@components/Button'
-import Loading from '@components/Loading'
-import NFTSelector from '@components/NFTS/NFTSelector'
+//import Loading from '@components/Loading'
+//import NFTSelector from '@components/NFTS/NFTSelector'
 import { ChevronRightIcon } from '@heroicons/react/outline'
 import useQueryContext from '@hooks/useQueryContext'
 import useRealm from '@hooks/useRealm'
-import { NftVoterClient } from '@solana/governance-program-library'
+//import { NftVoterClient } from '@solana/governance-program-library'
 import {
   getTokenOwnerRecordAddress,
-  SYSTEM_PROGRAM_ID,
-  withCreateTokenOwnerRecord,
+  //SYSTEM_PROGRAM_ID,
+  //withCreateTokenOwnerRecord,
 } from '@solana/spl-governance'
-import { Transaction, TransactionInstruction } from '@solana/web3.js'
-import { sendTransaction } from '@utils/send'
+//import { Transaction, TransactionInstruction } from '@solana/web3.js'
+//import { sendTransaction } from '@utils/send'
 import Link from 'next/link'
-import { getNftVoterWeightRecord } from 'NftVotePlugin/sdk/accounts'
+//import { getNftVoterWeightRecord } from 'NftVotePlugin/sdk/accounts'
 import useNftPluginStore from 'NftVotePlugin/store/nftPluginStore'
 import { useState, useEffect } from 'react'
 import useVotePluginsClientStore from 'stores/useVotePluginsClientStore'
@@ -23,15 +23,15 @@ const SwitchboardPermissionCard = () => {
   const { fmtUrlWithCluster } = useQueryContext()
   const connected = useWalletStore((s) => s.connected)
   const wallet = useWalletStore((s) => s.current)
-  const client = useVotePluginsClientStore(
+  //const client = useVotePluginsClientStore(
     (s) => s.state.currentRealmVotingClient
   )
-  const nfts = useNftPluginStore((s) => s.state.votingNfts)
-  const isLoading = useNftPluginStore((s) => s.state.isLoadingNfts)
-  const connection = useWalletStore((s) => s.connection)
+  //const nfts = useNftPluginStore((s) => s.state.votingNfts)
+  //const isLoading = useNftPluginStore((s) => s.state.isLoadingNfts)
+  //const connection = useWalletStore((s) => s.connection)
   const [tokenOwnerRecordPk, setTokenOwneRecordPk] = useState('')
   const { tokenRecords, realm, symbol, mint, councilMint } = useRealm()
-  const { fetchRealm } = useWalletStore((s) => s.actions)
+  //const { fetchRealm } = useWalletStore((s) => s.actions)
   const ownTokenRecord = wallet?.publicKey
     ? tokenRecords[wallet.publicKey!.toBase58()]
     : null
