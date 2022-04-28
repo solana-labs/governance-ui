89d3237dbb98f4ee219005d98e238162dab4446b
Albert Hermida <alberthermida@gmail.com>
2022-04-28 18:26:44 -0400

    modify-sb-init: move around plugin files to match nft plugin

---
+++ SwitchboardVotePlugin/store/switchboardStore.ts
@@ -0,0 +1,40 @@
+import { BN } from '@project-serum/anchor'
+import {
+  /*MaxVoterWeightRecord,*/ ProgramAccount,
+} from '@solana/spl-governance'
+import { NFTWithMeta, VotingClient } from '@utils/uiTypes/VotePlugin'
+import create, { State } from 'zustand'
+
+interface SwitchboardPluginStore extends State {
+  state: {
+    votingPower: BN
+    isLoading: boolean
+  }
+  setVotingPower: (votingPower: BN) => void
+  setIsLoading: (val: boolean) => void
+}
+
+const defaultState = {
+  votingPower: new BN(0),
+  isLoading: false,
+}
+
+const useSwitchboardPluginStore = create<SwitchboardPluginStore>(
+  (set, _get) => ({
+    state: {
+      ...defaultState,
+    },
+    setIsLoading: (val) => {
+      set((s) => {
+        s.state.isLoading = val
+      })
+    },
+    setVotingPower: (votingPower) => {
+      set((s) => {
+        s.state.votingPower = votingPower
+      })
+    },
+  })
+)
+
+export default useSwitchboardPluginStore
