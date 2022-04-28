8d68a6b9a31e362100e802758d1afc8f648c6859
Corinne Bösch <corinne.boesch@gmail.com>
2022-04-28 13:39:38 +0100

    redirecting root route from realms to solana

---
+++ pages/_app_1.tsx
@@ -0,0 +1,19 @@
+import '../styles/index.css'
+import Solana from './solana'
+
+function App() {
+  return (
+    <div>
+      {/* <ThemeProvider defaultTheme="Mango"> */}
+      {/* <NavBar /> */}
+      {/* <PageBodyContainer> */}
+      <Solana />
+      {/* </PageBodyContainer> */}
+      {/* </ThemeProvider> */}
+      {/* <Footer />
+      {/* <Footer className="bg-bkg-13" /> */}
+    </div>
+  )
+}
+
+export default App
