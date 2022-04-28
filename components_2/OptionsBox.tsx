3610d570baa64110995c6aca93833a84af19d4e2
Corinne Bösch <corinne.boesch@gmail.com>
2022-04-28 10:52:52 +0100

    updated OptionsBox flex

--- components_2/OptionsBox.tsx
+++ components_2/OptionsBox.tsx
@@ -1,19 +1,27 @@
-export const OptionsBox = ({ imgSrc, title, text }) => {
+export const OptionsBox = ({ imgSrc, title, text, direction = 'ltr' }) => {
   return (
-    <div className="flex flex-col align-middle">
-      <div className="my-12 px-28 text-center">
+    <div
+      className={`flex flex-col ${
+        direction === 'rtl' ? 'sm:flex-row-reverse' : 'sm:flex-row'
+      } align-middle`}
+    >
+      <div id="text" className="my-12 px-28 text-center">
         <h2 className="font-thin">{title}</h2>
         <p className="opacity-70">{text}</p>
       </div>
-      <div className="p-16 border bg-[#131418] z-0">
-        {/* <img
+      <div
+        id="image"
+        className="h-40 relative p-16 border bg-[#131418] z-0 w-64 sm:w-9"
+      >
+        <img
           src="/img/realms-web/backgrounds/glow-round.svg"
-          className={shadowClass}
-        /> */}
-        <div id="glow"></div>
+          className="absolute z-5 left-0 top-0 mt-8"
+          // className={shadowClass}
+        />
+        {/* <div id="glow"></div> */}
         <img
           src={`/img/realms-web/backgrounds/${imgSrc}.svg`}
-          className="my-2 h-40 absolute z-10"
+          className="h-40 absolute z-10 left-0 top-0 mt-8"
           // className="my-2 h-40 absolute z-10"
         />
       </div>
--- components_2/RealmsOptions.tsx
+++ components_2/RealmsOptions.tsx
@@ -19,23 +19,27 @@ const RealmsOptions = () => {
         title="Mint tokens for your community"
         text="Use your tokens to provide membership and voting power to your
           community members."
+        direction="rtl"
       />
       <OptionsBox
         imgSrc="PLACEHOLDER-member-address"
         title="Invite and Coordinate"
         text="Bring members of your community onboard to participate in your DAO’s
           activity."
+        direction="ltr"
       />
       <OptionsBox
         imgSrc="PLACEHOLDER-vote-options"
         title="Vote on the Issues"
         text={`Members are given a democratic voice in each DAO’s voting process.`}
+        direction="rtl"
       />
       <OptionsBox
         imgSrc="PLACEHOLDER-treasury-fund"
         title="Fund and Allocate the Treasury"
         text="Leverage your DAO’s treasury to fund goals and ideas voted on by its
           members."
+        direction="ltr"
       />
     </div>
   )
