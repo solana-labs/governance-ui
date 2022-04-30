77206f9c86c15eaeedaf8bd543a25bf94f64497f
0xavro <0xavroarrow@gmail.com>
2022-04-30 17:47:48 -0400

    Integrate dialect notification modal

--- components/NotificationsSwitch/index.tsx
+++ components/NotificationsSwitch/index.tsx
@@ -1,52 +1,62 @@
 import { useRef, useState } from 'react'
-// import * as anchor from '@project-serum/anchor'
+import * as anchor from '@project-serum/anchor'
 import {
-  // IncomingThemeVariables,
+  IncomingThemeVariables,
+  NotificationsModal,
   defaultVariables,
 } from '@dialectlabs/react-ui'
-// import { WalletType } from '@dialectlabs/react'
+import { WalletType } from '@dialectlabs/react'
 import { Transition } from '@headlessui/react'
-// import { useTheme } from 'next-themes'
-// import useWalletStore from 'stores/useWalletStore'
+import { useTheme } from 'next-themes'
+import useWalletStore from 'stores/useWalletStore'
+// import { Main } from 'next/document'
 
-// const REALMS_PUBLIC_KEY = new anchor.web3.PublicKey(
-//   'BUxZD6aECR5B5MopyvvYqJxwSKDBhx2jSSo1U32en6mj'
-// )
+const REALMS_PUBLIC_KEY = new anchor.web3.PublicKey(
+  'BUxZD6aECR5B5MopyvvYqJxwSKDBhx2jSSo1U32en6mj'
+)
 
-// const themeVariables: IncomingThemeVariables = {
-//   light: {
-//     colors: {
-//       bg: 'bg-bkg-1',
-//     },
-//     button: `${defaultVariables.light.button} bg-primary-light border-primary-light font-bold rounded-full hover:bg-primary-dark`,
-//     buttonLoading: `${defaultVariables.light.buttonLoading} rounded-full min-h-[40px]`,
-//     secondaryDangerButton: `${defaultVariables.light.secondaryDangerButton} rounded-full`,
-//     bellButton: `!bg-bkg-2 !shadow-none text-fgd-1 h-10 rounded-full w-10 hover:bg-bkg-3`,
-//     modal: `${defaultVariables.light.modal} sm:border sm:rounded-md sm:border-fgd-4 sm:shadow-md`,
-//     modalWrapper: `${defaultVariables.dark.modalWrapper} sm:top-14`,
-//   },
-//   dark: {
-//     colors: {
-//       bg: 'bg-bkg-1',
-//       highlight: 'border border-fgd-4',
-//     },
-//     button: `${defaultVariables.dark.button} bg-primary-light border-primary-light font-bold rounded-full hover:bg-primary-dark`,
-//     buttonLoading: `${defaultVariables.dark.buttonLoading} rounded-full min-h-[40px]`,
-//     secondaryDangerButton: `${defaultVariables.dark.secondaryDangerButton} rounded-full`,
-//     bellButton:
-//       '!bg-bkg-2 !shadow-none text-fgd-1 h-10 rounded-full w-10 hover:bg-bkg-3',
-//     modal: `${defaultVariables.dark.modal} bg-bkg-1 sm:border sm:border-fgd-4 shadow-md sm:rounded-md`,
-//     modalWrapper: `${defaultVariables.dark.modalWrapper} sm:top-14`,
-//   },
-// }
+enum ModalStates {
+  Selection = 1,
+  Dialect = 2,
+}
+
+const themeVariables: IncomingThemeVariables = {
+  light: {
+    colors: {
+      bg: 'bg-bkg-1',
+    },
+    button: `${defaultVariables.light.button} bg-primary-light border-primary-light font-bold rounded-full hover:bg-primary-dark`,
+    buttonLoading: `${defaultVariables.light.buttonLoading} rounded-full min-h-[40px]`,
+    secondaryDangerButton: `${defaultVariables.light.secondaryDangerButton} rounded-full`,
+    bellButton: `!bg-bkg-2 !shadow-none text-fgd-1 h-10 rounded-full w-10 hover:bg-bkg-3`,
+    modal: `${defaultVariables.light.modal} sm:border sm:rounded-md sm:border-fgd-4 sm:shadow-md`,
+    modalWrapper: `${defaultVariables.dark.modalWrapper} sm:top-14`,
+  },
+  dark: {
+    colors: {
+      bg: 'bg-bkg-1',
+      highlight: 'border border-fgd-4',
+    },
+    button: `${defaultVariables.dark.button} bg-primary-light border-primary-light font-bold rounded-full hover:bg-primary-dark`,
+    buttonLoading: `${defaultVariables.dark.buttonLoading} rounded-full min-h-[40px]`,
+    secondaryDangerButton: `${defaultVariables.dark.secondaryDangerButton} rounded-full`,
+    bellButton:
+      '!bg-bkg-2 !shadow-none text-fgd-1 h-10 rounded-full w-10 hover:bg-bkg-3',
+    modal: `${defaultVariables.dark.modal} bg-bkg-1 sm:border sm:border-fgd-4 shadow-md sm:rounded-md`,
+    modalWrapper: `${defaultVariables.dark.modalWrapper} sm:top-14`,
+  },
+}
 
 export default function NotificationsSwitch() {
-  // const { theme } = useTheme()
+  const { theme } = useTheme()
   const wrapperRef = useRef(null)
-  // const { current: wallet, connection } = useWalletStore()
-  // const cluster = connection.cluster
+  const { current: wallet, connection } = useWalletStore()
+  const cluster = connection.cluster
 
   const [openModal, setOpenModal] = useState(false)
+  const [modalState, setModalState] = useState<ModalStates>(
+    ModalStates.Selection
+  )
 
   // return (
   //   <NotificationsButton
@@ -59,7 +69,8 @@ export default function NotificationsSwitch() {
   //   />
   // )
   // console.log("bell: ", defaultVariables.dark.icons.bell({}));
-  const Bell = defaultVariables.dark.icons.bell
+  const BellIcon = defaultVariables.dark.icons.bell
+
   return (
     <>
       <Transition
@@ -67,46 +78,64 @@ export default function NotificationsSwitch() {
         show={openModal}
         {...defaultVariables.animations.popup}
       >
-        <div ref={wrapperRef} className="w-full h-full bg-bkg-3">
-          <div className="h-full flex flex-col items-center py-8">
-            <Bell />
-            <h2 className="mb-4">Realms Notifications</h2>
-            <div className="w-full p-4">
-              <div className="flex flex-col items-center bg-bkg-1 px-4 py-6">
-                <div className="flex w-full justify-between">
-                  <div>
-                    <h2 className="inline-block">Dialect</h2>
+        {modalState === ModalStates.Selection && (
+          <div ref={wrapperRef} className="w-full h-full bg-bkg-3">
+            <div className="h-full flex flex-col items-center py-8">
+              <BellIcon />
+              <h2 className="mb-4">Realms Notifications</h2>
+              <div className="w-full p-4">
+                <div className="flex flex-col items-center bg-bkg-1 px-4 py-6">
+                  <div className="flex w-full justify-between">
+                    <div>
+                      <h2 className="inline-block">Dialect</h2>
+                    </div>
+                    <div>
+                      <p className="inline-block">Wallet</p>
+                    </div>
                   </div>
-                  <div>
-                    <p className="inline-block">Wallet</p>
-                  </div>
-                </div>
 
-                <div className="flex w-full justify-start">
-                  <div>
-                    <p className="inline-block">
-                      Dialect is the first protocol for smart messaging -
-                      dynamic, composable dapp notifications and
-                      wallet-to-wallet chat
-                    </p>
+                  <div className="flex w-full justify-start">
+                    <div>
+                      <p className="inline-block">
+                        Dialect is the first protocol for smart messaging -
+                        dynamic, composable dapp notifications and
+                        wallet-to-wallet chat
+                      </p>
+                    </div>
                   </div>
-                </div>
 
-                <div className="flex w-full justify-center pt-3">
-                  <button className="bg-white rounded-full py-1 w-full text-black">
-                    Use Dialect
-                  </button>
+                  <div className="flex w-full justify-center pt-3">
+                    <button
+                      className="bg-white rounded-full py-1 w-full text-black"
+                      onClick={() => setModalState(ModalStates.Dialect)}
+                    >
+                      Use Dialect
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
+
+        {modalState === ModalStates.Dialect && (
+          <NotificationsModal
+            wallet={(wallet as unknown) as WalletType}
+            network={cluster as string}
+            publicKey={REALMS_PUBLIC_KEY}
+            theme={theme === 'Dark' ? 'dark' : 'light'}
+            variables={themeVariables}
+            notifications={[{ name: 'New proposals', detail: 'Event' }]}
+            onBackClick={() => setModalState(ModalStates.Selection)}
+            channels={['web3']}
+          />
+        )}
       </Transition>
       <button
         className="bg-bkg-2 default-transition flex items-center justify-center h-10 rounded-full w-10 hover:bg-bkg-3"
         onClick={() => setOpenModal(!openModal)}
       >
-        {defaultVariables.dark.icons.bell({})}
+        <BellIcon />
       </button>
     </>
   )
