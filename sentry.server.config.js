5e86a6456c61257e36988ba1e3a5f3f06ebea9ee
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-05-05 00:55:58 +0100

    wip: try to remove env and fix the build

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -5,7 +5,7 @@
 import * as Sentry from '@sentry/nextjs'
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
-const SENTRY_ENVIRONMENT = process.env.VERCEL_ENV
+// const SENTRY_ENVIRONMENT = process.env.VERCEL_ENV
 
 Sentry.init({
   dsn:
@@ -13,7 +13,7 @@ Sentry.init({
     'https://18ab56d9b27a4483a29e51d6520afbc6@o434108.ingest.sentry.io/6380292',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
-  environment: SENTRY_ENVIRONMENT,
+  // environment: SENTRY_ENVIRONMENT,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
