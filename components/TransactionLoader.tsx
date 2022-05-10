9730b01992487ee9504460f896a4119f4d49b6a2
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-05-10 09:33:22 +0200

    loader ui

--- components/TransactionLoader.tsx
+++ components/TransactionLoader.tsx
@@ -1,4 +1,5 @@
 import useTransactionsStore from 'stores/useTransactionStore'
+import Loading from './Loading'
 import Modal from './Modal'
 
 const TransactionLoader = () => {
@@ -7,7 +8,7 @@ const TransactionLoader = () => {
     transactionsCount,
     processedTransactions,
   } = useTransactionsStore()
-
+  const currentlyProcessing = processedTransactions + 1
   return isProcessing ? (
     <Modal
       onClose={() => {
@@ -15,9 +16,15 @@ const TransactionLoader = () => {
       }}
       isOpen={isProcessing}
     >
-      <h2>Processing {transactionsCount} transactions</h2>
-      <div>
+      <h2>
         Transactions left to process {transactionsCount - processedTransactions}
+      </h2>
+      <div className="flex items-center text-xs">
+        Currently processing transaction {currentlyProcessing} out of{' '}
+        {transactionsCount}
+        <div className="ml-2">
+          <Loading></Loading>
+        </div>
       </div>
       <div className="pb-5 pt-2">
         <div className="w-full h-4 bg-gray-200 rounded-full dark:bg-gray-700">
