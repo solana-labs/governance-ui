161fe3323e25c5897d34e669c48b203b03f518a0
Jonathan <jonathan3692bf@users.noreply.github.com>
2022-05-10 22:06:51 +0200

    Hide FAQ button in FormFooter

--- pages/solana/create_dao/[type]/components/FormFooter.tsx
+++ pages/solana/create_dao/[type]/components/FormFooter.tsx
@@ -12,16 +12,16 @@ const FormFooter: React.FC<FormFooterProps> = ({
   isValid,
   prevClickHandler,
   submitClickHandler,
-  faqTitle,
+  // faqTitle,
 }) => {
   return (
-    <div className="flex flex-wrap items-center justify-between pt-20 md:pt-32">
-      <Button type="button" tertiary>
+    <div className="flex flex-wrap items-center justify-end pt-20 md:pt-32">
+      {/* <Button type="button" tertiary>
         <div className="flex items-center">
           <img src="/1-Landing-v2/icon-faq.png" className="w-6 h-6 mx-2" />
           <div className="pr-3">{faqTitle}</div>
         </div>
-      </Button>
+      </Button> */}
       <div className="flex justify-between w-full mt-8 space-x-8 md:w-fit md:mt-0">
         <Button type="button" onClick={prevClickHandler}>
           <img
