aed2f8dc9ab029092cc8485ec2e0aee1d38c46d6
Nathan Chung <105258726+Nathan-Notifi@users.noreply.github.com>
2022-05-09 20:56:30 -0700

    create notifi ui component in alerts panel

--- components/NotificationsSwitch/index.tsx
+++ components/NotificationsSwitch/index.tsx
@@ -182,7 +182,6 @@ export default function NotificationsSwitch() {
           <div>
             <p className="inline-block text-sm">{description}</p>
           </div>
-        </div>
 
         <div className="flex w-full justify-center pt-3">
           <Button
@@ -196,7 +195,7 @@ export default function NotificationsSwitch() {
             Use {formatName(name)}
           </Button>
         </div>
-      </div>
+      )}
     </div>
   )
 
