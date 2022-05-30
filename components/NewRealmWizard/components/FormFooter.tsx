b99ac97842f82b2d6c7a1679ce38b8f8efb78299
Jonathan <jonathan3692bf@users.noreply.github.com>
2022-05-30 11:21:22 +0300

    Start building form nav buttons

--- components/NewRealmWizard/components/FormFooter.tsx
+++ components/NewRealmWizard/components/FormFooter.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { NewButton as Button } from '@components/Button'
-
+import { ArrowLeftIcon } from '@heroicons/react/solid'
 interface FormFooterProps {
   isValid?: boolean
   prevClickHandler: React.MouseEventHandler<HTMLButtonElement>
@@ -17,18 +16,21 @@ const FormFooter: React.FC<FormFooterProps> = ({
   return (
     <div className="flex flex-wrap items-center justify-end pt-10 sm:pt-12 md:pt-16">
       <div className="flex justify-center w-full mt-8 space-x-8 md:justify-between md:w-fit md:mt-0">
-        <Button type="button" onClick={prevClickHandler}>
-          <img
-            src="/1-Landing-v2/icon-arrow-black.png"
-            className="w-6 h-6 mx-2 rotate-180"
-          />
-        </Button>
-        <Button type="submit" disabled={!isValid} onClick={submitClickHandler}>
-          <img
-            src="/1-Landing-v2/icon-arrow-black.png"
-            className="w-6 h-6 mx-2"
-          />
-        </Button>
+        <button
+          type="button"
+          className="rounded-full outline outline-white/10 hover:outline-white active:outline-none active:bg-white/10 focused:outline-white focused:bg-white/10 disabled:cursor-not-allowed"
+          onClick={prevClickHandler}
+        >
+          <ArrowLeftIcon className="w-6 m-4" />
+        </button>
+        <button
+          type="submit"
+          className="text-black default-transition rounded-full bg-gradient-to-r from-[#00C2FF] via-[#00E4FF] to-[#87F2FF] transition-from-gradient-background"
+          disabled={!isValid}
+          onClick={submitClickHandler}
+        >
+          <ArrowLeftIcon className="w-6 m-4 rotate-180" />
+        </button>
       </div>
     </div>
   )
