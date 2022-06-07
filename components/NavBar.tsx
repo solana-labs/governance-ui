854a6e6aa1f9472c9e1a07e0fd9dd70098b2b45b
Jonathan <jonathan3692bf@users.noreply.github.com>
2022-06-07 20:10:12 +0200

    Use new logo for Navbar, make responsive for mobile

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -10,17 +10,19 @@ const NavBar = () => {
 
   return (
     <div className="grid grid-cols-12 mb-3">
-      <div className="col-span-12 xl:col-start-2 xl:col-span-10 flex h-20 items-center justify-between px-4 md:px-8 xl:px-4">
+      <div className="flex items-baseline justify-between h-20 col-span-12 px-4 pt-2 sm:pt-0 sm:items-center xl:col-start-2 xl:col-span-10 md:px-8 xl:px-4">
         <Link href={fmtUrlWithCluster('/realms')}>
-          <div className="cursor-pointer flex items-center">
-            <img src="/img/solana-logo.svg" className="h-8 mr-3" />
-            {/* <h1 className="text-lg">Sierra</h1> */}
+          <div className="flex cursor-pointer sm:items-center min-w-[36px]">
+            <img src="/img/logo-realms-blue.png" className="w-8 h-8" />
+            <span className="hidden sm:block">Realms</span>
           </div>
         </Link>
-        <div className="flex items-center space-x-4">
-          <ThemeSwitch />
-          <NotificationsSwitch />
+        <div className="flex flex-wrap items-center justify-end space-y-2 sm:flex-row-reverse sm:space-y-0">
           <ConnectWalletButton />
+          <div className="min-w-[33%] sm:min-w-fit sm:space-x-4 sm:mx-4 flex justify-between">
+            <NotificationsSwitch />
+            <ThemeSwitch />
+          </div>
         </div>
       </div>
     </div>
