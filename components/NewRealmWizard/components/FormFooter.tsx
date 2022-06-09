e875153ebbeb7ff431330aaa1b1489f03a9720a6
Jonathan <jonathan3692bf@users.noreply.github.com>
2022-06-09 21:21:40 +0200

    update arrow icons

--- components/NewRealmWizard/components/FormFooter.tsx
+++ components/NewRealmWizard/components/FormFooter.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
-import { ArrowLeftIcon } from '@heroicons/react/solid'
 interface FormFooterProps {
   isValid?: boolean
   prevClickHandler: React.MouseEventHandler<HTMLButtonElement>
   submitClickHandler?: React.MouseEventHandler<HTMLButtonElement>
+}
+
+function ArrowRight({ className }) {
+  return (
+    <div className={className}>
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="m12.75 3 9.75 9m0 0-9.75 9.75M22.5 12h-21"
+          stroke="currentColor"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+        />
+      </svg>
+    </div>
+  )
 }
 
 const FormFooter: React.FC<FormFooterProps> = ({
@@ -19,15 +39,15 @@ const FormFooter: React.FC<FormFooterProps> = ({
           className="rounded-full outline outline-white/10 hover:outline-white active:outline-none active:bg-white/10 focused:outline-white focused:bg-white/10 disabled:cursor-not-allowed"
           onClick={prevClickHandler}
         >
-          <ArrowLeftIcon className="w-6 m-4" />
+          <ArrowRight className="m-4 rotate-180" />
         </button>
         <button
           type="submit"
-          className="relative text-black default-transition rounded-full bg-gradient-to-r from-[#00C2FF] via-[#00E4FF] to-[#87F2FF] transition-from-gradient-background disabled:cursor-not-allowed"
+          className="relative text-black rounded-full hover:text-black default-transition bg-brand-gradient transition-from-gradient-background disabled:cursor-not-allowed"
           disabled={!isValid}
           onClick={submitClickHandler}
         >
-          <ArrowLeftIcon className="w-6 m-4 rotate-180" />
+          <ArrowRight className="relative z-20 m-4" />
         </button>
       </div>
     </div>
