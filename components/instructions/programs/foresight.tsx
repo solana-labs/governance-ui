3a456cb0cd4580a9180e0f2fb622f7f29925aea5
Kevin Heavey <kevinheavey123@gmail.com>
2022-06-09 16:49:39 +0100

    fetch account data to show more context

--- components/instructions/programs/foresight.tsx
+++ components/instructions/programs/foresight.tsx
@@ -1,5 +1,10 @@
-import { Connection } from '@solana/web3.js'
-import { consts, generatedTypes } from '@foresight-tmp/foresight-sdk'
+import { Connection, PublicKey } from '@solana/web3.js'
+import { utils } from '@project-serum/anchor'
+import {
+  consts,
+  generatedTypes,
+  generatedAccounts,
+} from '@foresight-tmp/foresight-sdk'
 import { AccountMetaData } from '@solana/spl-governance'
 import { IDL } from '@foresight-tmp/foresight-sdk/'
 import { layout as initCategoryLayout } from '@foresight-tmp/foresight-sdk/dist/generated/instructions/initCategory'
@@ -14,6 +19,25 @@ function displayParsedArg(argName: string, parsed: string): JSX.Element {
       {argName}: {parsed}
     </p>
   )
+}
+
+function numArrayToString(data: number[]): string {
+  return Buffer.from(data).toString()
+}
+
+function parseMetadata(
+  metadata: generatedAccounts.MarketMetadata,
+  includeImage = true
+): string {
+  const base = {
+    title: numArrayToString(metadata.title),
+    description: numArrayToString(metadata.description),
+    rules: numArrayToString(metadata.rules),
+  }
+  const maybeImageObj = includeImage
+    ? { image: numArrayToString(metadata.image) }
+    : {}
+  return JSON.stringify({ ...base, ...maybeImageObj }, null, 2)
 }
 
 function toDecodable(data: Uint8Array): Buffer {
@@ -32,6 +56,31 @@ function findAccounts(ixName: string): { name: string }[] {
     })
 }
 
+async function fetchId(
+  connection: Connection,
+  pubkey: PublicKey,
+  accountType:
+    | typeof generatedAccounts.Category
+    | typeof generatedAccounts.MarketList
+): Promise<string> {
+  const account = await accountType.fetch(connection, pubkey)
+  return account === null ? 'Error: not found' : numArrayToString(account.id)
+}
+
+async function fetchCategoryId(
+  connection: Connection,
+  pubkey: PublicKey
+): Promise<string> {
+  return await fetchId(connection, pubkey, generatedAccounts.Category)
+}
+
+async function fetchMarketListId(
+  connection: Connection,
+  pubkey: PublicKey
+): Promise<string> {
+  return await fetchId(connection, pubkey, generatedAccounts.MarketList)
+}
+
 export const FORESIGHT_INSTRUCTIONS = {
   [consts.DEVNET_PID]: {
     65: {
@@ -45,10 +94,7 @@ export const FORESIGHT_INSTRUCTIONS = {
         const args = decodeIxData(data, initCategoryLayout)
         return (
           <>
-            {displayParsedArg(
-              'categoryId',
-              Buffer.from(args.categoryId).toString()
-            )}
+            {displayParsedArg('categoryId', numArrayToString(args.categoryId))}
           </>
         )
       },
@@ -66,7 +112,7 @@ export const FORESIGHT_INSTRUCTIONS = {
           <>
             {displayParsedArg(
               'marketListId',
-              Buffer.from(args.marketListId).toString()
+              numArrayToString(args.marketListId)
             )}
           </>
         )
@@ -76,41 +122,71 @@ export const FORESIGHT_INSTRUCTIONS = {
       name: 'Foresight: Init Market',
       accounts: findAccounts('initMarket'),
       getDataUI: async (
-        _connection: Connection,
+        connection: Connection,
         data: Uint8Array,
-        _accounts: AccountMetaData[]
+        accounts: AccountMetaData[]
       ) => {
         const args = decodeIxData(data, initMarketLayout)
-        return <>{displayParsedArg('marketId', args.marketId[0].toString())}</>
+        const marketListId = await fetchMarketListId(
+          connection,
+          accounts[1].pubkey
+        )
+        return (
+          <>
+            {displayParsedArg('marketId', args.marketId[0].toString())}
+            {displayParsedArg('marketListId', marketListId)}
+          </>
+        )
       },
     },
     218: {
       name: 'Foresight: Add Market List to Category',
       accounts: findAccounts('addMarketListToCategory'),
       getDataUI: async (
-        _connection: Connection,
+        connection: Connection,
         _data: Uint8Array,
-        _accounts: AccountMetaData[]
+        accounts: AccountMetaData[]
       ) => {
-        return <></>
+        const categoryId = await fetchCategoryId(connection, accounts[1].pubkey)
+        const marketListId = await fetchMarketListId(
+          connection,
+          accounts[2].pubkey
+        )
+        return (
+          <>
+            {' '}
+            {displayParsedArg('categoryId', categoryId)}
+            {displayParsedArg('marketListId', marketListId)}
+          </>
+        )
       },
     },
     90: {
       name: 'Foresight: Set Market Metadata',
       accounts: findAccounts('writeToFieldMarketMetadata'),
       getDataUI: async (
-        _connection: Connection,
+        connection: Connection,
         data: Uint8Array,
-        _accounts: AccountMetaData[]
+        accounts: AccountMetaData[]
       ) => {
         const args = decodeIxData(data, writeToFieldMarketMetadataLayout)
         const field = generatedTypes.MarketMetadataFields.fromDecoded(
           args.field
         ).kind
+        const metadataAccount = await generatedAccounts.MarketMetadata.fetch(
+          connection,
+          accounts[1].pubkey
+        )
+        const currentMetadata =
+          metadataAccount === null
+            ? 'Error: not found.'
+            : parseMetadata(metadataAccount)
         return (
           <>
             {displayParsedArg('field', field)}
-            {displayParsedArg('content', Buffer.from(args.string).toString())}
+            {displayParsedArg('content', numArrayToString(args.string))}
+            <h4>Note: this is the current metadata:</h4>
+            <p>{currentMetadata}</p>
           </>
         )
       },
@@ -119,13 +195,44 @@ export const FORESIGHT_INSTRUCTIONS = {
       name: 'Foresight: Resolve Market',
       accounts: findAccounts('resolveMarket'),
       getDataUI: async (
-        _connection: Connection,
+        connection: Connection,
         data: Uint8Array,
-        _accounts: AccountMetaData[]
+        accounts: AccountMetaData[]
       ) => {
         const args = decodeIxData(data, resolveMarketLayout)
         const winner = args.winningBracket === 0 ? 'YES' : 'NO'
-        return <>{displayParsedArg('Winner', winner)}</>
+        const marketPubkey = accounts[2].pubkey
+        const marketAccount = await generatedAccounts.Market.fetch(
+          connection,
+          marketPubkey
+        )
+        const notFoundMsg = 'Error: not found.'
+        let currentMetadata: string
+        if (marketAccount === null) {
+          currentMetadata = notFoundMsg
+        } else {
+          const marketId = Buffer.from(marketAccount.id)
+          const marketListId = Buffer.from(marketAccount.marketListId)
+          const [metadataPubkey] = utils.publicKey.findProgramAddressSync(
+            [Buffer.from('market_metadata'), marketId, marketListId],
+            new PublicKey(consts.DEVNET_PID)
+          )
+          const metadataAccount = await generatedAccounts.MarketMetadata.fetch(
+            connection,
+            metadataPubkey
+          )
+          currentMetadata =
+            metadataAccount === null
+              ? 'Error: not found.'
+              : parseMetadata(metadataAccount, false)
+        }
+        return (
+          <>
+            {displayParsedArg('Winner', winner)}
+            <h4>Note: here is the relevant market metadata:</h4>
+            <p>{currentMetadata}</p>
+          </>
+        )
       },
     },
   },
