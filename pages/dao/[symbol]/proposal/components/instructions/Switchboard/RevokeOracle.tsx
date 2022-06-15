35169e4da4430132ff108568a340e75b7267c1e5
Connor O'Hara <connor@switchboard.xyz>
2022-06-15 17:13:09 -0400

    cleaned up comments

--- pages/dao/[symbol]/proposal/components/instructions/Switchboard/RevokeOracle.tsx
+++ pages/dao/[symbol]/proposal/components/instructions/Switchboard/RevokeOracle.tsx
@@ -38,86 +38,11 @@ const SwitchboardRevokeOracle = ({
   index: number
   _governance: ProgramAccount<Governance> | null
 }) => {
-  /*const {
-    inputProps,
-    effector,
-    governedAccountSelect,
-    wallet,
-  } = commonAssets<ForesightHasMarketId>(
-    { marketListId: '', marketId: 0 },
-    index,
-    governance
-  )
-  async function ixCreator(form: ForesightHasMarketId) {
-    const { ix } = await foresightGov.genInitMarketIx(
-      Buffer.from(form.marketListId.padEnd(20)),
-      utils.intToArray(form.marketId, 1),
-      new PublicKey(consts.DEVNET_PID),
-      wallet!.publicKey!
-    )
-    return ix
-  }
-  effector(ixCreator)*/
-
-  /*return (
-    <>
-      {governedAccountSelect}
-      <ForesightMarketListIdInput {...inputProps} />
-      <ForesightMarketIdInput {...inputProps} />
-    </>
-  )*/
 
   const [form, setForm] = useState<SwitchboardRevokeOracleForm>({oraclePubkey: undefined, queuePubkey: undefined})
   const connection = useWalletStore((s) => s.connection)
   const wallet = useWalletStore((s) => s.current)
   const { handleSetInstructions } = useContext(NewProposalContext)
-
-  /*useEffect(() => {
-
-    async function getIdls() {
-      const options = anchor.AnchorProvider.defaultOptions()
-      const provider = new anchor.AnchorProvider(
-        connection.current,
-        (wallet as unknown) as anchor.Wallet,
-        options
-      )
-
-      let idl = await anchor.Program.fetchIdl(sbv2.SBV2_MAINNET_PID, provider)
-      if (!idl) {
-        console.log("Off chain idl");
-        idl = sbIdl as anchor.Idl
-      }
-
-      let addinIdl = await anchor.Program.fetchIdl(SWITCHBOARD_ADDIN_ID, provider)
-      if (!addinIdl) {
-        console.log("Off chain addin idl");
-        addinIdl = gonIdl as anchor.Idl
-      }
-
-      const switchboardProgram = new anchor.Program(
-        idl,
-        SWITCHBOARD_ID,
-        provider
-      )
-      console.log(switchboardProgram);
-
-      const addinProgram = new anchor.Program(
-        addinIdl,
-        SWITCHBOARD_ADDIN_ID,
-        provider
-      )
-      console.log(switchboardProgram);
-
-      setPrograms({...programs, ['provider']: provider});
-      setPrograms({...programs, ['addinProgram']: addinProgram});
-      setPrograms({...programs, ['switchboardProgram']: switchboardProgram});
-      setPrograms({...programs, ['addinIdl']: addinIdl});
-      setPrograms({...programs, ['idl']: idl});
-
-    }
-    getIdls();
-
-  }, [realm]);*/
 
   useEffect(() => {
 
@@ -127,17 +52,6 @@ const SwitchboardRevokeOracle = ({
     }, index)
 
   }, [form]);
-
-  /*useEffect(() => {
-    async function getAndSetGov() {
-      let gov = await getGovernance(connection.current, SWITCHBOARD_GRANT_AUTHORITY);
-      console.log("GOT GOV");
-      console.log(gov);
-      setGovernance(SWITCHBOARD_GRANT_AUTHORITY);
-      //console.log("And set it.");
-    }
-    getAndSetGov();
-  }, [realm]);*/
 
   async function getInstruction(): Promise<UiInstruction> {
 
@@ -202,50 +116,9 @@ const SwitchboardRevokeOracle = ({
         isValid: true,
         governance: gov,
       };
-      /*const isValid = await validateInstruction()
-
-      if (
-        !connection ||
-        !isValid ||
-        !programId ||
-        !form.governedAccount?.governance?.account ||
-        !form.splTokenMintUIName ||
-        !wallet?.publicKey
-      ) {
-        return {
-          serializedInstruction: '',
-          isValid: false,
-          governance: form.governedAccount?.governance,
-        }
-      }
-
-      const [tx] = await createAssociatedTokenAccount(
-        // fundingAddress
-        wallet.publicKey,
-
-        // walletAddress
-        form.governedAccount.governance.pubkey,
-
-        // splTokenMintAddress
-        getSplTokenMintAddressByUIName(form.splTokenMintUIName)
-      )
-
-      return {
-        serializedInstruction: serializeInstructionToBase64(tx),
-        isValid: true,
-        governance: form.governedAccount.governance,
-      }*/
     }
 
   return (
-    /*onChange={(evt) =>
-      props.handleSetForm({
-      value: evt.target.value,
-      propertyName: 'categoryId',
-      })
-      }*/
-    //error={props.formErrors['categoryId']}
-    //value={props.form.categoryId}
     <>
     <Input
       label="Oracle Pubkey"
