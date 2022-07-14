3ee5ce0d878d0e30aa30980db1b2b455b7ea4b34
KishiTheMechanic <s.kishi@elsoul.nl>
2022-07-14 20:10:52 +0200

    dropzone and show preview

--- pages/dao/[symbol]/params/MetadataCreationModal.tsx
+++ pages/dao/[symbol]/params/MetadataCreationModal.tsx
@@ -26,13 +26,16 @@ import {
 import { abbreviateAddress } from '@utils/formatting'
 import * as yup from 'yup'
 import { useDropzone } from 'react-dropzone'
+import { AssetAccount } from '@utils/uiTypes/assets'
+import GovernedAccountSelect from '@components/inputs/GovernedAccountSelect'
+import useGovernanceAssets from '@hooks/useGovernanceAssets'
+import { AccountType } from '@utils/uiTypes/assets'
 
 interface GovernanceConfigForm {
-  mintAccount: string
+  mintAccount: AssetAccount | undefined
   name: string
   symbol: string
   description: string
-  jsonURL: string
 }
 
 const MetadataCreationModal = ({
@@ -44,8 +47,16 @@ const MetadataCreationModal = ({
   isOpen: boolean
   governance: ProgramAccount<Governance>
 }) => {
+  console.log({ governance })
   const router = useRouter()
   const { realm, canChooseWhoVote, symbol, mint } = useRealm()
+  const { assetAccounts } = useGovernanceAssets()
+  const mintGovernancesWithMintInfo = assetAccounts.filter((x) => {
+    return x.type === AccountType.MINT
+  })
+  console.log({ mintGovernancesWithMintInfo })
+
+  const shouldBeGoverned = false
   const config = governance?.account.config
   const { fmtUrlWithCluster } = useQueryContext()
   const wallet = useWalletStore((s) => s.current)
@@ -55,26 +66,12 @@ const MetadataCreationModal = ({
   const [voteByCouncil, setVoteByCouncil] = useState(false)
   const [selectedImage, setSelectedImage] = useState<null | string>(null)
   const [imageFile, setImageFile] = useState<null | Buffer>(null)
-  const [imageUrl, setImageUrl] = useState(null)
   const [form, setForm] = useState<GovernanceConfigForm>({
+    mintAccount: undefined,
     name: '',
+    symbol: '',
     description: '',
   })
-  const handleSetForm = ({ propertyName, value }) => {
-    setFormErrors({})
-    setForm({ ...form!, [propertyName]: value })
-  }
-  const schema = yup.object().shape({})
-  const handleCreate = async () => {
-    const isValid = await validateInstruction({ schema, form, setFormErrors })
-    let serializedInstruction = ''
-    if (isValid && governance?.account && wallet?.publicKey && realm) {
-      setCreatingProposal(true)
-
-      setCreatingProposal(false)
-    }
-  }
-
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
       'image/*': [],
@@ -85,7 +82,7 @@ const MetadataCreationModal = ({
       const reader = new FileReader()
       const file = acceptedFiles[0]
       if (file) {
-        setSelectedImage(file.name)
+        setSelectedImage(URL.createObjectURL(file))
         reader.onload = function () {
           if (reader.result) {
             setImageFile(Buffer.from(reader.result as ArrayBuffer))
@@ -95,58 +92,80 @@ const MetadataCreationModal = ({
       }
     },
   })
+  const handleSetForm = ({ propertyName, value }) => {
+    setFormErrors({})
+    setForm({ ...form!, [propertyName]: value })
+  }
+  const schema = yup.object().shape({})
+
+  const handleCreate = async () => {
+    const isValid = await validateInstruction({ schema, form, setFormErrors })
+    let serializedInstruction = ''
+    if (isValid && governance?.account && wallet?.publicKey && realm) {
+      setCreatingProposal(true)
+
+      setCreatingProposal(false)
+    }
+  }
+
+  const uploadImage = () => {}
+  const createJson = () => {}
+  const uploadJson = () => {}
 
   return (
     <Modal sizeClassName="sm:max-w-3xl" onClose={closeModal} isOpen={isOpen}>
       <div className="w-full space-y-4">
         <h3 className="flex flex-col mb-4">Create token metadata</h3>
+        <GovernedAccountSelect
+          label="Mint"
+          governedAccounts={mintGovernancesWithMintInfo}
+          onChange={(value) => {
+            handleSetForm({ value, propertyName: 'mintAccount' })
+          }}
+          value={form.mintAccount}
+          error={formErrors['mintAccount']}
+          shouldBeGoverned={shouldBeGoverned}
+          governance={governance}
+        />
         <div className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-1">
-          {!imageUrl ? (
-            <div className="mt-1 sm:mt-0 sm:col-span-1">
-              <div
-                {...getRootProps({
-                  className:
-                    'max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md cursor-pointer',
-                })}
-              >
-                <div className="space-y-1 text-center">
-                  <svg
-                    className="mx-auto h-12 w-12 text-gray-400"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 48 48"
-                    aria-hidden="true"
+          <div className="mt-1 sm:mt-0 sm:col-span-1">
+            <div
+              {...getRootProps({
+                className:
+                  'max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md cursor-pointer',
+              })}
+            >
+              <div className="space-y-1 text-center">
+                <svg
+                  className="mx-auto h-12 w-12 text-gray-400"
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 48 48"
+                  aria-hidden="true"
+                >
+                  <path
+                    d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+                <div className="flex text-sm text-gray-600">
+                  <label
+                    htmlFor="image-upload"
+                    className="relative cursor-pointer rounded-md font-medium text-primary-dark hover:text-primary-light focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
                   >
-                    <path
-                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                  <div className="flex text-sm text-gray-600">
-                    <label
-                      htmlFor="image-upload"
-                      className="relative cursor-pointer rounded-md font-medium text-primary-dark hover:text-primary-light focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                    >
-                      <span>Upload an image</span>
-                      <input {...getInputProps()} />
-                    </label>
-                    <p className="pl-1">or drag and drop</p>
-                  </div>
-                  {!selectedImage ? null : (
-                    <p className="text-sm text-gray-500">{selectedImage}</p>
-                  )}
+                    <span>Upload an image</span>
+                    <input {...getInputProps()} />
+                  </label>
+                  <p className="pl-1">or drag and drop</p>
                 </div>
+                {!selectedImage ? null : (
+                  <img src={selectedImage} className="w-32 m-auto" />
+                )}
               </div>
             </div>
-          ) : (
-            <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-              <a href={imageUrl} target="_blank" rel="noreferrer">
-                {imageUrl}
-              </a>
-            </div>
-          )}
+          </div>
         </div>
         <Input
           label="Name"
@@ -176,7 +195,7 @@ const MetadataCreationModal = ({
         />
         <Textarea
           label="Description"
-          placeholder="Description of the metadata (optional)"
+          placeholder="Description of the token"
           value={form?.description}
           onChange={(evt) =>
             handleSetForm({
