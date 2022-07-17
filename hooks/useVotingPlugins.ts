3a34e90be148e02c7fe22df27cd4d40eeb3dfa3d
dankelleher <dankelleher@yahoo.com>
2022-07-17 06:56:55 +0200

    Remove attempted registration of gatekeeper network when using the nft plugin

--- hooks/useVotingPlugins.ts
+++ hooks/useVotingPlugins.ts
@@ -464,7 +464,6 @@ export function useVotingPlugins() {
     if (usedCollectionsPks.length && realm) {
       if (connected && currentClient.walletPk?.toBase58()) {
         handleGetNfts()
-        handleRegisterGatekeeperNetwork()
       }
       handleMaxVoterWeight()
     } else if (realm) {
