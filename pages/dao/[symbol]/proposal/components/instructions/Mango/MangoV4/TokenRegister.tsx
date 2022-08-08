72992a1bc188a98ac1ae905259da95373c3fc87a
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-08-08 18:00:10 +0200

    Feature/mango v4 inst (#926)
    * token register instruction
    
    * token edit and register
    
    * serum 3 register market
    
    * fix + perpcreate wip
    
    * perp create
    
    * perp edit
    
    * iinstructions in form
    
    * register trustless
    
    * fix
    
    * fixes
    
    * fix
    
    * fix
    
    * fixes

---
+++ pages/dao/[symbol]/proposal/components/instructions/Mango/MangoV4/TokenRegister.tsx
@@ -0,0 +1,312 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import React, { useContext, useEffect, useState } from 'react'
+import useRealm from '@hooks/useRealm'
+import { PublicKey, SYSVAR_RENT_PUBKEY } from '@solana/web3.js'
+import * as yup from 'yup'
+import { isFormValid } from '@utils/formValidation'
+import { UiInstruction } from '@utils/uiTypes/proposalCreationTypes'
+import { NewProposalContext } from '../../../../new'
+import useGovernanceAssets from '@hooks/useGovernanceAssets'
+import { Governance } from '@solana/spl-governance'
+import { ProgramAccount } from '@solana/spl-governance'
+import useWalletStore from 'stores/useWalletStore'
+import { serializeInstructionToBase64 } from '@solana/spl-governance'
+import { I80F48 } from '@blockworks-foundation/mango-client'
+import { AccountType, AssetAccount } from '@utils/uiTypes/assets'
+import InstructionForm, {
+  InstructionInput,
+  InstructionInputType,
+} from '../../FormCreator'
+import UseMangoV4 from '../../../../../../../../hooks/useMangoV4'
+
+interface RegisterTokenForm {
+  governedAccount: AssetAccount | null
+  mintPk: string
+  oraclePk: string
+  oracleConfFilter: number
+  tokenIndex: number
+  name: string
+  adjustmentFactor: number
+  util0: number
+  rate0: number
+  util1: number
+  rate1: number
+  maxRate: number
+  loanFeeRate: number
+  loanOriginationFeeRate: number
+  maintAssetWeight: number
+  initAssetWeight: number
+  maintLiabWeight: number
+  initLiabWeight: number
+  liquidationFee: number
+}
+
+const TokenRegister = ({
+  index,
+  governance,
+}: {
+  index: number
+  governance: ProgramAccount<Governance> | null
+}) => {
+  const wallet = useWalletStore((s) => s.current)
+  const { getClient, ADMIN_PK, GROUP_NUM } = UseMangoV4()
+  const { realmInfo } = useRealm()
+  const { assetAccounts } = useGovernanceAssets()
+  const governedProgramAccounts = assetAccounts.filter(
+    (x) => x.type === AccountType.PROGRAM
+  )
+  const { connection } = useWalletStore()
+  const shouldBeGoverned = index !== 0 && governance
+  const programId: PublicKey | undefined = realmInfo?.programId
+  const [form, setForm] = useState<RegisterTokenForm>({
+    governedAccount: null,
+    mintPk: '',
+    oraclePk: '',
+    oracleConfFilter: 0,
+    tokenIndex: 0,
+    name: '',
+    adjustmentFactor: 0,
+    util0: 0,
+    rate0: 0,
+    util1: 0,
+    rate1: 0,
+    maxRate: 0,
+    loanFeeRate: 0,
+    loanOriginationFeeRate: 0,
+    maintAssetWeight: 0,
+    initAssetWeight: 0,
+    maintLiabWeight: 0,
+    initLiabWeight: 0,
+    liquidationFee: 0,
+  })
+  const [formErrors, setFormErrors] = useState({})
+  const { handleSetInstructions } = useContext(NewProposalContext)
+  const handleSetForm = ({ propertyName, value }) => {
+    setFormErrors({})
+    setForm({ ...form, [propertyName]: value })
+  }
+  const validateInstruction = async (): Promise<boolean> => {
+    const { isValid, validationErrors } = await isFormValid(schema, form)
+    setFormErrors(validationErrors)
+    return isValid
+  }
+  async function getInstruction(): Promise<UiInstruction> {
+    const isValid = await validateInstruction()
+    let serializedInstruction = ''
+    if (
+      isValid &&
+      programId &&
+      form.governedAccount?.governance?.account &&
+      wallet?.publicKey
+    ) {
+      const client = await getClient(connection, wallet)
+      const group = await client.getGroupForCreator(ADMIN_PK, GROUP_NUM)
+      const tokenIndex = group.banksMap.size
+      //Mango instruction call and serialize
+      //TODO dao sol account as payer
+      const ix = await client.program.methods
+        .tokenRegister(
+          tokenIndex,
+          form.name.toUpperCase(),
+          {
+            confFilter: {
+              val: I80F48.fromNumber(Number(form.oracleConfFilter)).getData(),
+            },
+          } as any, // future: nested custom types dont typecheck, fix if possible?
+          {
+            adjustmentFactor: Number(form.adjustmentFactor),
+            util0: Number(form.util0),
+            rate0: Number(form.rate0),
+            util1: Number(form.util1),
+            rate1: Number(form.rate1),
+            maxRate: Number(form.maxRate),
+          },
+          Number(form.loanFeeRate),
+          Number(form.loanOriginationFeeRate),
+          Number(form.maintAssetWeight),
+          Number(form.initAssetWeight),
+          Number(form.maintLiabWeight),
+          Number(form.initLiabWeight),
+          Number(form.liquidationFee)
+        )
+        .accounts({
+          group: group.publicKey,
+          admin: ADMIN_PK,
+          mint: new PublicKey(form.mintPk),
+          oracle: new PublicKey(form.oraclePk),
+          payer: wallet.publicKey,
+          rent: SYSVAR_RENT_PUBKEY,
+        })
+        .instruction()
+
+      serializedInstruction = serializeInstructionToBase64(ix)
+    }
+    const obj: UiInstruction = {
+      serializedInstruction: serializedInstruction,
+      isValid,
+      governance: form.governedAccount?.governance,
+    }
+    return obj
+  }
+  useEffect(() => {
+    handleSetForm({
+      propertyName: 'programId',
+      value: programId?.toString(),
+    })
+  }, [realmInfo?.programId])
+  useEffect(() => {
+    handleSetInstructions(
+      { governedAccount: form.governedAccount?.governance, getInstruction },
+      index
+    )
+  }, [form])
+  const schema = yup.object().shape({
+    governedAccount: yup
+      .object()
+      .nullable()
+      .required('Program governed account is required'),
+  })
+  const inputs: InstructionInput[] = [
+    {
+      label: 'Governance',
+      initialValue: form.governedAccount,
+      name: 'governedAccount',
+      type: InstructionInputType.GOVERNED_ACCOUNT,
+      shouldBeGoverned: shouldBeGoverned as any,
+      governance: governance,
+      options: governedProgramAccounts,
+    },
+    {
+      label: 'Mint Pk',
+      initialValue: form.mintPk,
+      type: InstructionInputType.INPUT,
+      name: 'mintPk',
+    },
+    {
+      label: 'Oracle Pk',
+      initialValue: form.oraclePk,
+      type: InstructionInputType.INPUT,
+      name: 'oraclePk',
+    },
+    {
+      label: 'Oracle Configuration Filter',
+      initialValue: form.oracleConfFilter,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'oracleConfFilter',
+    },
+    {
+      label: 'Token Name',
+      initialValue: form.name,
+      type: InstructionInputType.INPUT,
+      name: 'name',
+    },
+    {
+      label: 'Adjustment Factor',
+      initialValue: form.adjustmentFactor,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'adjustmentFactor',
+    },
+    {
+      label: 'Util 0',
+      initialValue: form.util0,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'util0',
+    },
+    {
+      label: 'Rate 0',
+      initialValue: form.rate0,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'rate0',
+    },
+    {
+      label: 'Util 1',
+      initialValue: form.util1,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'util1',
+    },
+    {
+      label: 'Rate 1',
+      initialValue: form.rate1,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'rate1',
+    },
+    {
+      label: 'Max Rate',
+      initialValue: form.maxRate,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'maxRate',
+    },
+    {
+      label: 'Loan Fee Rate',
+      initialValue: form.loanFeeRate,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'loanFeeRate',
+    },
+    {
+      label: 'Loan Origination Fee Rate',
+      initialValue: form.loanOriginationFeeRate,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'loanOriginationFeeRate',
+    },
+    {
+      label: 'Maint Asset Weight',
+      initialValue: form.maintAssetWeight,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'maintAssetWeight',
+    },
+    {
+      label: 'Init Asset Weight',
+      initialValue: form.initAssetWeight,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'initAssetWeight',
+    },
+    {
+      label: 'Maint Liab Weight',
+      initialValue: form.maintLiabWeight,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'maintLiabWeight',
+    },
+    {
+      label: 'Init Liab Weight',
+      initialValue: form.initLiabWeight,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'initLiabWeight',
+    },
+    {
+      label: 'Liquidation Fee',
+      initialValue: form.liquidationFee,
+      type: InstructionInputType.INPUT,
+      inputType: 'number',
+      name: 'liquidationFee',
+    },
+  ]
+
+  return (
+    <>
+      {form && (
+        <InstructionForm
+          outerForm={form}
+          setForm={setForm}
+          inputs={inputs}
+          setFormErrors={setFormErrors}
+          formErrors={formErrors}
+        ></InstructionForm>
+      )}
+    </>
+  )
+}
+
+export default TokenRegister
