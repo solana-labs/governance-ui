43771c39f06c0e6359a532873ca089d7bec3fca0
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-08-25 01:24:03 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -21,20 +21,6 @@ config = withTM({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
-    config.module.rules.push({
-      test: /\.(ts)x?$/, // Just `tsx?` file only
-      use: [
-        // options.defaultLoaders.babel, I don't think it's necessary to have this loader too
-        {
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: true,
-            experimentalWatchApi: true,
-            onlyCompileBundledFiles: true,
-          },
-        },
-      ],
-    })
     if (!isServer) config.resolve.fallback.fs = false
     return config
   },
