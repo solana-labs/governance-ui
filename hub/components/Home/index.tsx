c85599bae301bd292f5740ca731199fa18ce702b
Niranjan Ramadas <mail@niranjan.me>
2022-09-28 13:55:40 -0400

    Add short descriptions to Realms

--- hub/components/Home/index.tsx
+++ hub/components/Home/index.tsx
@@ -45,16 +45,12 @@ export function Home(props: Props) {
           ({ realm }) => (
             <HomeLayout
               bannerUrl={
-                realm.bannerImageUrl ||
-                (realm.publicKey.toBase58() ===
-                'DA5G7QQbFioZ6K33wQcH8fVdgFcnaDjLD7DLQkapZg5X'
-                  ? 'https://dao.metaplex.com/img/bg-metaplex.svg'
-                  : getDefaultBannerUrl(realm.publicKey))
+                realm.bannerImageUrl || getDefaultBannerUrl(realm.publicKey)
               }
               sidebar={(isStickied) => (
                 <Sidebar.Content
                   compressed={isStickied}
-                  description={null}
+                  description={realm.shortDescription}
                   iconUrl={realm.iconUrl}
                   membersCount={realm.membersCount}
                   realm={realm.publicKey}
