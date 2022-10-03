c5ca579e61b07f1e8fe61f25caa4d950648ee10a
agrippa kellum <ask252@cornell.edu>
2022-10-03 17:51:07 -0400

    BaseGovernanceFormV3 includes both council and community now

--- components/AssetsList/BaseGovernanceForm.tsx
+++ components/AssetsList/BaseGovernanceForm.tsx
@@ -3,27 +3,21 @@ import Select from '@components/inputs/Select'
 import AmountSlider from '@components/Slider'
 import Switch from '@components/Switch'
 import useRealm from '@hooks/useRealm'
-import {
-  GovernanceConfig,
-  VoteThreshold,
-  VoteThresholdType,
-  VoteTipping,
-} from '@solana/spl-governance'
+import { GovernanceConfig, VoteTipping } from '@solana/spl-governance'
 import { DISABLED_VOTER_WEIGHT } from '@tools/constants'
 import {
   fmtPercentage,
-  getDaysFromTimestamp,
   getMintMinAmountAsDecimal,
   getMintNaturalAmountFromDecimal,
   getMintSupplyFractionAsDecimalPercentage,
   getMintSupplyPercentageAsDecimal,
-  getTimestampFromDays,
   parseMintNaturalAmountFromDecimal,
 } from '@tools/sdk/units'
 import BigNumber from 'bignumber.js'
 import { max, min } from 'bn.js'
 import produce from 'immer'
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { BaseGovernanceFormV3 } from './BaseGovernanceFormV3'
 
 export interface BaseGovernanceFormFieldsV2 {
   _programVersion: 2
@@ -252,229 +246,6 @@ const BaseGovernanceFormV2 = ({
     </>
   )
 }
-const BaseGovernanceFormV3 = ({
-  formErrors,
-  form,
-  setForm,
-  setFormErrors,
-}: {
-  formErrors: any
-  setForm: React.Dispatch<React.SetStateAction<BaseGovernanceFormFieldsV3>>
-  setFormErrors: any
-  form: BaseGovernanceFormFieldsV3
-}) => {
-  const { realmInfo, mint: realmMint } = useRealm()
-
-  const isMaxMinCommunityNumber =
-    form.minCommunityTokensToCreateProposal.toString() ===
-    DISABLED_VOTER_WEIGHT.toString()
-  const [showMinCommunity, setMinCommunity] = useState(!isMaxMinCommunityNumber)
-
-  // @asktree: unclear that this should not just be an effect in the parent
-  const handleSetForm = ({ propertyName, value }) => {
-    setFormErrors({})
-    setForm({ ...form, [propertyName]: value })
-  }
-
-  const validateMinMax = (e) => {
-    const fieldName = e.target.name
-    const min = e.target.min || 0
-    const max = e.target.max || Number.MAX_SAFE_INTEGER
-    const value = form[fieldName]
-    const currentPrecision = new BigNumber(min).decimalPlaces()
-
-    handleSetForm({
-      value:
-        e.target.value === DISABLED_VOTER_WEIGHT.toString()
-          ? DISABLED_VOTER_WEIGHT.toString()
-          : parseFloat(
-              Math.max(
-                Number(min),
-                Math.min(Number(max), Number(value))
-              ).toFixed(currentPrecision)
-            ),
-      propertyName: fieldName,
-    })
-  }
-
-  // TODO @asktree: all this stuff should really just wait on realmInfo instead of having weird defaults
-  // Use 1% of mint supply as the default value for minTokensToCreateProposal and the default increment step in the input editor
-  const mintSupply1Percent = realmMint
-    ? getMintSupplyPercentageAsDecimal(realmMint, 1)
-    : 100
-  const minTokenAmount = realmMint
-    ? getMintMinAmountAsDecimal(realmMint)
-    : 0.0001
-  // If the supply is small and 1% is below the minimum mint amount then coerce to the minimum value
-  const minTokenStep = Math.max(mintSupply1Percent ?? 0, minTokenAmount)
-
-  const getSupplyPercent = () => {
-    const hasMinTokensPercentage =
-      !!minCommunityTokensPercentage && !isNaN(minCommunityTokensPercentage)
-    const percent =
-      hasMinTokensPercentage && minCommunityTokensPercentage
-        ? fmtPercentage(minCommunityTokensPercentage)
-        : ''
-    return hasMinTokensPercentage && <>{`${percent} of token supply`}</>
-  }
-
-  const minCommunityTokensPercentage = useMemo(() => {
-    if (form.minCommunityTokensToCreateProposal === undefined) return undefined
-    if (realmMint === undefined) return undefined
-
-    const communityMinTokens = realmMint
-      ? parseMintNaturalAmountFromDecimal(
-          form.minCommunityTokensToCreateProposal.toString(),
-          realmMint.decimals
-        )
-      : 0
-
-    return getMintSupplyFractionAsDecimalPercentage(
-      realmMint,
-      communityMinTokens
-    )
-  }, [form.minCommunityTokensToCreateProposal, realmInfo?.symbol])
-
-  return (
-    <>
-      <div className="text-sm mb-3 flex flex-col gap-4">
-        <Input
-          label="min instruction hold up time (days)"
-          value={getDaysFromTimestamp(form.minInstructionHoldUpTime)}
-          type="number"
-          min={0}
-          name="minInstructionHoldUpTime"
-          onBlur={validateMinMax}
-          onChange={(evt) =>
-            handleSetForm({
-              value: getTimestampFromDays(parseInt(evt.target.value)),
-              propertyName: 'minInstructionHoldUpTime',
-            })
-          }
-          error={formErrors['minInstructionHoldUpTime']}
-        />
-        <Input
-          label="Max voting time (days)"
-          value={getDaysFromTimestamp(form.maxVotingTime)}
-          name="maxVotingTime"
-          type="number"
-          min={0.01}
-          onBlur={validateMinMax}
-          onChange={(evt) =>
-            handleSetForm({
-              value: getTimestampFromDays(parseInt(evt.target.value)),
-              propertyName: 'maxVotingTime',
-            })
-          }
-          error={formErrors['maxVotingTime']}
-        />
-        <div>
-          <div className="mb-2">Min community tokens to create proposal</div>
-          <div className="flex flex-row text-xs items-center">
-            <Switch
-              checked={showMinCommunity}
-              onChange={() => {
-                setMinCommunity(!showMinCommunity)
-                if (!showMinCommunity === true) {
-                  handleSetForm({
-                    value: 1,
-                    propertyName: 'minCommunityTokensToCreateProposal',
-                  })
-                } else {
-                  handleSetForm({
-                    value: DISABLED_VOTER_WEIGHT.toString(),
-                    propertyName: 'minCommunityTokensToCreateProposal',
-                  })
-                }
-              }}
-            />{' '}
-            <div className="ml-3">
-              {showMinCommunity ? 'Enabled' : 'Disabled'}
-            </div>
-          </div>
-          <div className="mt-2">
-            {showMinCommunity && (
-              <Input
-                value={form.minCommunityTokensToCreateProposal}
-                type="number"
-                name="minCommunityTokensToCreateProposal"
-                min={minTokenAmount}
-                step={minTokenStep}
-                onBlur={validateMinMax}
-                onChange={(evt) =>
-                  handleSetForm({
-                    value: evt.target.value,
-                    propertyName: 'minCommunityTokensToCreateProposal',
-                  })
-                }
-                error={formErrors['minCommunityTokensToCreateProposal']}
-              />
-            )}
-            <span className="text-gray-200 whitespace-nowrap">
-              ({showMinCommunity && getSupplyPercent()})
-            </span>
-          </div>
-        </div>
-        <Input
-          label="Community yes vote threshold (%)"
-          // TODO handle disabled case
-          value={form.communityVoteThreshold.value}
-          max={100}
-          min={1}
-          name="voteThreshold"
-          type="number"
-          onChange={(evt) => {
-            const x = parseInt(evt.target.value)
-            setForm((prev) => ({
-              ...prev,
-              communityVoteThreshold: new VoteThreshold({
-                type: VoteThresholdType.YesVotePercentage,
-                value: Math.max(0, Math.min(100, x)),
-              }),
-            }))
-          }}
-          error={formErrors['communityVoteThreshold']}
-        />
-        <div className="max-w-lg pb-5">
-          <AmountSlider
-            step={1}
-            // TODO handle disabled case
-            value={form.communityVoteThreshold.value!}
-            disabled={false}
-            onChange={(x) => {
-              setForm((prev) => ({
-                ...prev,
-                communityVoteThreshold: new VoteThreshold({
-                  type: VoteThresholdType.YesVotePercentage,
-                  value: Math.max(0, Math.min(100, x)),
-                }),
-              }))
-            }}
-          />
-        </div>
-        <Select
-          label="Community vote tipping"
-          value={VoteTipping[form.communityVoteTipping]}
-          onChange={(selected) =>
-            handleSetForm({
-              value: selected,
-              propertyName: 'communityVoteTipping',
-            })
-          }
-        >
-          {Object.keys(VoteTipping)
-            .filter((vt) => typeof VoteTipping[vt as any] === 'string')
-            .map((vt) => (
-              <Select.Option key={vt} value={vt}>
-                {VoteTipping[vt as any]}{' '}
-              </Select.Option>
-            ))}
-        </Select>{' '}
-      </div>
-    </>
-  )
-}
-
 const BaseGovernanceForm = ({
   form,
   ...props
---
+++ components/AssetsList/BaseGovernanceFormV3.tsx
@@ -0,0 +1,296 @@
+import Input from '@components/inputs/Input'
+import Select from '@components/inputs/Select'
+import AmountSlider from '@components/Slider'
+import Switch from '@components/Switch'
+import useRealm from '@hooks/useRealm'
+import {
+  VoteThreshold,
+  VoteThresholdType,
+  VoteTipping,
+} from '@solana/spl-governance'
+import { DISABLED_VOTER_WEIGHT } from '@tools/constants'
+import {
+  fmtPercentage,
+  getDaysFromTimestamp,
+  getMintMinAmountAsDecimal,
+  getMintSupplyFractionAsDecimalPercentage,
+  getMintSupplyPercentageAsDecimal,
+  getTimestampFromDays,
+  parseMintNaturalAmountFromDecimal,
+} from '@tools/sdk/units'
+import BN from 'bn.js'
+import React, { useEffect, useMemo } from 'react'
+import { BaseGovernanceFormFieldsV3 } from './BaseGovernanceForm'
+
+export const BaseGovernanceFormV3 = ({
+  formErrors,
+  form,
+  setForm,
+  setFormErrors,
+}: {
+  formErrors: any
+  setForm: React.Dispatch<React.SetStateAction<BaseGovernanceFormFieldsV3>>
+  setFormErrors: any
+  form: BaseGovernanceFormFieldsV3
+}) => {
+  const { realmInfo, mint: realmMint } = useRealm()
+
+  // @asktree: unclear that this should not just be an effect in the parent, I am just replicating the behavior of previous components
+  useEffect(() => {
+    setFormErrors({})
+  }, [form])
+
+  // TODO @asktree: all this stuff should really just wait on realmInfo instead of having weird defaults
+  // Use 1% of mint supply as the default value for minTokensToCreateProposal and the default increment step in the input editor
+  const mintSupply1Percent = realmMint
+    ? getMintSupplyPercentageAsDecimal(realmMint, 1)
+    : 100
+  const minTokenAmount = realmMint
+    ? getMintMinAmountAsDecimal(realmMint)
+    : 0.0001
+  // If the supply is small and 1% is below the minimum mint amount then coerce to the minimum value
+  const minTokenStep = Math.max(mintSupply1Percent ?? 0, minTokenAmount)
+
+  // TODO figure out what to do for council too
+  const getSupplyPercent = () => {
+    const hasMinTokensPercentage =
+      !!minCommunityTokensPercentage && !isNaN(minCommunityTokensPercentage)
+    const percent =
+      hasMinTokensPercentage && minCommunityTokensPercentage
+        ? fmtPercentage(minCommunityTokensPercentage)
+        : ''
+    return hasMinTokensPercentage && <>{`(${percent} of token supply)`}</>
+  }
+
+  const minCommunityTokensPercentage = useMemo(() => {
+    if (form.minCommunityTokensToCreateProposal === undefined) return undefined
+    if (realmMint === undefined) return undefined
+
+    const communityMinTokens = realmMint
+      ? parseMintNaturalAmountFromDecimal(
+          form.minCommunityTokensToCreateProposal.toString(),
+          realmMint.decimals
+        )
+      : 0
+
+    return getMintSupplyFractionAsDecimalPercentage(
+      realmMint,
+      communityMinTokens
+    )
+  }, [form.minCommunityTokensToCreateProposal, realmInfo?.symbol])
+
+  return (
+    <>
+      <div className="text-sm mb-3 flex flex-col gap-4">
+        <Input
+          label="min instruction hold up time (days)"
+          value={getDaysFromTimestamp(form.minInstructionHoldUpTime)}
+          type="number"
+          min={0}
+          name="minInstructionHoldUpTime"
+          onChange={(evt) =>
+            setForm((prev) => ({
+              ...prev,
+              minInstructionHoldUpTime: getTimestampFromDays(
+                parseInt(evt.target.value)
+              ),
+            }))
+          }
+          error={formErrors['minInstructionHoldUpTime']}
+        />
+        <Input
+          label="Max voting time (days)"
+          value={getDaysFromTimestamp(form.maxVotingTime)}
+          name="maxVotingTime"
+          type="number"
+          min={0.01}
+          onChange={(evt) =>
+            setForm((prev) => ({
+              ...prev,
+              maxVotingTime: getTimestampFromDays(parseInt(evt.target.value)),
+            }))
+          }
+          error={formErrors['maxVotingTime']}
+        />
+        {(['community', 'council'] as const).map((govPop) => {
+          const capitalized = govPop === 'community' ? 'Community' : 'Council'
+          const minProposalTokensEnabled =
+            form[
+              govPop === 'community'
+                ? 'minCommunityTokensToCreateProposal'
+                : 'minCouncilTokensToCreateProposal'
+            ].toString() !== DISABLED_VOTER_WEIGHT.toString()
+
+          return (
+            <>
+              <div>
+                <div className="mb-2">
+                  Min {govPop} tokens to create proposal
+                </div>
+                <div className="flex flex-row text-xs items-center">
+                  <Switch
+                    // TODO
+                    checked={minProposalTokensEnabled}
+                    onChange={() => {
+                      setForm((prev) =>
+                        !minProposalTokensEnabled
+                          ? {
+                              ...prev,
+                              ...(govPop === 'community'
+                                ? {
+                                    minCommunityTokensToCreateProposal: new BN(
+                                      1
+                                    ),
+                                  }
+                                : {
+                                    minCouncilTokensToCreateProposal: new BN(1),
+                                  }),
+                            }
+                          : {
+                              ...prev,
+                              ...(govPop === 'community'
+                                ? {
+                                    minCommunityTokensToCreateProposal: DISABLED_VOTER_WEIGHT,
+                                  }
+                                : {
+                                    minCouncilTokensToCreateProposal: DISABLED_VOTER_WEIGHT,
+                                  }),
+                            }
+                      )
+                    }}
+                  />{' '}
+                  <div className="ml-3">
+                    {minProposalTokensEnabled ? 'Enabled' : 'Disabled'}
+                  </div>
+                </div>
+                <div className="mt-2">
+                  {minProposalTokensEnabled && (
+                    <Input
+                      value={
+                        govPop === 'community'
+                          ? form.minCommunityTokensToCreateProposal
+                          : form.minCouncilTokensToCreateProposal
+                      }
+                      type="number"
+                      name="minCommunityTokensToCreateProposal"
+                      min={minTokenAmount}
+                      step={minTokenStep}
+                      onChange={(evt) =>
+                        setForm((prev) => ({
+                          ...prev,
+                          ...(govPop === 'community'
+                            ? {
+                                minCommunityTokensToCreateProposal: new BN(
+                                  evt.target.value
+                                ),
+                              }
+                            : {
+                                minCouncilTokensToCreateProposal: new BN(
+                                  evt.target.value
+                                ),
+                              }),
+                        }))
+                      }
+                      error={
+                        formErrors[`min${capitalized}TokensToCreateProposal`]
+                      }
+                    />
+                  )}
+                  <span className="text-gray-200 whitespace-nowrap">
+                    {minProposalTokensEnabled && getSupplyPercent()}
+                  </span>
+                </div>
+              </div>
+              <Input
+                label={`${capitalized} yes vote threshold (%)`}
+                // TODO handle disabled case
+                value={
+                  govPop === 'community'
+                    ? form.communityVoteThreshold.value
+                    : form.councilVoteThreshold.value
+                }
+                max={100}
+                min={1}
+                name={govPop + 'Threshold'}
+                type="number"
+                onChange={(evt) => {
+                  const x = parseInt(evt.target.value)
+
+                  // TODO make type safe
+                  const y =
+                    govPop === 'community'
+                      ? 'communityVoteThreshold'
+                      : 'councilVoteThreshold'
+                  setForm((prev) => ({
+                    ...prev,
+                    [y]: new VoteThreshold({
+                      type: VoteThresholdType.YesVotePercentage,
+                      value: Math.max(0, Math.min(100, x)),
+                    }),
+                  }))
+                }}
+                error={formErrors[govPop + 'VoteThreshold']}
+              />
+              <div className="max-w-lg pb-5">
+                <AmountSlider
+                  step={1}
+                  // TODO handle disabled case
+                  value={
+                    govPop === 'community'
+                      ? form.communityVoteThreshold.value!
+                      : form.councilVoteThreshold.value!
+                  }
+                  disabled={false}
+                  onChange={(x) => {
+                    // this is in fact type-safe!
+                    const y =
+                      govPop === 'community'
+                        ? 'communityVoteThreshold'
+                        : 'councilVoteThreshold'
+                    setForm((prev) => ({
+                      ...prev,
+                      [y]: new VoteThreshold({
+                        type: VoteThresholdType.YesVotePercentage,
+                        value: Math.max(0, Math.min(100, x)),
+                      }),
+                    }))
+                  }}
+                />
+              </div>
+              <Select
+                label={`${capitalized} vote tipping`}
+                value={
+                  VoteTipping[
+                    govPop === 'community'
+                      ? form.communityVoteTipping
+                      : form.councilVoteTipping
+                  ]
+                }
+                onChange={(selected) =>
+                  setForm((prev) => ({
+                    ...prev,
+                    ...(govPop === 'community'
+                      ? {
+                          communityVoteTipping: selected,
+                        }
+                      : {
+                          councilVoteTipping: selected,
+                        }),
+                  }))
+                }
+              >
+                {Object.keys(VoteTipping)
+                  .filter((vt) => typeof VoteTipping[vt as any] === 'string')
+                  .map((vt) => (
+                    <Select.Option key={vt} value={vt}>
+                      {VoteTipping[vt as any]}{' '}
+                    </Select.Option>
+                  ))}
+              </Select>
+            </>
+          )
+        })}
+      </div>
+    </>
+  )
+}
