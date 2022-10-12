d644741114fb0a5fa770376039d4aea04f81ae80
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-10-12 03:07:58 +0200

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,4 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 const Index = () => {
-  const router = useRouter()
-
-  useEffect(() => {
-    const { REALM } = process.env
-    const mainUrl = REALM ? `/dao/${REALM}` : '/realms'
-    router.replace(mainUrl)
-  }, [])
-
   return null
 }
 
