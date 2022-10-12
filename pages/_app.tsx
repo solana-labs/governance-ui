135f9c8471068140b40669368d137b6da7b427d7
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-10-12 02:45:52 +0200

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,14 @@
 import type { AppProps } from 'next/app'
 import '@dialectlabs/react-ui/index.css'
 
-import { App as BaseApp } from '@components/App'
-import { App as HubApp } from '@hub/App'
-
 import '../styles/index.css'
 import '../styles/typography.css'
 import '@hub/components/controls/RichTextEditor/index.css'
 
-export default function App({ Component, pageProps, router }: AppProps) {
+export default function App({ router }: AppProps) {
   if (router.pathname.startsWith('/realm/[id]')) {
-    return (
-      <HubApp>
-        <Component {...pageProps} />
-      </HubApp>
-    )
+    return <div>Offline</div>
   }
 
-  return (
-    <BaseApp>
-      <Component {...pageProps} />
-    </BaseApp>
-  )
+  return <div>Offline</div>
 }
