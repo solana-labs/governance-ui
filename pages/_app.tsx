5cb89a2fa0fadc2dccf96d3e433f77a412d2b610
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-10-12 03:55:59 +0200

    Revert "Feature/fix 222 (#1087)"
    This reverts commit 009e1b3663a830732b9bef09b4586709228986c8.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
+import type { AppProps } from 'next/app'
 import '@dialectlabs/react-ui/index.css'
 
 import '../styles/index.css'
 import '../styles/typography.css'
 import '@hub/components/controls/RichTextEditor/index.css'
 
-export default function App() {
+export default function App({ router }: AppProps) {
+  if (router.pathname.startsWith('/realm/[id]')) {
+    return <div>Offline</div>
+  }
+
   return <div>Offline</div>
 }
