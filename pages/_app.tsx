ae32bf3b8a60030dc8a62321c412af1146dad7f4
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-10-12 03:56:13 +0200

    Revert "fix (#1086)"
    This reverts commit f13cdb455273f5ea0ae8705c3b3e7e9f91026884.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,25 @@
 import type { AppProps } from 'next/app'
 import '@dialectlabs/react-ui/index.css'
 
+import { App as BaseApp } from '@components/App'
+import { App as HubApp } from '@hub/App'
+
 import '../styles/index.css'
 import '../styles/typography.css'
 import '@hub/components/controls/RichTextEditor/index.css'
 
-export default function App({ router }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
   if (router.pathname.startsWith('/realm/[id]')) {
-    return <div>Offline</div>
+    return (
+      <HubApp>
+        <Component {...pageProps} />
+      </HubApp>
+    )
   }
 
-  return <div>Offline</div>
+  return (
+    <BaseApp>
+      <Component {...pageProps} />
+    </BaseApp>
+  )
 }
