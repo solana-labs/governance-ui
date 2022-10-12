009e1b3663a830732b9bef09b4586709228986c8
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-10-12 03:09:24 +0200

    Feature/fix 222 (#1087)
    * fix
    
    * fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,9 @@
-import type { AppProps } from 'next/app'
 import '@dialectlabs/react-ui/index.css'
 
 import '../styles/index.css'
 import '../styles/typography.css'
 import '@hub/components/controls/RichTextEditor/index.css'
 
-export default function App({ router }: AppProps) {
-  if (router.pathname.startsWith('/realm/[id]')) {
-    return <div>Offline</div>
-  }
-
+export default function App() {
   return <div>Offline</div>
 }
