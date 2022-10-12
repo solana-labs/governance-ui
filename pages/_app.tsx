92a222acffa94be9912f36d9e0e25564bc56078c
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-10-12 04:03:24 +0200

    Feature/revert fixes (#1089)
    * Revert "Feature/fix 222 (#1087)"
    
    This reverts commit 009e1b3663a830732b9bef09b4586709228986c8.
    
    * Revert "fix (#1086)"
    
    This reverts commit f13cdb455273f5ea0ae8705c3b3e7e9f91026884.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,25 @@
+import type { AppProps } from 'next/app'
 import '@dialectlabs/react-ui/index.css'
+
+import { App as BaseApp } from '@components/App'
+import { App as HubApp } from '@hub/App'
 
 import '../styles/index.css'
 import '../styles/typography.css'
 import '@hub/components/controls/RichTextEditor/index.css'
 
-export default function App() {
-  return <div>Offline</div>
+export default function App({ Component, pageProps, router }: AppProps) {
+  if (router.pathname.startsWith('/realm/[id]')) {
+    return (
+      <HubApp>
+        <Component {...pageProps} />
+      </HubApp>
+    )
+  }
+
+  return (
+    <BaseApp>
+      <Component {...pageProps} />
+    </BaseApp>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 const Index = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const { REALM } = process.env
+    const mainUrl = REALM ? `/dao/${REALM}` : '/realms'
+    router.replace(mainUrl)
+  }, [])
+
   return null
 }
 
