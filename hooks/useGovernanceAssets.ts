a551051b64a5697b69ebe024cf9052215db12607
Adrian Brzeziński <a.brzezinski94@gmail.com>
2022-10-21 11:48:29 +0200

    Feature/claim mango tokens (#1106)
    * claim table
    
    * fix
    
    * claim wrap up
    
    * fixes
    
    * fix

--- hooks/useGovernanceAssets.ts
+++ hooks/useGovernanceAssets.ts
@@ -428,6 +428,11 @@ export default function useGovernanceAssets() {
       isVisible: canUseTokenTransferInstruction,
     },
     {
+      id: Instructions.ClaimMangoTokens,
+      name: 'Mango: Claim Tokens',
+      isVisible: canUseTokenTransferInstruction,
+    },
+    {
       id: Instructions.DepositIntoVolt,
       name: 'Friktion: Deposit into Volt',
       isVisible: canUseAnyInstruction,
