ab10d428b88cf0dbe6532f2acf0bdf262abb2bb5
agrippa <ask252@cornell.edu>
2022-11-11 14:17:41 -0500

    Introduce react-query + use it to discriminate program version in the creation wizard (#1208)
    * add @tanstack/react-query
    
    * use react-query to ask about programversion
    
    * display version and loading status nicely
    
    * remove unused variable
    
    * delete queryKeys file -- maybe later.
    
    * revert unchanged file
    
    * remove console logs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
 import '@dialectlabs/react-ui/index.css'
+import { QueryClientProvider } from '@tanstack/react-query'
 
+import queryClient from '@hooks/queries/queryClient'
 import { App as BaseApp } from '@components/App'
 import { App as HubApp } from '@hub/App'
 
@@ -23,22 +25,19 @@ export default function App({ Component, pageProps, router }: AppProps) {
   if (router.pathname.startsWith('/code')) {
     return <Component {...pageProps} />
   }
-
-  if (
-    router.pathname.startsWith('/realm/[id]') ||
-    router.pathname.startsWith('/ecosystem') ||
-    router.pathname.startsWith('/discover')
-  ) {
-    return (
-      <HubApp>
-        <Component {...pageProps} />
-      </HubApp>
-    )
-  }
-
   return (
-    <BaseApp>
-      <Component {...pageProps} />
-    </BaseApp>
+    <QueryClientProvider client={queryClient}>
+      {router.pathname.startsWith('/realm/[id]') ||
+      router.pathname.startsWith('/ecosystem') ||
+      router.pathname.startsWith('/discover') ? (
+        <HubApp>
+          <Component {...pageProps} />
+        </HubApp>
+      ) : (
+        <BaseApp>
+          <Component {...pageProps} />
+        </BaseApp>
+      )}
+    </QueryClientProvider>
   )
 }
