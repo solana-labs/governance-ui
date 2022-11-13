7690a3af0c52e1055d47747e415c57ddbea2e46a
agrippa kellum <ask252@cornell.edu>
2022-11-13 14:56:12 -0500

    deps

--- components/NewRealmWizard/components/steps/YesVotePercentageThresholdForm.tsx
+++ components/NewRealmWizard/components/steps/YesVotePercentageThresholdForm.tsx
@@ -87,8 +87,7 @@ export default function YesVotePercentageForm({
         : CouncilYesVotePercentageSchema,
       setValue
     )
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- TODO please fix, it can cause difficult bugs. You might wanna check out https://bobbyhadz.com/blog/react-hooks-exhaustive-deps for info. -@asktree
-  }, [])
+  }, [forCommunity, formData, setValue])
 
   function serializeValues(values) {
     onSubmit({ step: currentStep, data: values })
