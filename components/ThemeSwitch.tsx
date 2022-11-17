c3e125831a7135bdff02fed35ab32ba664ec18a1
cbergz <charlie.bergman@gmail.com>
2022-11-17 17:05:25 -0500

    another attempted at theme

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,31 +1,30 @@
-// import { useEffect, useState } from 'react'
-// import { useTheme } from 'next-themes'
-// import { MoonIcon, SunIcon } from '@heroicons/react/outline'
+import { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
+import { MoonIcon, SunIcon } from '@heroicons/react/outline'
 
-// const ThemeSwitch = () => {
-//   const { theme, setTheme } = useTheme()
-//   const [mounted, setMounted] = useState(false)
+const ThemeSwitch = () => {
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
 
-//   useEffect(() => setMounted(true), [])
+  useEffect(() => setMounted(false), [])
 
-//   return mounted ? (
-//     theme === 'Dark' ? (
-//       <button
-//         className="bg-bkg-2 default-transition flex items-center justify-center h-10 rounded-full w-10 hover:bg-bkg-3"
-//         onClick={() => setTheme('Light')}
-//       >
-//         <SunIcon className="h-5 text-fgd-1 w-5" />
-//       </button>
-//     ) : (
-//       <button
-//         className="bg-bkg-2 default-transition flex items-center justify-center h-10 rounded-full w-10 hover:bg-bkg-3"
-//         onClick={() => setTheme('Dark')}
-//       >
-//         <MoonIcon className="h-5 text-fgd-1 w-5" />
-//       </button>
-//     )
-//   ) : null
-// }
+  return mounted ? (
+    theme === 'Dark' ? (
+      <button
+        className="bg-bkg-2 default-transition flex items-center justify-center h-10 rounded-full w-10 hover:bg-bkg-3"
+        onClick={() => setTheme('Light')}
+      >
+        <SunIcon className="h-5 text-fgd-1 w-5" />
+      </button>
+    ) : (
+      <button
+        className="bg-bkg-2 default-transition flex items-center justify-center h-10 rounded-full w-10 hover:bg-bkg-3"
+        onClick={() => setTheme('Dark')}
+      >
+        <MoonIcon className="h-5 text-fgd-1 w-5" />
+      </button>
+    )
+  ) : null
+}
 
-// export default ThemeSwitch
-export {}
+export default ThemeSwitch
