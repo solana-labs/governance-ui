20abfdebbab0094e295fa29622b789311940f100
cbergz <charlie.bergman@gmail.com>
2022-11-17 11:25:08 -0500

    coral

--- components/PageBodyContainer.tsx
+++ components/PageBodyContainer.tsx
@@ -11,16 +11,16 @@ const PageBodyContainer = ({ children }) => {
           isNewRealmsWizard ? '' : 'min-h-[calc(100vh_-_80px)] pb-64'
         }`}
       >
-        <div className="fixed top-0 left-30 w-[100vw] h-[100vh] bg-bkg-1">
-          <picture>
-            <source srcSet="/img/shimmer.svg" media="(min-width: 640px)" />
-            <img src="/img/shimmer.svg" />
-          </picture>
-        </div>
         <div className="fixed top-0 left-0 w-[100vw] h-[100vh] bg-bkg-1">
           <picture>
             <source srcSet="/img/wavestop.svg" media="(min-width: 640px)" />
             <img src="/img/wavestop.svg" />
+          </picture>
+        </div>
+        <div className="fixed bottom-0 left-0 w-[100vw] h-[100vh] bg-bkg-1">
+          <picture>
+            <source srcSet="/img/coral.svg" media="(min-width: 640px)" />
+            <img src="/img/coral.svg" />
           </picture>
         </div>
         <div className="relative z-[3] col-span-12 px-4 md:px-8 xl:px-4 xl:col-start-2 xl:col-span-10">
