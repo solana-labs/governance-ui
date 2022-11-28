7f8131043a0ef93a85aac9c506fb432c093b44b7
agrippa kellum <ask252@cornell.edu>
2022-11-28 14:02:53 -0500

    more refactor

--- components/VotePanel/VotePanel.tsx
+++ components/VotePanel/VotePanel.tsx
@@ -1,70 +1,21 @@
-import { useHasVoteTimeExpired } from '../../hooks/useHasVoteTimeExpired'
-import useRealm from '../../hooks/useRealm'
 import { ProposalState } from '@solana/spl-governance'
-import { GoverningTokenRole } from '@solana/spl-governance'
 import { BanIcon } from '@heroicons/react/solid'
 
 import useWalletStore from '../../stores/useWalletStore'
-import useVotePluginsClientStore from 'stores/useVotePluginsClientStore'
 import Tooltip from '@components/Tooltip'
-import { VotingClientType } from '@utils/uiTypes/VotePlugin'
 import VetoButtons from './VetoButtons'
 import { CastVoteButtons } from './CastVoteButtons'
 import { YouVoted } from './YouVoted'
-import { useIsVoting } from './hooks'
+import { useIsVoting, useOwnVoteRecord } from './hooks'
 
 const VotePanel = () => {
-  const client = useVotePluginsClientStore(
-    (s) => s.state.currentRealmVotingClient
-  )
-  const { proposal, voteRecordsByVoter, tokenRole } = useWalletStore(
-    (s) => s.selectedProposal
-  )
-  const { ownTokenRecord, ownCouncilTokenRecord, ownVoterWeight } = useRealm()
-  const wallet = useWalletStore((s) => s.current)
+  const { proposal } = useWalletStore((s) => s.selectedProposal)
   const connected = useWalletStore((s) => s.connected)
 
-  // Handle state based on if a delegated wallet has already voted or not
-  const ownVoteRecord =
-    tokenRole === GoverningTokenRole.Community && ownTokenRecord
-      ? voteRecordsByVoter[
-          ownTokenRecord.account.governingTokenOwner.toBase58()
-        ]
-      : ownCouncilTokenRecord
-      ? voteRecordsByVoter[
-          ownCouncilTokenRecord.account.governingTokenOwner.toBase58()
-        ]
-      : wallet?.publicKey && voteRecordsByVoter[wallet.publicKey.toBase58()]
-
-  const voterTokenRecord =
-    tokenRole === GoverningTokenRole.Community
-      ? ownTokenRecord
-      : ownCouncilTokenRecord
+  const ownVoteRecord = useOwnVoteRecord()
 
   const isVoteCast = ownVoteRecord !== undefined
   const isVoting = useIsVoting()
-
-  const hasMinAmountToVote =
-    voterTokenRecord &&
-    ownVoterWeight.hasMinAmountToVote(
-      voterTokenRecord.account.governingTokenMint
-    )
-
-  const isVoteEnabled =
-    connected && isVoting && !isVoteCast && hasMinAmountToVote
-
-  const voteTooltipContent = !connected
-    ? 'You need to connect your wallet to be able to vote'
-    : !isVoting && isVoteCast
-    ? 'Proposal is not in a voting state anymore.'
-    : client.clientType === VotingClientType.NftVoterClient && !voterTokenRecord
-    ? 'You must join the Realm to be able to vote'
-    : !voterTokenRecord ||
-      !ownVoterWeight.hasMinAmountToVote(
-        voterTokenRecord.account.governingTokenMint
-      )
-    ? 'You don’t have governance power to vote in this dao'
-    : ''
 
   const didNotVote =
     connected &&
@@ -76,16 +27,11 @@ const VotePanel = () => {
 
   return (
     <>
+      {/* START: Note that these components control for themselves whether they are displayed and may not be visible */}
       <YouVoted />
-      {isVoting && (
-        <CastVoteButtons
-          {...{
-            voteTooltipContent,
-            isVoteEnabled: !!isVoteEnabled,
-          }}
-        />
-      )}
+      <CastVoteButtons />
       <VetoButtons />
+      {/* END */}
       {didNotVote && (
         <div className="bg-bkg-2 p-4 md:p-6 rounded-lg flex flex-col items-center justify-center">
           <h3 className="text-center mb-0">You did not vote</h3>
