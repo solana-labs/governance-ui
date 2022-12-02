7c08b77ff16f837e30c5b706b8d54b4495ea0ad4
agrippa kellum <ask252@cornell.edu>
2022-12-02 14:10:01 -0500

    refactor our old ownVoteRecord hook

--- components/VotePanel/hooks.ts
+++ components/VotePanel/hooks.ts
@@ -27,29 +27,6 @@ export const useVotingPop = () => {
   return votingPop
 }
 
-export const useOwnVoteRecord = () => {
-  const { voteRecordsByVoter, tokenRole } = useWalletStore(
-    (s) => s.selectedProposal
-  )
-  const { ownTokenRecord, ownCouncilTokenRecord } = useRealm()
-  const wallet = useWalletStore((s) => s.current)
-
-  // If we are using a delegate wallet, use the vote record for the *owner* of the *electoral* TokenOwnerRecord
-  // note: this means that your delegated *veto* TokenOwnerRecord may have a different owner, and thus a different VoteRecord.
-  const ownVoteRecord =
-    tokenRole === GoverningTokenRole.Community && ownTokenRecord
-      ? voteRecordsByVoter[
-          ownTokenRecord.account.governingTokenOwner.toBase58()
-        ]
-      : ownCouncilTokenRecord
-      ? voteRecordsByVoter[
-          ownCouncilTokenRecord.account.governingTokenOwner.toBase58()
-        ]
-      : wallet?.publicKey && voteRecordsByVoter[wallet.publicKey.toBase58()]
-
-  return ownVoteRecord
-}
-
 export const useVoterTokenRecord = () => {
   const { tokenRole } = useWalletStore((s) => s.selectedProposal)
   const { ownTokenRecord, ownCouncilTokenRecord } = useRealm()
