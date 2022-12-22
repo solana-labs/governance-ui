090c08b1f78d02a4a86aa19d984bbf859bdba2f1
0xcen <c3noriega@gmail.com>
2022-12-22 10:31:04 +0100

    feat: add domain name resolver

--- tools/validators/pubkey.ts
+++ tools/validators/pubkey.ts
@@ -1,5 +1,7 @@
 import { coerce, instance, string } from 'superstruct'
 import { PublicKey } from '@solana/web3.js'
+import { getDomainKey, NameRegistryState } from '@bonfida/spl-name-service'
+import { getConnectionContext } from '@utils/connection'
 
 export const PublicKeyFromString = coerce(
   instance(PublicKey),
@@ -10,6 +12,29 @@ export const PublicKeyFromString = coerce(
 export const tryParseKey = (key: string): PublicKey | null => {
   try {
     return new PublicKey(key)
+  } catch (error) {
+    return null
+  }
+}
+
+export const tryParseDomain = async (
+  domain: string
+): Promise<PublicKey | null> => {
+  const { current: connection } = getConnectionContext('mainnet')
+  try {
+    // pda
+    const { pubkey: domainPDA } = await getDomainKey(
+      domain.trim().toLowerCase()
+    )
+
+    const { nftOwner, registry } = await NameRegistryState.retrieve(
+      connection,
+      domainPDA
+    )
+
+    if (nftOwner) return nftOwner
+
+    return registry.owner
   } catch (error) {
     return null
   }
