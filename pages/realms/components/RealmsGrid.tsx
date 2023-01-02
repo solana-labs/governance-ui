1723b139301af9571c262393dee63eaed4a1fa52
Walid Khori <34014558+wkhori@users.noreply.github.com>
2023-01-02 00:52:05 -0600

    fix grid arrows

--- pages/realms/components/RealmsGrid.tsx
+++ pages/realms/components/RealmsGrid.tsx
@@ -52,8 +52,8 @@ const RealmBox = React.forwardRef<HTMLDivElement, IRealmBox>(
         </div>
         <h3 className="text-center">{realm.displayName ?? realm.symbol}</h3>
       </div>
-      {editing && (
-        <AiOutlineDrag className="absolute cursor-grab active:cursor-grabbing left-0 right-0 top-0 bottom-0 m-auto h-20 w-20 opacity-50" />
+      {editing && !inGrid && (
+        <AiOutlineDrag className="absolute cursor-grab active:cursor-grabbing right-1 top-1 m-auto h-8 w-8 hover:opacity-50" />
       )}
       {editing && inGrid && (
         <div
