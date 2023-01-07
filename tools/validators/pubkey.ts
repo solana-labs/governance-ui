6b692435cbbd5c463c541b2ef79b926eb3117653
0xcen <c3noriega@gmail.com>
2023-01-07 15:25:45 +0100

    chore: remove unused imports

--- tools/validators/pubkey.ts
+++ tools/validators/pubkey.ts
@@ -1,17 +1,8 @@
 import { coerce, instance, string } from 'superstruct'
-import {
-  Connection,
-  GetProgramAccountsFilter,
-  PublicKey,
-} from '@solana/web3.js'
+import { PublicKey } from '@solana/web3.js'
 
 import { getConnectionContext } from '@utils/connection'
-import {
-  MINT_PREFIX,
-  NAME_TOKENIZER_ID,
-  resolve,
-} from '@bonfida/spl-name-service'
-import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
+import { resolve } from '@bonfida/spl-name-service'
 
 export const PublicKeyFromString = coerce(
   instance(PublicKey),
@@ -36,7 +27,6 @@ export const tryParseDomain = async (
 
     return publicKey
   } catch (error) {
-    console.log('🚀 ~ file: pubkey.ts:32 ~ error', error)
     return null
   }
 }
