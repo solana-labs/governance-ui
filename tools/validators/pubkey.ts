c93f2328b72dac893629f1fe7191d5be696bf5ec
Carlos <75177443+0xcen@users.noreply.github.com>
2023-01-18 21:15:49 +0100

    resolves tokenized domains

--- tools/validators/pubkey.ts
+++ tools/validators/pubkey.ts
@@ -1,17 +1,8 @@
 import { coerce, instance, string } from 'superstruct'
-import {
-  Connection,
-  GetProgramAccountsFilter,
-  PublicKey,
-} from '@solana/web3.js'
+import { PublicKey } from '@solana/web3.js'
 
 import { getConnectionContext } from '@utils/connection'
-import {
-  MINT_PREFIX,
-  NAME_TOKENIZER_ID,
-  resolve,
-} from '@bonfida/spl-name-service'
-import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
+import { resolveDomain } from '@utils/domains'
 
 export const PublicKeyFromString = coerce(
   instance(PublicKey),
@@ -32,9 +23,12 @@ export const tryParseDomain = async (
 ): Promise<PublicKey | null> => {
   const { current: connection } = getConnectionContext('mainnet')
   try {
-    const publicKey = await resolve(connection, domain.toLowerCase().trim())
+    const publicKey = await resolveDomain(
+      connection,
+      domain.toLowerCase().trim()
+    )
 
-    return publicKey
+    return publicKey ?? null
   } catch (error) {
     console.log('🚀 ~ file: pubkey.ts:32 ~ error', error)
     return null
