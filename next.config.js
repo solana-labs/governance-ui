d537e7904b6fd4d8e437bc5e292e91add5800816
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-01-18 01:59:56 +0100

    add new env configs options (#1348)
    * add new envs configs
    
    * fix

--- next.config.js
+++ next.config.js
@@ -25,6 +25,9 @@ config = withTM({
     return config
   },
   env: {
+    MAIN_VIEW_SHOW_MAX_TOP_TOKENS_NUM:
+      process.env.MAIN_VIEW_SHOW_MAX_TOP_TOKENS_NUM,
+    DISABLE_NFTS: process.env.DISABLE_NFTS,
     REALM: process.env.REALM,
     MAINNET_RPC: process.env.MAINNET_RPC,
     DEVNET_RPC: process.env.DEVNET_RPC,
