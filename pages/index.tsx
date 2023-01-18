2cecf48411629620ba0e854cb5aa5e36368fd2f7
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-01-18 01:09:24 +0100

    add new envs configs

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,12 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 const Index = () => {
   const router = useRouter()
-
+  const REALM_SYMBOL = process?.env?.REALM_SYMBOL
   useEffect(() => {
-    const { REALM } = process.env
-    const mainUrl = REALM ? `/dao/${REALM}` : '/realms'
+    const mainUrl = REALM_SYMBOL ? `/dao/${REALM_SYMBOL}` : '/realms'
     router.replace(mainUrl)
     // eslint-disable-next-line react-hooks/exhaustive-deps -- TODO please fix, it can cause difficult bugs. You might wanna check out https://bobbyhadz.com/blog/react-hooks-exhaustive-deps for info. -@asktree
-  }, [])
+  }, [REALM_SYMBOL])
 
   return null
 }
