e47ad5af8acfb5945445d6c3a2b4ae008967edd8
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-01-23 21:57:50 +0100

    client setup

--- hooks/useMangoV4.tsx
+++ hooks/useMangoV4.tsx
@@ -5,11 +5,18 @@ import { PublicKey } from '@solana/web3.js'
 import { ConnectionContext } from '@utils/connection'
 import { WalletSigner } from '@solana/spl-governance'
 import { MangoClient, MANGO_V4_ID } from '@blockworks-foundation/mango-v4'
+import useWalletStore from 'stores/useWalletStore'
 
 export default function UseMangoV4() {
+  const cluster = useWalletStore((s) => s.connection).cluster
   const GROUP_NUM = 0
   const ADMIN_PK = new PublicKey('BJFYN2ZbcxRSTFGCAVkUEn4aJF99xaPFuyQj2rq5pFpo')
-  const GROUP = new PublicKey('78b8f4cGCwmZ9ysPFMWLaLTkkaYnUjwMJYStWe5RTSSX')
+  const DEVNET_GROUP = PublicKey.default
+  const MAINNET_GROUP = new PublicKey(
+    '78b8f4cGCwmZ9ysPFMWLaLTkkaYnUjwMJYStWe5RTSSX'
+  )
+  const clientCluster = cluster === 'devnet' ? 'devnet' : 'mainnet-beta'
+  const GROUP = cluster === 'devnet' ? DEVNET_GROUP : MAINNET_GROUP
   const getClient = async (
     connection: ConnectionContext,
     wallet: WalletSigner
@@ -22,8 +29,8 @@ export default function UseMangoV4() {
     )
     const client = await MangoClient.connect(
       adminProvider,
-      'mainnet-beta',
-      MANGO_V4_ID['mainnet-beta']
+      clientCluster,
+      MANGO_V4_ID[clientCluster]
     )
     return client
   }
