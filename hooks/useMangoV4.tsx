08ee98b388687cf27ae2f116493878aaf01ce708
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-01-23 22:52:08 +0100

    devnet group

--- hooks/useMangoV4.tsx
+++ hooks/useMangoV4.tsx
@@ -11,7 +11,9 @@ export default function UseMangoV4() {
   const cluster = useWalletStore((s) => s.connection).cluster
   const GROUP_NUM = 0
   const ADMIN_PK = new PublicKey('BJFYN2ZbcxRSTFGCAVkUEn4aJF99xaPFuyQj2rq5pFpo')
-  const DEVNET_GROUP = PublicKey.default
+  const DEVNET_GROUP = new PublicKey(
+    'Bpk8VzppSEkygd4KgXSgVzgVHib4EArhbDzyRpiS4yaf'
+  )
   const MAINNET_GROUP = new PublicKey(
     '78b8f4cGCwmZ9ysPFMWLaLTkkaYnUjwMJYStWe5RTSSX'
   )
