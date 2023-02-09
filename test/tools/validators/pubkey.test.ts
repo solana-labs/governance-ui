13e303aafca9f6dd41ff48b4e5b96343eec26b76
dankelleher <dankelleher@yahoo.com>
2023-02-09 11:14:18 +0100

    Fix for an integration test against SNS that uses a domain that has changed owner.

--- test/tools/validators/pubkey.test.ts
+++ test/tools/validators/pubkey.test.ts
@@ -1,17 +1,11 @@
 import { tryParseDomain } from '@tools/validators/pubkey'
 
 describe('Public Key Resolves ', () => {
-  const domain = 'carlos_0x.sol'
-  const tokenizedDomain = 'snakeoil.sol'
-  const pubkey = '9apnHjEQ8enLaPLxP7z9VQTphWE4nWqZcyHJYocQMpSE'
+  const domain = 'realms.sol'
+  const pubkey = '33m47vH6Eav6jr5Ry86XjhRft2jRBLDnDgPSHoquXi2Z'
 
   test('domains to publicKey', async () => {
     const resolvedKey = await tryParseDomain(domain)
-    expect(resolvedKey?.toBase58()).toEqual(pubkey)
-  })
-
-  test('tokenized domains to publicKey', async () => {
-    const resolvedKey = await tryParseDomain(tokenizedDomain)
     expect(resolvedKey?.toBase58()).toEqual(pubkey)
   })
 })
