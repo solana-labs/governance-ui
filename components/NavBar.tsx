ca44c058cb878cc0d545284372df3ecbefefc936
Beario <jakedolbear@hotmail.com>
2023-02-24 09:40:35 -0500

    Update Navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,7 @@
-import useQueryContext from '@hooks/useQueryContext'
 import Link from 'next/link'
 import dynamic from 'next/dynamic'
 import NotificationsSwitch from './NotificationsSwitch'
-import ThemeSwitch from './ThemeSwitch'
-import { ExternalLinkIcon } from '@heroicons/react/outline'
+
 
 const ConnectWalletButtonDynamic = dynamic(
   async () => await import('./ConnectWalletButton'),
@@ -11,12 +9,11 @@ const ConnectWalletButtonDynamic = dynamic(
 )
 
 const NavBar = () => {
-  const { fmtUrlWithCluster } = useQueryContext()
 
   return (
     <div className="flex flex-col sm:grid sm:grid-cols-12 relative z-20">
       <div className="flex items-center justify-between h-20 col-span-12 px-4 xl:col-start-2 xl:col-span-10 md:px-8 xl:px-4">
-        <Link href={fmtUrlWithCluster('/realms')}>
+        <Link href='https://www.udderchaos.io/'>
           <div className="flex cursor-pointer sm:items-center min-w-[36px]">
             <picture>
               <source
@@ -28,16 +25,6 @@ const NavBar = () => {
           </div>
         </Link>
         <div className="flex items-center justify-end space-x-2 md:space-x-4">
-          <a
-            className="border-b border-transparent items-center cursor-pointer -mb-[1px] transition-colors hidden sm:flex hover:border-white"
-            href="https://docs.realms.today/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div className="text-white text-sm">Read the Docs</div>
-            <ExternalLinkIcon className="stroke-white h-4 w-4 ml-2" />
-          </a>
-          <ThemeSwitch />
           <NotificationsSwitch />
           <ConnectWalletButtonDynamic />
         </div>
