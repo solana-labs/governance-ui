df71c6b71359dba62ffa8cdca2b6a133bc379c7a
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-03-29 18:24:42 +0200

    temp fix USDC price

--- utils/services/tokenPrice.tsx
+++ utils/services/tokenPrice.tsx
@@ -5,6 +5,7 @@ import { mergeDeepRight } from 'ramda'
 import { notify } from '@utils/notifications'
 import { WSOL_MINT } from '@components/instructions/tools'
 import overrides from 'public/realms/token-overrides.json'
+import { MAINNET_USDC_MINT } from '@foresight-tmp/foresight-sdk/dist/consts'
 
 //this service provide prices it is not recommended to get anything more from here besides token name or price.
 //decimals from metadata can be different from the realm on chain one
@@ -53,10 +54,12 @@ class TokenPriceService {
     }
   }
   async fetchTokenPrices(mintAddresses: string[]) {
+    console.log(mintAddresses)
     if (mintAddresses.length) {
       const mintAddressesWithSol = [...mintAddresses, WSOL_MINT]
       const symbols = mintAddressesWithSol.join(',')
       try {
+        const USDC_MINT = MAINNET_USDC_MINT.toBase58()
         const response = await axios.get(`${endpoint}?ids=${symbols}`)
         const priceToUsd: Price[] = response?.data?.data
           ? Object.values(response.data.data)
@@ -69,6 +72,15 @@ class TokenPriceService {
         this._tokenPriceToUSDlist = {
           ...this._tokenPriceToUSDlist,
           ...keyValue,
+        }
+        if (!this._tokenPriceToUSDlist[USDC_MINT]) {
+          this._tokenPriceToUSDlist[USDC_MINT] = {
+            id: USDC_MINT,
+            mintSymbol: 'USDC',
+            price: 1,
+            vsToken: USDC_MINT,
+            vsTokenSymbol: 'USDC',
+          }
         }
       } catch (e) {
         notify({
