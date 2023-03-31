925f7cf68749341669f2ddf2614603b69379d54b
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-03-31 22:21:47 +0200

    toggle hidden items

--- components/treasuryV2/WalletList/WalletListItem/AssetList/OtherAssetsList.tsx
+++ components/treasuryV2/WalletList/WalletListItem/AssetList/OtherAssetsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CollectionIcon } from '@heroicons/react/outline'
 
 import {
@@ -23,11 +23,14 @@ interface Props {
   expanded?: boolean
   assets: (Mint | Domains | Programs | RealmAuthority | Unknown)[]
   selectedAssetId?: string | null
+  itemsToHide: string[]
   onSelect?(asset: Mint | Domains | Programs | RealmAuthority | Unknown): void
   onToggleExpand?(): void
 }
 
 export default function OtherAssetsList(props: Props) {
+  const [showHiddenItems, setShowHiddenItems] = useState(false)
+
   return (
     <Collapsible
       className={props.className}
@@ -37,59 +40,68 @@ export default function OtherAssetsList(props: Props) {
       icon={<CollectionIcon className="stroke-white/50" />}
       title="Other Assets"
       onToggleExpand={props.onToggleExpand}
+      onToggleHiddenItems={() => setShowHiddenItems(!showHiddenItems)}
+      itemsToHide={props.itemsToHide}
+      showHiddenItems={showHiddenItems}
     >
-      {props.assets.map((asset, i) => {
-        switch (asset.type) {
-          case AssetType.Mint:
-            return (
-              <MintListItem
-                key={i}
-                mint={asset}
-                selected={props.selectedAssetId === asset.id}
-                onSelect={() => props.onSelect?.(asset)}
-              />
-            )
-          case AssetType.Domain:
-            return (
-              <DomainListItem
-                key={i}
-                count={asset.count}
-                selected={props.selectedAssetId === asset.id}
-                onSelect={() => props.onSelect?.(asset)}
-              />
-            )
-          case AssetType.Programs:
-            return (
-              <ProgramsListItem
-                key={i}
-                count={asset.count}
-                selected={props.selectedAssetId === asset.id}
-                onSelect={() => props.onSelect?.(asset)}
-              />
-            )
-          case AssetType.RealmAuthority:
-            return (
-              <RealmAuthorityListItem
-                key={i}
-                name={asset.name}
-                selected={props.selectedAssetId === asset.id}
-                thumbnail={asset.icon}
-                onSelect={() => props.onSelect?.(asset)}
-              />
-            )
-          case AssetType.Unknown:
-            return (
-              <UnknownAssetListItem
-                key={i}
-                count={asset.count}
-                name={asset.name}
-                selected={props.selectedAssetId === asset.id}
-                thumbnail={asset.icon}
-                onSelect={() => props.onSelect?.(asset)}
-              />
-            )
-        }
-      })}
+      {props.assets
+        .filter((x) =>
+          showHiddenItems
+            ? true
+            : !props.itemsToHide.includes((x as any).address)
+        )
+        .map((asset, i) => {
+          switch (asset.type) {
+            case AssetType.Mint:
+              return (
+                <MintListItem
+                  key={i}
+                  mint={asset}
+                  selected={props.selectedAssetId === asset.id}
+                  onSelect={() => props.onSelect?.(asset)}
+                />
+              )
+            case AssetType.Domain:
+              return (
+                <DomainListItem
+                  key={i}
+                  count={asset.count}
+                  selected={props.selectedAssetId === asset.id}
+                  onSelect={() => props.onSelect?.(asset)}
+                />
+              )
+            case AssetType.Programs:
+              return (
+                <ProgramsListItem
+                  key={i}
+                  count={asset.count}
+                  selected={props.selectedAssetId === asset.id}
+                  onSelect={() => props.onSelect?.(asset)}
+                />
+              )
+            case AssetType.RealmAuthority:
+              return (
+                <RealmAuthorityListItem
+                  key={i}
+                  name={asset.name}
+                  selected={props.selectedAssetId === asset.id}
+                  thumbnail={asset.icon}
+                  onSelect={() => props.onSelect?.(asset)}
+                />
+              )
+            case AssetType.Unknown:
+              return (
+                <UnknownAssetListItem
+                  key={i}
+                  count={asset.count}
+                  name={asset.name}
+                  selected={props.selectedAssetId === asset.id}
+                  thumbnail={asset.icon}
+                  onSelect={() => props.onSelect?.(asset)}
+                />
+              )
+          }
+        })}
     </Collapsible>
   )
 }
