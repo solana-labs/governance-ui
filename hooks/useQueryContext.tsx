6826958af23e1d685fe41402c1711569982fa348
agrippa kellum <ask252@cornell.edu>
2023-04-04 13:52:57 -0400

    persist viewAs when navigating

--- hooks/useQueryContext.tsx
+++ hooks/useQueryContext.tsx
@@ -3,16 +3,28 @@ import { EndpointTypes } from '../models/types'
 
 export default function useQueryContext() {
   const router = useRouter()
-  const { cluster } = router.query
+  const { cluster, viewAs } = router.query
 
   const endpoint = cluster ? (cluster as EndpointTypes) : 'mainnet'
   const hasClusterOption = endpoint !== 'mainnet'
-  const fmtUrlWithCluster = (url) => {
-    if (hasClusterOption) {
-      const mark = url.includes('?') ? '&' : '?'
-      return decodeURIComponent(`${url}${mark}cluster=${endpoint}`)
-    }
-    return url
+  const fmtUrlWithCluster = (url: string) => {
+    const urlWithCluster = hasClusterOption
+      ? decodeURIComponent(
+          `${url}${url.includes('?') ? '&' : '?'}cluster=${endpoint}`
+        )
+      : url
+
+    // apologies for the overly verbose code
+    const urlWithViewAs =
+      typeof viewAs === 'string'
+        ? decodeURIComponent(
+            `${urlWithCluster}${
+              urlWithCluster.includes('?') ? '&' : '?'
+            }viewAs=${viewAs}`
+          )
+        : urlWithCluster
+
+    return urlWithViewAs
   }
 
   return {
