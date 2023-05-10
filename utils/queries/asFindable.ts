325e7310002402fa44580374e5703e64f2adaba6
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-05-11 00:43:09 +0200

    fix

--- utils/queries/asFindable.ts
+++ utils/queries/asFindable.ts
@@ -2,7 +2,7 @@
  * @param {any}  thisContext - second param if you want to call function that is part of
  * an object eg. connection.getAccountInfo
  * getAccountInfo will loss this binding so you need to
- * pass connection as context depends on lib/function implementation
+ * pass connection as context. Depends on lib/function implementation
  */
 const asFindable = <P extends any[], R>(
   f: (...p: P) => Promise<R>,
