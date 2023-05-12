dcc5664d0071f2387f26af709ada6249e490b277
agrippa kellum <ask252@cornell.edu>
2023-05-11 20:15:02 -0400

    kill annoying middleware

--- stores/useSelectedDelegatorStore.tsx
+++ stores/useSelectedDelegatorStore.tsx
@@ -1,6 +1,5 @@
 import { PublicKey } from '@solana/web3.js'
 import create from 'zustand'
-import { persist } from 'zustand/middleware'
 
 type Store = {
   communityDelegator?: PublicKey
@@ -9,14 +8,9 @@ type Store = {
   setCouncilDelegator: (x?: PublicKey) => void
 }
 
-export const useSelectedDelegatorStore = create<Store>(
-  persist(
-    (set, _get) => ({
-      communityDelegator: undefined,
-      councilDelegator: undefined,
-      setCommunityDelegator: (x) => set({ communityDelegator: x }),
-      setCouncilDelegator: (x) => set({ councilDelegator: x }),
-    }),
-    { name: 'selected-delegators' }
-  )
-)
+export const useSelectedDelegatorStore = create<Store>((set, _get) => ({
+  communityDelegator: undefined,
+  councilDelegator: undefined,
+  setCommunityDelegator: (x) => set({ communityDelegator: x }),
+  setCouncilDelegator: (x) => set({ councilDelegator: x }),
+}))
