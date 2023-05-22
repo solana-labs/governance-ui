6a70b153a31aba9a8a56ae1c679017402932a867
agrippa kellum <ask252@cornell.edu>
2023-05-21 23:20:32 -0400

    create token account queries

---
+++ hooks/queries/tokenAccount.ts
@@ -0,0 +1,46 @@
+import { PublicKey } from '@solana/web3.js'
+import { useQuery } from '@tanstack/react-query'
+import { getOwnedTokenAccounts } from '@utils/tokens'
+import useWalletStore from 'stores/useWalletStore'
+import queryClient from './queryClient'
+
+export const tokenAccountQueryKeys = {
+  all: (cluster: string) => [cluster, 'TokenAccount'],
+  byPubkey: (cluster: string, k: PublicKey) => [
+    ...tokenAccountQueryKeys.all(cluster),
+    k.toString(),
+  ],
+  byOwner: (cluster: string, o: PublicKey) => [
+    ...tokenAccountQueryKeys.all(cluster),
+    'by Owner',
+    o.toString(),
+  ],
+}
+
+export const useTokenAccountsByOwnerQuery = (pubkey?: PublicKey) => {
+  const connection = useWalletStore((s) => s.connection)
+
+  const enabled = pubkey !== undefined
+  const query = useQuery({
+    queryKey: enabled
+      ? tokenAccountQueryKeys.byOwner(connection.cluster, pubkey)
+      : undefined,
+    queryFn: async () => {
+      if (!enabled) throw new Error()
+      const results = await getOwnedTokenAccounts(connection.current, pubkey)
+
+      // since we got the data for these accounts, lets save it
+      results.forEach((x) => {
+        queryClient.setQueryData(
+          tokenAccountQueryKeys.byPubkey(connection.cluster, x.publicKey),
+          { found: true, result: x }
+        )
+      })
+
+      return results
+    },
+    enabled,
+  })
+
+  return query
+}
