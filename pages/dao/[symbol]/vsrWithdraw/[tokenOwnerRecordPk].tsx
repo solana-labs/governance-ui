a394312f8b8420b9903cfc4617cd3bfeff7e2ec0
agrippa kellum <ask252@cornell.edu>
2023-05-24 20:59:02 -0400

    fix build error????

--- pages/dao/[symbol]/vsrWithdraw/[tokenOwnerRecordPk].tsx
+++ pages/dao/[symbol]/vsrWithdraw/[tokenOwnerRecordPk].tsx
@@ -1,20 +1,18 @@
 import { useRouter } from 'next/router'
 import LockTokensAccountWithdraw from 'VoteStakeRegistry/components/Account/LockTokensAccountWithdraw'
 
-const account = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks -- TODO this is potentially quite serious! please fix next time the file is edited, -@asktree
+const AccountViewPage = () => {
   const router = useRouter()
 
-  const tokenOwnerRecordPk = router?.query?.tokenOwnerRecordPk
+  const tokenOwnerRecordPk = router.query?.tokenOwnerRecordPk
 
-  const getAccountView = () => {
-    return (
+  return (
+    typeof tokenOwnerRecordPk === 'string' && (
       <LockTokensAccountWithdraw
         tokenOwnerRecordPk={tokenOwnerRecordPk}
       ></LockTokensAccountWithdraw>
     )
-  }
-  return getAccountView()
+  )
 }
 
-export default account
+export default AccountViewPage
