b514182b74a72b3393bc047c6536bb6503abd91b
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-06-01 19:06:33 +0200

    lint fix

--- components/instructions/programs/mangoV4.tsx
+++ components/instructions/programs/mangoV4.tsx
@@ -420,7 +420,7 @@ const displayArgs = async (connection: Connection, data: Uint8Array) => {
 }
 
 function commify(n) {
-  var parts = n.toString().split('.')
+  const parts = n.toString().split('.')
   const numberPart = parts[0]
   const decimalPart = parts[1]
   const thousands = /\B(?=(\d{3})+(?!\d))/g
