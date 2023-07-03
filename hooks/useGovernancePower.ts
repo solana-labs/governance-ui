e73be65d945203babfa713eedc7ddb9bd074b0f1
agrippa kellum <ask252@cornell.edu>
2023-07-02 23:43:23 -0400

    govpower hook

---
+++ hooks/useGovernancePower.ts
@@ -0,0 +1,38 @@
+import { useMemo } from 'react'
+import {
+  useTokenOwnerRecordsDelegatedToUser,
+  useUserCommunityTokenOwnerRecord,
+  useUserCouncilTokenOwnerRecord,
+} from './queries/tokenOwnerRecord'
+import { BN } from 'bn.js'
+import { useRealmQuery } from './queries/realm'
+
+const useGovernancePower = () => {
+  const councilTor = useUserCouncilTokenOwnerRecord()
+  const communityTor = useUserCommunityTokenOwnerRecord()
+  const realm = useRealmQuery().data?.result
+  const torsDelegatedToUser = useTokenOwnerRecordsDelegatedToUser()
+
+  const communityPowerFromDelegators = useMemo(() => {
+    realm === undefined
+      ? undefined
+      : torsDelegatedToUser
+          ?.filter((x) =>
+            x.account.governingTokenMint.equals(realm.account.communityMint)
+          )
+          .map((x) => x.account.governingTokenDepositAmount)
+          .reduce((x, acc) => x.add(acc), new BN(0))
+  }, [realm, torsDelegatedToUser])
+
+  const councilMintAddr = realm?.account.config.councilMint
+  const councilPowerFromDelegators = useMemo(() => {
+    councilMintAddr === undefined
+      ? undefined
+      : torsDelegatedToUser
+          ?.filter((x) => x.account.governingTokenMint.equals(councilMintAddr))
+          .map((x) => x.account.governingTokenDepositAmount)
+          .reduce((x, acc) => x.add(acc), new BN(0))
+  }, [councilMintAddr, torsDelegatedToUser])
+}
+
+export default useGovernancePower
