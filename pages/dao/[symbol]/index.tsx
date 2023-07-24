d17ccb68b39ec1b9d0189a812be239a76d558280
Adrian Brzeziński <a.brzezinski94@gmail.com>
2023-07-24 16:20:03 +0200

    fix council community mix in vsr (#1724)

--- pages/dao/[symbol]/index.tsx
+++ pages/dao/[symbol]/index.tsx
@@ -284,7 +284,7 @@ const REALM = () => {
             pubkey: selectedProposal.proposalPk,
             owner: realm.pubkey,
           },
-          relevantTokenRecord.pubkey
+          relevantTokenRecord
         )
         if (client.client instanceof NftVoterClient === false) {
           await withCastVote(
