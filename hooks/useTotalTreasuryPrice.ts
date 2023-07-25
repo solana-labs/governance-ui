56295499aea632a630aca8597144f76c26bc1374
agrippa <ask252@cornell.edu>
2023-07-25 14:48:42 -0400

    use react-query for jupiter token prices (#1720)

--- hooks/useTotalTreasuryPrice.ts
+++ hooks/useTotalTreasuryPrice.ts
@@ -1,9 +1,9 @@
 import { BN } from '@coral-xyz/anchor'
 import { getMintDecimalAmountFromNatural } from '@tools/sdk/units'
-import tokenPriceService from '@utils/services/tokenPrice'
 import BigNumber from 'bignumber.js'
-import { useState, useEffect } from 'react'
 import useGovernanceAssets from './useGovernanceAssets'
+import { useJupiterPricesByMintsQuery } from './queries/jupiterPrice'
+import { PublicKey } from '@metaplex-foundation/js'
 import { WSOL_MINT } from '@components/instructions/tools'
 import { AccountType } from '@utils/uiTypes/assets'
 
@@ -13,60 +13,53 @@ export function useTotalTreasuryPrice() {
     assetAccounts,
     auxiliaryTokenAccounts,
   } = useGovernanceAssets()
-  const [totalPriceFormatted, setTotalPriceFormatted] = useState('')
-  useEffect(() => {
-    async function calcTotalTokensPrice() {
-      const tokenAccountsTotalPrice = [
-        ...governedTokenAccountsWithoutNfts,
-        ...auxiliaryTokenAccounts,
-      ]
-        .filter((x) => typeof x.extensions.mint !== 'undefined')
-        .map((x) => {
-          return (
-            getMintDecimalAmountFromNatural(
-              x.extensions.mint!.account,
-              new BN(
-                x.isSol
-                  ? x.extensions.solAccount!.lamports
-                  : x.isToken || x.type === AccountType.AUXILIARY_TOKEN
-                  ? x.extensions.token!.account?.amount
-                  : 0
-              )
-            ).toNumber() *
-            tokenPriceService.getUSDTokenPrice(
-              x.extensions.mint!.publicKey.toBase58()
-            )
-          )
-        })
-        .reduce((acc, val) => acc + val, 0)
 
-      const stakeAccountsTotalPrice = assetAccounts
-        .filter((x) => x.extensions.stake)
-        .map((x) => {
-          return (
-            x.extensions.stake!.amount *
-            tokenPriceService.getUSDTokenPrice(WSOL_MINT)
-          )
-        })
-        .reduce((acc, val) => acc + val, 0)
+  const mintsToFetch = [
+    ...governedTokenAccountsWithoutNfts,
+    ...auxiliaryTokenAccounts,
+  ]
+    .filter((x) => typeof x.extensions.mint !== 'undefined')
+    .map((x) => x.extensions.mint!.publicKey)
 
-      const totalPrice = tokenAccountsTotalPrice + stakeAccountsTotalPrice
-      setTotalPriceFormatted(
-        totalPrice ? new BigNumber(totalPrice).toFormat(0) : ''
-      )
-    }
-    if (governedTokenAccountsWithoutNfts.length) {
-      calcTotalTokensPrice()
-    } else {
-      setTotalPriceFormatted('')
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- TODO please fix, it can cause difficult bugs. You might wanna check out https://bobbyhadz.com/blog/react-hooks-exhaustive-deps for info. -@asktree
-  }, [
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- TODO please fix, it can cause difficult bugs. You might wanna check out https://bobbyhadz.com/blog/react-hooks-exhaustive-deps for info. -@asktree
-    JSON.stringify(governedTokenAccountsWithoutNfts),
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- TODO please fix, it can cause difficult bugs. You might wanna check out https://bobbyhadz.com/blog/react-hooks-exhaustive-deps for info. -@asktree
-    JSON.stringify(tokenPriceService._tokenPriceToUSDlist),
+  const { data: prices } = useJupiterPricesByMintsQuery([
+    ...mintsToFetch,
+    new PublicKey(WSOL_MINT),
   ])
+
+  const totalTokensPrice = [
+    ...governedTokenAccountsWithoutNfts,
+    ...auxiliaryTokenAccounts,
+  ]
+    .filter((x) => typeof x.extensions.mint !== 'undefined')
+    .map((x) => {
+      return (
+        getMintDecimalAmountFromNatural(
+          x.extensions.mint!.account,
+          new BN(
+            x.isSol
+              ? x.extensions.solAccount!.lamports
+              : x.isToken || x.type === AccountType.AUXILIARY_TOKEN
+              ? x.extensions.token!.account?.amount
+              : 0
+          )
+        ).toNumber() *
+        (prices?.[x.extensions.mint!.publicKey.toBase58()]?.price ?? 0)
+      )
+    })
+    .reduce((acc, val) => acc + val, 0)
+
+  const stakeAccountsTotalPrice = assetAccounts
+    .filter((x) => x.extensions.stake)
+    .map((x) => {
+      return x.extensions.stake!.amount * (prices?.[WSOL_MINT]?.price ?? 0)
+    })
+    .reduce((acc, val) => acc + val, 0)
+
+  const totalPrice = totalTokensPrice + stakeAccountsTotalPrice
+
+  const totalPriceFormatted = governedTokenAccountsWithoutNfts.length
+    ? new BigNumber(totalPrice).toFormat(0)
+    : ''
 
   return {
     totalPriceFormatted,
