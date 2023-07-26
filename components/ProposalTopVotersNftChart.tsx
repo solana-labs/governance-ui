e3b34eddc453f80f8a7af7ebd113bb82d16478c2
Hau Chu <hchoward1998@gmail.com>
2023-07-25 23:58:42 -0500

    draw nft ing

---
+++ components/ProposalTopVotersNftChart.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  showNft: boolean
+  className?: string
+  data: any[]
+  endpoint: string
+  highlighted?: string
+}
+
+const ProposalTopVotersNftChart = (props: Props) => {
+  return <div></div>
+}
+
+export default ProposalTopVotersNftChart
--- pages/dao/[symbol]/proposal/[pk]/explore.tsx
+++ pages/dao/[symbol]/proposal/[pk]/explore.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { useRouter } from 'next/router'
 import classNames from 'classnames'
 import { ChevronLeftIcon } from '@heroicons/react/solid'
@@ -14,6 +14,88 @@ import ProposalVoteResult from '@components/ProposalVoteResults'
 import ProposalRemainingVotingTime from '@components/ProposalRemainingVotingTime'
 import { useRouteProposalQuery } from '@hooks/queries/proposal'
 import useLegacyConnectionContext from '@hooks/useLegacyConnectionContext'
+import Switch from '@components/Switch'
+import useVotePluginsClientStore from 'stores/useVotePluginsClientStore'
+import { NFT_PLUGINS_PKS } from '@constants/plugins'
+import { useRealmConfigQuery } from '@hooks/queries/realmConfig'
+import { fetchDigitalAssetsByOwner } from '@hooks/queries/digitalAssets'
+import { PublicKey } from '@solana/web3.js'
+import { getNetworkFromEndpoint } from '@utils/connection'
+import { useConnection } from '@solana/wallet-adapter-react'
+import { useAsync } from 'react-async-hook'
+
+function filterAndMapVerifiedCollections(nfts, usedCollectionsPks) {
+  return nfts
+    ?.filter((nft) => {
+      const collection = nft.grouping.find((x) => x.group_key === 'collection')
+      if (
+        collection &&
+        usedCollectionsPks.includes(collection.group_value) &&
+        (collection.verified || typeof collection.verified === 'undefined') &&
+        nft.creators?.filter((x) => x.verified).length > 0
+      ) {
+        return true
+      } else {
+        return false
+      }
+    })
+    .reduce((prev, curr) => {
+      const collectionKey = curr.ownership.owner
+      if (typeof collectionKey === 'undefined') return prev
+
+      if (prev[collectionKey]) {
+        prev[collectionKey].push(curr)
+      } else {
+        prev[collectionKey] = [curr]
+      }
+      return prev
+    }, {})
+}
+
+export const useOwnerVerifiedCollections = (records) => {
+  const { connection } = useConnection()
+  const network = getNetworkFromEndpoint(connection.rpcEndpoint)
+  if (network === 'localnet') throw new Error()
+  const config = useRealmConfigQuery().data?.result
+  const currentPluginPk = config?.account.communityTokenConfig.voterWeightAddin
+
+  const [nftMintRegistrar] = useVotePluginsClientStore((s) => [
+    s.state.nftMintRegistrar,
+  ])
+
+  const usedCollectionsPks: string[] = useMemo(
+    () =>
+      (currentPluginPk &&
+        NFT_PLUGINS_PKS.includes(currentPluginPk?.toBase58()) &&
+        nftMintRegistrar?.collectionConfigs.map((x) =>
+          x.collection.toBase58()
+        )) ||
+      [],
+    [currentPluginPk, nftMintRegistrar?.collectionConfigs]
+  )
+
+  const enabled = records !== undefined && usedCollectionsPks !== undefined
+  return useAsync(async () => {
+    if (!enabled) throw new Error()
+
+    const ownersNfts = await Promise.all(
+      records.map(async (record) => {
+        const ownedNfts = await fetchDigitalAssetsByOwner(
+          network,
+          new PublicKey(record.key)
+        )
+
+        const verifiedNfts = filterAndMapVerifiedCollections(
+          ownedNfts,
+          usedCollectionsPks
+        )
+        console.log(verifiedNfts)
+        return verifiedNfts
+      })
+    )
+    return ownersNfts.filter((x) => x !== null)
+  }, [connection, enabled, usedCollectionsPks])
+}
 
 export default function Explore() {
   const proposal = useRouteProposalQuery().data?.result
@@ -23,8 +105,16 @@ export default function Explore() {
   const records = useVoteRecords(proposal)
   const signatories = useSignatories(proposal)
   const router = useRouter()
+  const [showNfts, setShowNfts] = useState(false)
+  const { result: votersNfts } = useOwnerVerifiedCollections(records)
+  const config = useRealmConfigQuery().data?.result
+  const currentPluginPk = config?.account.communityTokenConfig.voterWeightAddin
 
   const endpoint = connection.endpoint
+
+  const toggleShowNfts = () => {
+    setShowNfts(!showNfts)
+  }
 
   return (
     <div className="bg-bkg-2 rounded-lg p-4 space-y-3 md:p-6">
@@ -49,7 +139,26 @@ export default function Explore() {
             <h1 className="mr-2">{proposal?.account.name}</h1>
             <ProposalStateBadge proposal={proposal.account} />
           </div>
-          <h3 className="mb-4 mt-16">Top Voters</h3>
+          <div className="mb-4 mt-16 flex justify-between">
+            <h3 className="">Top Voters</h3>
+            <div
+              className={`${
+                currentPluginPk &&
+                NFT_PLUGINS_PKS.includes(currentPluginPk.toBase58())
+                  ? 'visible'
+                  : 'hidden'
+              } flex items-center`}
+            >
+              <p className="mb-0 mr-1 text-fgd-3">Show NFTs</p>
+              <Switch
+                checked={showNfts}
+                onChange={() => {
+                  toggleShowNfts()
+                }}
+              />
+            </div>
+          </div>
+
           <div
             className="grid gap-4 grid-cols-1 items-center lg:grid-cols-2"
             onMouseLeave={() => setHighlighted(undefined)}
@@ -95,6 +204,14 @@ export default function Explore() {
           <div className="animate-pulse bg-bkg-3 h-52 rounded-lg mt-16" />
         </div>
       )}
+      {/* {highlighted && position && (
+        <div
+          className="w-32 bg-white absolute"
+          style={{ left: position?.x + 300, top: position?.y + 100 }}
+        >
+          Helloworkd
+        </div>
+      )} */}
     </div>
   )
 }
