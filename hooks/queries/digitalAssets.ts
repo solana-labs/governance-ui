89dea4d334f1541352e75ce19acc7ead8997ec93
Hau Chu <hchoward1998@gmail.com>
2023-07-26 23:59:00 -0500

    fix typo

--- hooks/queries/digitalAssets.ts
+++ hooks/queries/digitalAssets.ts
@@ -43,7 +43,7 @@ export const digitalAssetsQueryKeys = {
   ],
   proofById: (network: Network, id: PublicKey) => [
     ...digitalAssetsQueryKeys.all(network),
-    'Proof by Id',
+    'proof By Id',
     id.toString(),
   ],
 }
