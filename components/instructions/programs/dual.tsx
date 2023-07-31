1794ac50defc9ad1f7c514959ca50657276c48ec
Britt Cyr <cyrbritt@gmail.com>
2023-07-30 20:19:57 -0400

    Include instructions for new reversible staking options

--- components/instructions/programs/dual.tsx
+++ components/instructions/programs/dual.tsx
@@ -8,13 +8,24 @@ import { AccountMetaData } from '@solana/spl-governance'
 import { tryGetMint } from '@utils/tokens'
 import { getMintDecimalAmountFromNatural } from '@tools/sdk/units'
 
-// TODO: Update with configV3 and initStrikeReversible
 interface configV2Instruction {
   lotSize: BN
   numTokens: BN
   optionExpiration: BN
   soName: string
   subscriptionPeriodEnd: BN
+}
+
+interface configV3Instruction {
+  lotSize: BN
+  numTokens: BN
+  optionExpiration: BN
+  soName: string
+  subscriptionPeriodEnd: BN
+}
+
+interface initStrikeReversibleInstruction {
+  strike: BN
 }
 
 interface initStrikeInstruction {
@@ -35,7 +46,70 @@ interface exerciseInstruction {
   strike: BN
 }
 
+interface exerciseReversibleInstruction {
+  amount: BN
+  strike: BN
+}
+
+// TODO: Include withdrawAll
+
 const INSTRUCTIONS = {
+  12345: {
+    name: 'Staking Option ConfigV3',
+    accounts: [
+      { name: 'authority' },
+      { name: 'soAuthority' },
+      { name: 'issueAuthority' },
+      { name: 'state' },
+      { name: 'baseVault' },
+      { name: 'quoteVault' },
+      { name: 'baseAccount' },
+      { name: 'quoteAccount' },
+      { name: 'baseMint' },
+      { name: 'quoteMint' },
+      { name: 'tokenProgram' },
+      { name: 'systemProgram' },
+      { name: 'rent' },
+    ],
+    getDataUI: async (
+      connection: Connection,
+      data: Uint8Array,
+      accounts: AccountMetaData[]
+    ) => {
+      const soHelper = new StakingOptions(connection.rpcEndpoint)
+
+      const decodedInstructionData = new BorshInstructionCoder(
+        soHelper.getIdl()
+      ).decode(Buffer.from(data))?.data as configV3Instruction
+
+      const baseMint = await tryGetMint(connection, accounts[7].pubkey)
+
+      const rawAmount = decodedInstructionData.numTokens
+      const tokenAmount = baseMint
+        ? getMintDecimalAmountFromNatural(baseMint.account, rawAmount)
+        : rawAmount
+
+      return (
+        <div className="space-y-3">
+          <div>
+            Expiration:{' '}
+            {new Date(
+              decodedInstructionData.optionExpiration.toNumber() * 1000
+            ).toDateString()}
+          </div>
+          <div>
+            Subscription Period End:{' '}
+            {new Date(
+              decodedInstructionData.subscriptionPeriodEnd.toNumber() * 1000
+            ).toDateString()}
+          </div>
+          <div>Num Tokens: {tokenAmount.toNumber()}</div>
+          <div>Lot size: {decodedInstructionData.lotSize.toNumber()}</div>
+          <div>SoName: {decodedInstructionData.soName}</div>
+        </div>
+      )
+    },
+  },
   225: {
     name: 'Staking Option ConfigV2',
     accounts: [
@@ -114,6 +188,31 @@ const INSTRUCTIONS = {
       )
     },
   },
+  123456: {
+    name: 'Staking Option InitStrikeReversible',
+    accounts: [
+      { name: 'authority' },
+      { name: 'payer' },
+      { name: 'state' },
+      { name: 'reverseOptionMint' },
+      { name: 'optionMint' },
+      { name: 'tokenProgram' },
+      { name: 'systemProgram' },
+      { name: 'rent' },
+    ],
+    getDataUI: async (connection: Connection, data: Uint8Array) => {
+      const soHelper = new StakingOptions(connection.rpcEndpoint)
+      const decodedInstructionData = new BorshInstructionCoder(
+        soHelper.getIdl()
+      ).decode(Buffer.from(data))?.data as initStrikeReversibleInstruction
+
+      return (
+        <div className="space-y-3">
+          <div>Strike atoms per lot: {decodedInstructionData.strike.toNumber()}</div>
+        </div>
+      )
+    },
+  },
   245: {
     name: 'Staking Option Name Token',
     accounts: [
@@ -155,6 +254,38 @@ const INSTRUCTIONS = {
       const decodedInstructionData = new BorshInstructionCoder(
         soHelper.getIdl()
       ).decode(Buffer.from(data))?.data as issueInstruction
+
+      return (
+        <div className="space-y-3">
+          <div>
+            Amount atoms of base: {decodedInstructionData.amount.toNumber()}
+          </div>
+          <div>Strike: {decodedInstructionData.strike.toNumber()}</div>
+        </div>
+      )
+    },
+  },
+  1234567: {
+    name: 'Staking Option ExerciseReversible',
+    accounts: [
+      { name: 'authority' },
+      { name: 'state' },
+      { name: 'userSoAccount' },
+      { name: 'optionMint' },
+      { name: 'userReverseSoAccount' },
+      { name: 'reverseOptionMint' },
+      { name: 'userQuoteAccount' },
+      { name: 'quoteVault' },
+      { name: 'baseVault' },
+      { name: 'userBaseAccount' },
+      { name: 'tokenProgram' },
+    ],
+    getDataUI: async (connection: Connection, data: Uint8Array) => {
+      const soHelper = new StakingOptions(connection.rpcEndpoint)
+
+      const decodedInstructionData = new BorshInstructionCoder(
+        soHelper.getIdl()
+      ).decode(Buffer.from(data))?.data as exerciseReversibleInstruction
 
       return (
         <div className="space-y-3">
