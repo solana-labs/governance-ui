e9ebddedfdbfba8cfbf74f17696c9c3e78090b01
Ryan <ryan.t.chern@gmail.com>
2023-07-31 22:13:58 -0700

    Update verify.tsx

--- pages/validator/verify.tsx
+++ pages/validator/verify.tsx
@@ -57,60 +57,60 @@ const ValidatorVerifyPage = () => {
   </div>
 </div>
   );
-    <div className="flex flex-col justify-center items-center h-screen p-4 text-center bg-gray-100 text-black">
-      <h1 className="text-3xl font-medium mt-8">Follow the steps to get verified:</h1>
-      <p className="italic underline text-center text-neutral-700 mt-4">Do NOT give this information to other parties. If you have any questions or concerns, reach out in the Solana Tech Discord.</p>
-      <div className="text-left text-black p-4">
-        <input
-          type="text"
-          className="border rounded text-base mb-2 w-full p-2"
-          value={publicKey}
-          onChange={(e) => setPublicKey(e.target.value)}
-          placeholder="1. Enter your public key (cli command: solana address)"
-        />
-        <p className="text-base text-neutral-700 mt-4">2. Make sure the Solana CLI ({">= 1.16"}) is installed and configured. See the <a href="https://docs.solana.com/cli/install-solana-cli-tools" className="underline text-black">Solana CLI installation guide</a> for more information.</p>
-        <p className="text-base text-neutral-700 mt-4">3. Sign the payload using the Solana CLI’s sign-offchain-message command and send to the endpoint.</p>
-        <div className="flex justify-between mb-2">
-          <pre style={{whiteSpace: "pre-wrap", wordBreak: "break-all"}} className="p-4 bg-gray-200 rounded">{`solana sign-offchain-message `}{`"`}{authCode ? authCode : ':discord-authorization-code'}{`"`}
-          {`| xargs -I {} curl -X POST http://localhost:3001/verify-gossip-keypair/`}{publicKey ? publicKey : ':public-key'}{'/'}
-          {authCode ? authCode : ':discord-authorization-code'}{` -H 'Content-Type: application/json' -d '{"signature": "'{}'"}'`}
-          </pre>
-        </div>
-          {/* <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded" onClick={() => copyToClipboard(`curl -X POST realms.today/api/verify-gossip-keypair/${publicKey ? publicKey : ':public-key'}/{authCode ? authCode : ':discord-authorization-code'} -payload PAYLOAD`)}>Copy</button> */}
-      </div>
-    </div>
+  //   <div className="flex flex-col justify-center items-center h-screen p-4 text-center bg-gray-100 text-black">
+  //     <h1 className="text-3xl font-medium mt-8">Follow the steps to get verified:</h1>
+  //     <p className="italic underline text-center text-neutral-700 mt-4">Do NOT give this information to other parties. If you have any questions or concerns, reach out in the Solana Tech Discord.</p>
+  //     <div className="text-left text-black p-4">
+  //       <input
+  //         type="text"
+  //         className="border rounded text-base mb-2 w-full p-2"
+  //         value={publicKey}
+  //         onChange={(e) => setPublicKey(e.target.value)}
+  //         placeholder="1. Enter your public key (cli command: solana address)"
+  //       />
+  //       <p className="text-base text-neutral-700 mt-4">2. Make sure the Solana CLI ({">= 1.16"}) is installed and configured. See the <a href="https://docs.solana.com/cli/install-solana-cli-tools" className="underline text-black">Solana CLI installation guide</a> for more information.</p>
+  //       <p className="text-base text-neutral-700 mt-4">3. Sign the payload using the Solana CLI’s sign-offchain-message command and send to the endpoint.</p>
+  //       <div className="flex justify-between mb-2">
+  //         <pre style={{whiteSpace: "pre-wrap", wordBreak: "break-all"}} className="p-4 bg-gray-200 rounded">{`solana sign-offchain-message `}{`"`}{authCode ? authCode : ':discord-authorization-code'}{`"`}
+  //         {`| xargs -I {} curl -X POST http://localhost:3001/verify-gossip-keypair/`}{publicKey ? publicKey : ':public-key'}{'/'}
+  //         {authCode ? authCode : ':discord-authorization-code'}{` -H 'Content-Type: application/json' -d '{"signature": "'{}'"}'`}
+  //         </pre>
+  //       </div>
+  //         {/* <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded" onClick={() => copyToClipboard(`curl -X POST realms.today/api/verify-gossip-keypair/${publicKey ? publicKey : ':public-key'}/{authCode ? authCode : ':discord-authorization-code'} -payload PAYLOAD`)}>Copy</button> */}
+  //     </div>
+  //   </div>
 
-  return (
-    <div className="flex flex-col justify-center items-center h-screen p-4 text-center bg-gray-100 text-black">
-      <h1 className="text-3xl font-medium mt-8">Follow the steps to get verified:</h1>
-      <p className="italic underline text-center text-neutral-700 mt-4">Do NOT give this information to other parties. If you have any questions/concerns, reach out in the Solana Tech Discord.</p>
-      <div className="text-left text-black p-4">
-        <input
-          type="text"
-          className="border rounded text-base mb-2 w-full p-2"
-          value={publicKey}
-          onChange={(e) => setPublicKey(e.target.value)}
-          placeholder="Enter your public key"
-        />
-        <p className="text-base text-neutral-700 mt-4">1. Make sure the Solana CLI ({">= 1.16"}) is installed and configured. See the <a href="https://docs.solana.com/cli/install-solana-cli-tools" className="underline text-black">Solana CLI installation guide</a> for more information.</p>
-        <p className="text-base text-neutral-700 mt-4">2. Send a cURL command with your validator public key to realms.today/api/verify-gossip-keypair/:public-key to receive a timebound “challenge” payload.</p>
-        <div className="flex justify-between mb-2">
-          <pre className="p-4 bg-gray-200 rounded">curl -X GET realms.today/api/verify-gossip-keypair/{publicKey ? publicKey : ':public-key'} -public-key VALIDATOR_PUBLIC_KEY</pre>
-          <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded" onClick={() => copyToClipboard(`curl -X GET realms.today/api/verify-gossip-keypair/${publicKey ? publicKey : ':public-key'} -public-key VALIDATOR_PUBLIC_KEY`)}>Copy</button>
-        </div>
-        <p className="text-base text-neutral-700 mt-4">3. Sign the payload using the gossip keypair using the Solana CLI’s sign-offchain-message command.</p>
-        <div className="flex justify-between mb-2">
-          <pre className="p-4 bg-gray-200 rounded">solana sign-offchain-message PAYLOAD</pre>
-          <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded" onClick={() => copyToClipboard(`solana sign-offchain-message PAYLOAD`)}>Copy</button>
-        </div>
-        <p className="text-base text-neutral-700 mt-4">4. POST the signed payload to realms.today/api/verify-gossip-keypair/{publicKey ? publicKey : ':public-key'}/{authCode ? authCode : ':discord-authorization-code'}</p>
-        <div className="flex justify-between">
-          <pre className="p-4 bg-gray-200 rounded">curl -X POST realms.today/api/verify-gossip-keypair/{publicKey ? publicKey : ':public-key'}/{authCode ? authCode : ':discord-authorization-code'} -payload PAYLOAD</pre>
-          <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded" onClick={() => copyToClipboard(`curl -X POST realms.today/api/verify-gossip-keypair/${publicKey ? publicKey : ':public-key'}/{authCode ? authCode : ':discord-authorization-code'} -payload PAYLOAD`)}>Copy</button>
-        </div>
-      </div>
-    </div>
-  );
+  // return (
+  //   <div className="flex flex-col justify-center items-center h-screen p-4 text-center bg-gray-100 text-black">
+  //     <h1 className="text-3xl font-medium mt-8">Follow the steps to get verified:</h1>
+  //     <p className="italic underline text-center text-neutral-700 mt-4">Do NOT give this information to other parties. If you have any questions/concerns, reach out in the Solana Tech Discord.</p>
+  //     <div className="text-left text-black p-4">
+  //       <input
+  //         type="text"
+  //         className="border rounded text-base mb-2 w-full p-2"
+  //         value={publicKey}
+  //         onChange={(e) => setPublicKey(e.target.value)}
+  //         placeholder="Enter your public key"
+  //       />
+  //       <p className="text-base text-neutral-700 mt-4">1. Make sure the Solana CLI ({">= 1.16"}) is installed and configured. See the <a href="https://docs.solana.com/cli/install-solana-cli-tools" className="underline text-black">Solana CLI installation guide</a> for more information.</p>
+  //       <p className="text-base text-neutral-700 mt-4">2. Send a cURL command with your validator public key to realms.today/api/verify-gossip-keypair/:public-key to receive a timebound “challenge” payload.</p>
+  //       <div className="flex justify-between mb-2">
+  //         <pre className="p-4 bg-gray-200 rounded">curl -X GET realms.today/api/verify-gossip-keypair/{publicKey ? publicKey : ':public-key'} -public-key VALIDATOR_PUBLIC_KEY</pre>
+  //         <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded" onClick={() => copyToClipboard(`curl -X GET realms.today/api/verify-gossip-keypair/${publicKey ? publicKey : ':public-key'} -public-key VALIDATOR_PUBLIC_KEY`)}>Copy</button>
+  //       </div>
+  //       <p className="text-base text-neutral-700 mt-4">3. Sign the payload using the gossip keypair using the Solana CLI’s sign-offchain-message command.</p>
+  //       <div className="flex justify-between mb-2">
+  //         <pre className="p-4 bg-gray-200 rounded">solana sign-offchain-message PAYLOAD</pre>
+  //         <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded" onClick={() => copyToClipboard(`solana sign-offchain-message PAYLOAD`)}>Copy</button>
+  //       </div>
+  //       <p className="text-base text-neutral-700 mt-4">4. POST the signed payload to realms.today/api/verify-gossip-keypair/{publicKey ? publicKey : ':public-key'}/{authCode ? authCode : ':discord-authorization-code'}</p>
+  //       <div className="flex justify-between">
+  //         <pre className="p-4 bg-gray-200 rounded">curl -X POST realms.today/api/verify-gossip-keypair/{publicKey ? publicKey : ':public-key'}/{authCode ? authCode : ':discord-authorization-code'} -payload PAYLOAD</pre>
+  //         <button className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded" onClick={() => copyToClipboard(`curl -X POST realms.today/api/verify-gossip-keypair/${publicKey ? publicKey : ':public-key'}/{authCode ? authCode : ':discord-authorization-code'} -payload PAYLOAD`)}>Copy</button>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default ValidatorVerifyPage
