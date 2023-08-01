57c2f589b8747eb2cd51c07fe8c87860ae792df0
agrippa kellum <ask252@cornell.edu>
2023-08-01 14:50:30 -0400

    stop storing TOR pubkey in url for account page

--- pages/dao/[symbol]/account/me.tsx
+++ pages/dao/[symbol]/account/me.tsx
@@ -1,32 +1,32 @@
 import React from 'react'
 import useRealm from '@hooks/useRealm'
-import { useRouter } from 'next/router'
-import { default as VoteStakeRegistryAccount } from '@components/Account'
+import { default as Account } from '@components/Account'
 import LockTokensAccount from 'VoteStakeRegistry/components/Account/LockTokensAccount'
 import { LockTokensAccount as HeliumLockTokensAccount } from 'HeliumVotePlugin/components/LockTokensAccount'
+import { useAddressQuery_CommunityTokenOwner } from '@hooks/queries/addresses/tokenOwnerRecord'
 
-const Account: React.FC = () => {
-  const router = useRouter()
+const AccountPage: React.FC = () => {
   const { vsrMode } = useRealm()
-  const tokenOwnerRecordPk = router?.query?.tokenOwnerRecordPk
+
+  const { data: tokenOwnerRecordPk } = useAddressQuery_CommunityTokenOwner()
 
   if (vsrMode) {
     if (vsrMode === 'helium') {
       return (
         <HeliumLockTokensAccount /* tokenOwnerRecordPk={tokenOwnerRecordPk} */>
-          <VoteStakeRegistryAccount withHeader={false} displayPanel={false} />
+          <Account withHeader={false} displayPanel={false} />
         </HeliumLockTokensAccount>
       )
     }
 
-    return (
+    return tokenOwnerRecordPk ? (
       <LockTokensAccount tokenOwnerRecordPk={tokenOwnerRecordPk}>
-        <VoteStakeRegistryAccount withHeader={false} displayPanel={false} />
+        <Account withHeader={false} displayPanel={false} />
       </LockTokensAccount>
-    )
+    ) : null
   }
 
-  return <VoteStakeRegistryAccount />
+  return <Account />
 }
 
-export default Account
+export default AccountPage
