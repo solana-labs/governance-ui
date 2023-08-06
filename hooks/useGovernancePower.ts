a7c302a6914e5d3a93d2d23a21353b8c75c3f57c
agrippa kellum <ask252@cornell.edu>
2023-08-06 15:04:19 -0400

    .

--- hooks/useGovernancePower.ts
+++ hooks/useGovernancePower.ts
@@ -35,4 +35,25 @@ const useGovernancePower = () => {
   }, [councilMintAddr, torsDelegatedToUser])
 }
 
+export const useCouncilPowerFromDelegators = () => {
+  const realm = useRealmQuery().data?.result
+  const torsDelegatedToUser = useTokenOwnerRecordsDelegatedToUser()
+  const councilMintAddr = realm?.account.config.councilMint
+
+  const councilPowerFromDelegators = useMemo(
+    () =>
+      councilMintAddr === undefined
+        ? undefined
+        : torsDelegatedToUser
+            ?.filter((x) =>
+              x.account.governingTokenMint.equals(councilMintAddr)
+            )
+            .map((x) => x.account.governingTokenDepositAmount)
+            .reduce((x, acc) => x.add(acc), new BN(0)),
+    [councilMintAddr, torsDelegatedToUser]
+  )
+
+  return councilPowerFromDelegators
+}
+
 export default useGovernancePower
