d89dd83216c78b941b999db23a17074a0e1b5adb
agrippa kellum <ask252@cornell.edu>
2023-09-22 15:38:50 -0400

    dont return boolean

--- pages/dao/[symbol]/account/DelegatorOptions.tsx
+++ pages/dao/[symbol]/account/DelegatorOptions.tsx
@@ -6,15 +6,13 @@ import DelegatorsList from './DelegatorsList'
 const DelegatorOptions = () => {
   const delegatesArray = useTokenOwnerRecordsDelegatedToUser()
 
-  return (
-    delegatesArray !== undefined &&
-    delegatesArray.length > 0 && (
-      <div className="bg-bkg-2 p-4 md:p-6 rounded-lg">
-        <h3 className="mb-4">Delegation Options</h3>
-        <SelectPrimaryDelegators />
-        {SHOW_DELEGATORS_LIST ? <DelegatorsList /> : ''}
-      </div>
-    )
+  return delegatesArray === undefined ||
+    delegatesArray.length < 1 ? undefined : (
+    <div className="bg-bkg-2 p-4 md:p-6 rounded-lg">
+      <h3 className="mb-4">Delegation Options</h3>
+      <SelectPrimaryDelegators />
+      {SHOW_DELEGATORS_LIST ? <DelegatorsList /> : ''}
+    </div>
   )
 }
 
