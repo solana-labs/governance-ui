06a5e3a4f6583992c3e7c6084433287046a90441
agrippa kellum <ask252@cornell.edu>
2023-09-22 14:39:45 -0400

    hide delegator options if no delegates

--- pages/dao/[symbol]/account/DelegatorOptions.tsx
+++ pages/dao/[symbol]/account/DelegatorOptions.tsx
@@ -1,14 +1,20 @@
 import SelectPrimaryDelegators from '@components/SelectPrimaryDelegators'
 import { SHOW_DELEGATORS_LIST } from '@constants/flags'
+import { useTokenOwnerRecordsDelegatedToUser } from '@hooks/queries/tokenOwnerRecord'
 import DelegatorsList from './DelegatorsList'
 
 const DelegatorOptions = () => {
+  const delegatesArray = useTokenOwnerRecordsDelegatedToUser()
+
   return (
-    <div className="bg-bkg-2 p-4 md:p-6 rounded-lg">
-      <h3 className="mb-4">Delegation Options</h3>
-      <SelectPrimaryDelegators />
-      {SHOW_DELEGATORS_LIST ? <DelegatorsList /> : ''}
-    </div>
+    delegatesArray !== undefined &&
+    delegatesArray.length > 0 && (
+      <div className="bg-bkg-2 p-4 md:p-6 rounded-lg">
+        <h3 className="mb-4">Delegation Options</h3>
+        <SelectPrimaryDelegators />
+        {SHOW_DELEGATORS_LIST ? <DelegatorsList /> : ''}
+      </div>
+    )
   )
 }
 
