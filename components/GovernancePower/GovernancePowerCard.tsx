c8531064a6950bb6b740859440d2064cb984875a
agrippa kellum <ask252@cornell.edu>
2023-11-08 10:47:52 -0500

    always show deposit options in vanilla

--- components/GovernancePower/GovernancePowerCard.tsx
+++ components/GovernancePower/GovernancePowerCard.tsx
@@ -1,12 +1,19 @@
 import { ChevronRightIcon } from '@heroicons/react/solid'
-import { useGovernancePowerAsync } from '@hooks/queries/governancePower'
+import {
+  determineVotingPowerType,
+  useGovernancePowerAsync,
+} from '@hooks/queries/governancePower'
 import { useRealmConfigQuery } from '@hooks/queries/realmConfig'
+import useSelectedRealmPubkey from '@hooks/selectedRealm/useSelectedRealmPubkey'
 import useQueryContext from '@hooks/useQueryContext'
 import useWalletOnePointOh from '@hooks/useWalletOnePointOh'
 import { GoverningTokenType } from '@solana/spl-governance'
+import { useConnection } from '@solana/wallet-adapter-react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useAsync } from 'react-async-hook'
 import GovernancePowerForRole from './GovernancePowerForRole'
+import { Deposit } from './Vanilla/Deposit'
 
 const GovernancePowerTitle = () => {
   const { symbol } = useRouter().query
@@ -28,6 +35,20 @@ const GovernancePowerTitle = () => {
       </Link>
     </div>
   )
+}
+
+// TODO: refactor deposit components to their own generic DepositForRole component
+const VanillaDeposit = ({ role }: { role: 'community' | 'council' }) => {
+  const { connection } = useConnection()
+
+  const realmPk = useSelectedRealmPubkey()
+
+  const { result: kind } = useAsync(async () => {
+    if (realmPk === undefined) return undefined
+    return determineVotingPowerType(connection, realmPk, role)
+  }, [connection, realmPk, role])
+
+  return kind === 'vanilla' ? <Deposit role={role} /> : <></>
 }
 
 const GovernancePowerCard = () => {
@@ -61,6 +82,10 @@ const GovernancePowerCard = () => {
       ) : bothZero ? (
         <div className={'text-xs text-white/50 mt-8'}>
           You do not have any governance power in this dao
+          <div className="flex flex-col">
+            <VanillaDeposit role="community" />
+            <VanillaDeposit role="council" />
+          </div>
         </div>
       ) : (
         <div className="flex flex-col gap-2">
