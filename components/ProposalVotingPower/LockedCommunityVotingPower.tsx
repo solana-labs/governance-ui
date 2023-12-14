7f88ec4a5bd6cc1cd7b7407716a31f4f8f385d58
agrippa <ask252@cornell.edu>
2023-12-13 19:24:04 -0500

    Agrippa/vsr-locked-race (#1982)

--- components/ProposalVotingPower/LockedCommunityVotingPower.tsx
+++ components/ProposalVotingPower/LockedCommunityVotingPower.tsx
@@ -1,28 +1,18 @@
 import useRealm from '@hooks/useRealm'
 import { BigNumber } from 'bignumber.js'
-import { useCallback, useMemo } from 'react'
+import { useMemo } from 'react'
 import classNames from 'classnames'
 
 import useDepositStore from 'VoteStakeRegistry/stores/useDepositStore'
-import { getMintDecimalAmount } from '@tools/sdk/units'
-import { SecondaryButton } from '@components/Button'
-import useVotePluginsClientStore from 'stores/useVotePluginsClientStore'
-import { notify } from '@utils/notifications'
 
 import { getMintMetadata } from '../instructions/programs/splToken'
-import depositTokensVSR from './depositTokensVSR'
-import useWalletOnePointOh from '@hooks/useWalletOnePointOh'
 import { useRealmQuery } from '@hooks/queries/realm'
-import {
-  useTokenOwnerRecordsDelegatedToUser,
-  useUserCommunityTokenOwnerRecord,
-} from '@hooks/queries/tokenOwnerRecord'
+import { useTokenOwnerRecordsDelegatedToUser } from '@hooks/queries/tokenOwnerRecord'
 import { useRealmCommunityMintInfoQuery } from '@hooks/queries/mintInfo'
-import { useConnection } from '@solana/wallet-adapter-react'
-import BN from 'bn.js'
 import { useVsrGovpower } from '@hooks/queries/plugins/vsr'
 import VSRCommunityVotingPower from 'VoteStakeRegistry/components/TokenBalance/VSRVotingPower'
 import { useSelectedDelegatorStore } from 'stores/useSelectedDelegatorStore'
+import DepositCommunityTokensBtn from 'VoteStakeRegistry/components/TokenBalance/DepositCommunityTokensBtn'
 
 interface Props {
   className?: string
@@ -32,23 +22,15 @@ export default function LockedCommunityVotingPower(props: Props) {
   const realm = useRealmQuery().data?.result
   const mint = useRealmCommunityMintInfoQuery().data?.result
 
-  const { realmInfo, realmTokenAccount } = useRealm()
-  const client = useVotePluginsClientStore((s) => s.state.vsrClient)
-  const { connection } = useConnection()
-  const endpoint = connection.rpcEndpoint
+  const { realmTokenAccount } = useRealm()
 
-  const getOwnedDeposits = useDepositStore((s) => s.getOwnedDeposits)
-  const votingPower = useVsrGovpower().data?.result ?? new BN(0)
+  const {
+    data: votingPowerResult,
+    isLoading: votingPowerLoading,
+  } = useVsrGovpower()
+  const votingPower = votingPowerResult?.result
 
-  const wallet = useWalletOnePointOh()
   const isLoading = useDepositStore((s) => s.state.isLoading)
-
-  const currentTokenOwnerRecord = useUserCommunityTokenOwnerRecord().data
-    ?.result
-
-  const tokenOwnerRecordPk = currentTokenOwnerRecord
-    ? currentTokenOwnerRecord.pubkey
-    : null
 
   const depositMint = realm?.account.communityMint
   const depositAmount = realmTokenAccount
@@ -57,56 +39,6 @@ export default function LockedCommunityVotingPower(props: Props) {
 
   const tokenName =
     getMintMetadata(depositMint)?.name ?? realm?.account.name ?? ''
-
-  const amount =
-    votingPower && mint
-      ? getMintDecimalAmount(mint, votingPower)
-      : new BigNumber('0')
-
-  const deposit = useCallback(async () => {
-    if (
-      client &&
-      realm &&
-      realmInfo &&
-      realmTokenAccount &&
-      wallet &&
-      wallet.publicKey
-    ) {
-      try {
-        await depositTokensVSR({
-          client,
-          connection,
-          endpoint,
-          realm,
-          realmInfo,
-          realmTokenAccount,
-          tokenOwnerRecordPk,
-          wallet,
-        })
-
-        await getOwnedDeposits({
-          client,
-          connection,
-          communityMintPk: realm.account.communityMint,
-          realmPk: realm.pubkey,
-          walletPk: wallet.publicKey,
-        })
-      } catch (e) {
-        console.error(e)
-        notify({ message: `Something went wrong ${e}`, type: 'error' })
-      }
-    }
-  }, [
-    client,
-    connection,
-    endpoint,
-    getOwnedDeposits,
-    realm,
-    realmInfo,
-    realmTokenAccount,
-    tokenOwnerRecordPk,
-    wallet,
-  ])
 
   const delegatedTors = useTokenOwnerRecordsDelegatedToUser()
   const selectedDelegator = useSelectedDelegatorStore(
@@ -127,7 +59,7 @@ export default function LockedCommunityVotingPower(props: Props) {
     [delegatedTors, realm?.account.communityMint, selectedDelegator]
   )
 
-  if (isLoading || !(votingPower && mint)) {
+  if (isLoading || votingPowerLoading || !(votingPower && mint)) {
     return (
       <div
         className={classNames(props.className, 'rounded-md bg-bkg-1 h-[76px]')}
@@ -137,7 +69,7 @@ export default function LockedCommunityVotingPower(props: Props) {
 
   return (
     <div className={props.className}>
-      {amount.isZero() && (relevantDelegators?.length ?? 0) < 1 ? (
+      {votingPower.isZero() && (relevantDelegators?.length ?? 0) < 1 ? (
         <div className={'text-xs text-white/50'}>
           You do not have any voting power in this dao.
         </div>
@@ -155,9 +87,7 @@ export default function LockedCommunityVotingPower(props: Props) {
             more {tokenName} votes in your wallet. Do you want to deposit them
             to increase your voting power in this Dao?
           </div>
-          <SecondaryButton className="mt-4 w-48" onClick={deposit}>
-            Deposit
-          </SecondaryButton>
+          <DepositCommunityTokensBtn inAccountDetails={false} />
         </>
       )}
     </div>
