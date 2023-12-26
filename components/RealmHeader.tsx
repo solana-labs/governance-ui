3d9a9a0d161880600d9b9f1add3cbd3f2ef37a90
cbergz <charlie.bergman@gmail.com>
2023-12-26 14:16:10 -0500

    revert header

--- components/RealmHeader.tsx
+++ components/RealmHeader.tsx
@@ -1,26 +1,15 @@
-// import { useEffect, useState } from 'react'
+import React from 'react'
 import useRealm from 'hooks/useRealm'
-import { ChartPieIcon, ChatAlt2Icon, CogIcon, UsersIcon } from '@heroicons/react/outline'
+import { ChatAlt2Icon, CogIcon, UsersIcon } from '@heroicons/react/outline'
 import { ChevronLeftIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
 import useQueryContext from 'hooks/useQueryContext'
-import { tryParsePublicKey } from '@tools/core/pubkey'
-import { NFT_PLUGINS_PKS } from '@constants/plugins'
 
 const RealmHeader = () => {
   const { fmtUrlWithCluster } = useQueryContext()
-  const {
-    realm,
-    realmInfo,
-    realmDisplayName,
-    symbol,
-    config,
-    vsrMode,
-  } = useRealm()
-  // const { REALM } = process.env
-  const isBackNavVisible = realmInfo?.symbol !== 'ORCA' // hide backnav for the default realm
+  const { realmInfo, realmDisplayName, symbol, config } = useRealm()
 
-  // const explorerHost = getRealmExplorerHost(realmInfo)
+  const isBackNavVisible = realmInfo?.symbol !== 'ORCA' // hide backnav for the default realm
 
   const forumUrl = `https://forums.orca.so/`
 
@@ -40,7 +29,7 @@ const RealmHeader = () => {
         ) : null}
       </div>
       <div className="flex flex-col items-center md:flex-row md:justify-between">
-        {realmInfo?.displayName ? (
+        {realmDisplayName ? (
           <div className="flex items-center">
             <div className="flex flex-col items-center pb-3 md:flex-row md:pb-0">
               {/* {realmInfo?.ogImage ? (
@@ -50,11 +39,11 @@ const RealmHeader = () => {
                 ></img>
               ) : (
                 <div className="bg-[rgba(255,255,255,0.1)] h-14 w-14 flex font-bold items-center justify-center rounded-full text-fgd-3">
-                  {realmInfo.displayName.charAt(0)}
+                  {realmDisplayName?.charAt(0)}
                 </div>
               )} */}
               <div className="flex items-center">
-                <h1 className="ml-3">{realmDisplayName}</h1>
+                <h1 className="ml-3">Orca Governance</h1>
               </div>
             </div>
           </div>
@@ -62,25 +51,11 @@ const RealmHeader = () => {
           <div className="w-40 h-10 rounded-md animate-pulse bg-bkg-3" />
         )}
         <div className="flex items-center space-x-4">
-          {(!config?.account.communityTokenConfig.voterWeightAddin ||
-            NFT_PLUGINS_PKS.includes(
-              config?.account.communityTokenConfig.voterWeightAddin.toBase58()
-            )) && (
+          {!config?.account.communityTokenConfig.voterWeightAddin && (
             <Link href={fmtUrlWithCluster(`/dao/${symbol}/members`)}>
               <a className="flex items-center text-sm cursor-pointer default-transition text-fgd-2 hover:text-fgd-3">
                 <UsersIcon className="flex-shrink-0 w-5 h-5 mr-1" />
                 Members
-              </a>
-            </Link>
-          )}
-          {vsrMode === 'default' && (
-            <Link href={fmtUrlWithCluster(`/dao/${symbol}/token-stats`)}>
-              <a className="flex items-center text-sm cursor-pointer default-transition text-fgd-2 hover:text-fgd-3">
-                <ChartPieIcon className="flex-shrink-0 w-5 h-5 mr-1" />
-                {typeof symbol === 'string' && tryParsePublicKey(symbol)
-                  ? realm?.account.name
-                  : symbol}{' '}
-                stats
               </a>
             </Link>
           )}
