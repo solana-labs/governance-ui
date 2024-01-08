0c3862f8d2279db04ad96290aa88650be9b6561b
Guillermo Bescos <g.bescos@yahoo.com>
2024-01-08 20:38:12 +0000

    Format

--- stores/useVotePluginsClientStore.tsx
+++ stores/useVotePluginsClientStore.tsx
@@ -242,14 +242,14 @@ const useVotePluginsClientStore = create<UseVotePluginsClientStore>(
     },
     handleSetPythClient: async (wallet, connection) => {
       if (wallet) {
-      const pythClient = await PythClient.connect(connection.current, wallet as unknown as Wallet)
-      const maxVoterWeight = (await pythClient.program.methods.updateMaxVoterWeight().pubkeys()).maxVoterRecord
-      set((s) => {
-        s.state.pythClient = pythClient
-        s.state.maxVoterWeight = maxVoterWeight
-      })
+        const pythClient = await PythClient.connect(connection.current, wallet as unknown as Wallet)
+        const maxVoterWeight = (await pythClient.program.methods.updateMaxVoterWeight().pubkeys()).maxVoterRecord
+        set((s) => {
+          s.state.pythClient = pythClient
+          s.state.maxVoterWeight = maxVoterWeight
+        })
+      }
     }
-  }
   })
 )
 
