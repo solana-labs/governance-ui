e89530bbbe28d565723827b5861aa3e9b8c82f55
Adrian Brzeziński <a.brzezinski94@gmail.com>
2024-01-18 02:22:51 +0100

    fix

--- components/instructions/programs/mangoV4.tsx
+++ components/instructions/programs/mangoV4.tsx
@@ -530,22 +530,40 @@ const instructions = () => ({
               />
               <DisplayNullishProperty
                 label="Deposit Limit"
-                value={
+                value={`${
                   mintInfo
                     ? toUiDecimals(
                         new BN(formattedProposedArgs.depositLimit.toString()),
                         mintInfo.account.decimals
                       )
                     : formattedProposedArgs.depositLimit
-                }
-                suggestedVal={
+                } ${args.name} ($${
+                  mintInfo
+                    ? (
+                        toUiDecimals(
+                          new BN(formattedProposedArgs.depositLimit.toString()),
+                          mintInfo.account.decimals
+                        ) * oracleData.uiPrice
+                      ).toFixed(0)
+                    : 0
+                })`}
+                suggestedVal={`${
                   mintInfo && invalidFields?.depositLimit
                     ? toUiDecimals(
                         new BN(invalidFields.depositLimit.toString()),
                         mintInfo.account.decimals
                       )
                     : invalidFields.depositLimit
-                }
+                } ${args.name} ($${
+                  mintInfo && invalidFields.depositLimit
+                    ? (
+                        toUiDecimals(
+                          new BN(invalidFields.depositLimit.toString()),
+                          mintInfo.account.decimals
+                        ) * oracleData.uiPrice
+                      ).toFixed(0)
+                    : 0
+                })`}
               />
               <DisplayListingPropertyWrapped
                 label="Interest Target Utilization"
@@ -1379,9 +1397,30 @@ const instructions = () => ({
               />
               <DisplayNullishProperty
                 label="Deposit Limit"
-                value={parsedArgs.depositLimit}
-                currentValue={bankFormattedValues?.depositLimit}
-                suggestedVal={invalidFields.depositLimit}
+                value={`${
+                  bank && parsedArgs.depositLimit
+                    ? toUiDecimals(
+                        new BN(parsedArgs.depositLimit),
+                        bank.mintDecimals
+                      )
+                    : parsedArgs.depositLimit
+                } ${bank?.name}`}
+                currentValue={`${
+                  bank && bankFormattedValues?.depositLimit
+                    ? toUiDecimals(
+                        new BN(bankFormattedValues.depositLimit),
+                        bank.mintDecimals
+                      )
+                    : bankFormattedValues?.depositLimit
+                } ${bank?.name}`}
+                suggestedVal={`${
+                  bank && invalidFields?.depositLimit
+                    ? toUiDecimals(
+                        new BN(invalidFields.depositLimit),
+                        bank.mintDecimals
+                      )
+                    : invalidFields?.depositLimit
+                } ${bank?.name}`}
               />
               {parsedArgs?.maintWeightShiftAbort && (
                 <DisplayNullishProperty
