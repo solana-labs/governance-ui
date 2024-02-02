a4c7849146b54616eee40380a140ce7e8a3b3b52
Noah Drabinsky <noah.drabinsky1@gmail.com>
2024-02-02 10:29:59 -0700

    Add plugin typing

--- plugin-library/getPlugins.ts
+++ plugin-library/getPlugins.ts
@@ -2,6 +2,7 @@ import { PublicKey, Connection, Keypair } from '@solana/web3.js'
 import { fetchRealmConfigQuery } from '@hooks/queries/realmConfig'
 import { findPluginName, PluginName } from '@constants/plugins'
 import { getRegistrarPDA } from '@utils/plugin/accounts'
+import { BN } from '@coral-xyz/anchor'
 import {
   GatewayClient,
   QuadraticClient,
@@ -9,6 +10,14 @@ import {
 import { loadClient } from './loadClient'
 import { AnchorProvider } from '@coral-xyz/anchor'
 import EmptyWallet from '@utils/Mango/listingTools'
+
+export interface PluginData {
+  pluginProgramId: PublicKey
+  name: string | undefined // you may need undefined here to allow "unknown" plugins
+  params: any // the most challenging one- probably a typed data structure of some kind, that is related to the plugin, e.g. QV plugin has params: { coefficients: number[] }, Gateway plugin has params: { gatekeeperNetwork: Plugin } - these would come from the registrar
+  voterWeight: BN // the weight after applying this plugin (taken from the voter's voterWeightRecord account)
+  maxVoterWeight: BN | undefined // see above - can be undefined if the plugin does not set a max vw
+}
 
 export const getPredecessorProgramId = async (
   client: GatewayClient | QuadraticClient,
