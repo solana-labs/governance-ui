7cd094dd950585592a65d3db278bf04f7593a252
guibescos <59208140+guibescos@users.noreply.github.com>
2024-02-02 17:32:15 +0000

    [Pyth] Display voting power properly (#2098)

--- hooks/useVsrMode.ts
+++ hooks/useVsrMode.ts
@@ -1,8 +1,8 @@
 import { useMemo } from 'react'
-import { HELIUM_VSR_PLUGINS_PKS, VSR_PLUGIN_PKS } from '../constants/plugins'
+import { HELIUM_VSR_PLUGINS_PKS, PYTH_PLUGIN_PK, VSR_PLUGIN_PKS } from '../constants/plugins'
 import { useRealmConfigQuery } from './queries/realmConfig'
 
-export const useVsrMode = (): undefined | 'default' | 'helium' => {
+export const useVsrMode = (): undefined | 'default' | 'helium' | 'pyth' => {
   const config = useRealmConfigQuery().data?.result
   const mode = useMemo(() => {
     const currentPluginPk =
@@ -11,6 +11,7 @@ export const useVsrMode = (): undefined | 'default' | 'helium' => {
     if (VSR_PLUGIN_PKS.includes(currentPluginPk?.toBase58())) return 'default'
     if (HELIUM_VSR_PLUGINS_PKS.includes(currentPluginPk?.toBase58()))
       return 'helium'
+    if (PYTH_PLUGIN_PK.includes(currentPluginPk?.toBase58())) return 'pyth'
   }, [config?.account?.communityTokenConfig])
 
   return mode
