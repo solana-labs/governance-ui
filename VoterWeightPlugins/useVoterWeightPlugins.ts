8899fdd124010aa38fd29cd76d0863a12c585533
dankelleher <dankelleher@yahoo.com>
2024-02-21 11:45:11 +0100

    Some comments

--- VoterWeightPlugins/useVoterWeightPlugins.ts
+++ VoterWeightPlugins/useVoterWeightPlugins.ts
@@ -1,3 +1,15 @@
+/**
+ * This is the *main* voter weight plugin hook. It exposes everything the rest of the code should need to know about voter weight plugins, including:
+ * - the calculated voter weights based on the realm's plugin configuration
+ * - the list of plugins, their names and details
+ * - a number of functions for creating plugin instructions (e.g. updateVoterWeightRecord etc)
+ * Notably not included in here are functions to cast vote or any other instructions on the spl-governance program.
+ * Although these occasionally differ per plugin (castNftVote), they are typically generic and receive a voter weight record as an input.
+ *
+ * This hook deliberately avoids using other hooks in the realms UI. In principle, this allows it to be exported to a separate npm library and used in other SPL-Governance UIs in the future.
+ *
+ * Realms itself should prefer useRealmVoterWeightPlugins which is a convenience wrapper for this hook.
+ */
 import { PublicKey, TransactionInstruction } from '@solana/web3.js'
 import queryClient from '@hooks/queries/queryClient'
 import { updateVoterWeight } from './lib/updateVoterWeight'
@@ -19,6 +31,9 @@ import { PluginName } from '@constants/plugins'
 import {VoterWeightAction} from "@solana/spl-governance";
 import {useTokenOwnerRecord} from "./hooks/useTokenOwnerRecord";
 
+/**
+ * Represents the return type of the `useVoterWeightPlugins` hook.
+ */
 export interface UseVoterWeightPluginsReturnType {
   isReady: boolean
   plugins: VoterWeightPluginInfo[] | undefined // undefined means we are still loading
@@ -38,6 +53,12 @@ export interface UseVoterWeightPluginsReturnType {
   includesPlugin: (name: PluginName) => boolean
 }
 
+/**
+ * Retrieves voter weight plugin information, calculated voter weights and provides functions to create/update voter weight records.
+ *
+ * @param {UseVoterWeightPluginsArgs}
+ * @returns {UseVoterWeightPluginsReturnType}
+ */
 export const useVoterWeightPlugins = (
   args: UseVoterWeightPluginsArgs
 ): UseVoterWeightPluginsReturnType => {
