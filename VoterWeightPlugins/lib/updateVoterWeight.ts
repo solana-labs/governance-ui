d97c332ba2212729f13826524674439b5ca493e6
dankelleher <dankelleher@yahoo.com>
2024-02-21 17:10:51 +0100

    WIP work to accept a predecessor VWR PDA when chaining updateVoterWeight instructions

--- VoterWeightPlugins/lib/updateVoterWeight.ts
+++ VoterWeightPlugins/lib/updateVoterWeight.ts
@@ -4,6 +4,7 @@ import {
 } from '@solana/web3.js'
 import {VoterWeightAction} from "@solana/spl-governance";
 import {VoterWeightPluginInfo} from "./types";
+import mint from "../../pages/dao/[symbol]/proposal/components/instructions/Mint";
 
 interface UpdateVoterWeightRecordArgs {
   walletPublicKey: PublicKey
@@ -23,15 +24,26 @@ export const updateVoterWeight = async ({
   const preIxes: TransactionInstruction[] = []
   const postIxes: TransactionInstruction[] = []
 
+  // This callback returns the previous plugin's voter weight record PDA
+  // We pass it into the current plugin's updateVoterWeightRecord method
+  // so that it knows what the previous voter weight record was.
+  // Most VWRs are derived the same way, but some (e.g. VSR) use a different derivation
+  // function, so this decouples the plugins in this regard.
+  let getVoterWeightRecordCallback: (() => Promise<PublicKey>) | undefined;
+
   for (const plugin of plugins) {
     const updateVoterWeightRecordIx = await plugin.client.updateVoterWeightRecord(
       walletPublicKey,
       realmPublicKey,
       governanceMintPublicKey,
-      action
+      action,
+      getVoterWeightRecordCallback
     )
     preIxes.push(...updateVoterWeightRecordIx.pre)
     postIxes.push(...updateVoterWeightRecordIx.post || [])
+
+    getVoterWeightRecordCallback = async () => plugin.client.getVoterWeightRecordPDA(realmPublicKey, governanceMintPublicKey, walletPublicKey).voterWeightPk
   }
+
   return { pre: preIxes, post: postIxes }
 }
