f85ba0c0dc9dea6b17756d8e8c45596c5488bfd1
agrippa kellum <ask252@cornell.edu>
2024-03-15 13:08:42 -0400

    .

--- components/GovernancePower/Power/VotingPowerCards.tsx
+++ components/GovernancePower/Power/VotingPowerCards.tsx
@@ -8,6 +8,7 @@ import LockedCommunityNFTRecordVotingPower from '@components/ProposalVotingPower
 import QuadraticVotingPower from '@components/ProposalVotingPower/QuadraticVotingPower'
 import { VSRCard } from '@components/GovernancePower/Power/VSRCard'
 import { VanillaCard } from '@components/GovernancePower/Power/Vanilla/VanillaCard'
+import DriftVotingPower from 'DriftStakeVoterPlugin/components/DriftVotingPower'
 
 /****
  * Note to plugin implementors.
@@ -31,6 +32,7 @@ const pluginsWithDedicatedVotingPowerUI = [
   'VSR',
   'gateway',
   'QV',
+  'drift',
 ] as const
 
 export type VotingCardProps = {
@@ -67,6 +69,8 @@ const CardForPlugin: FC<
       return <GatewayCard role={role} />
     case 'QV':
       return <QuadraticVotingPower role={role} />
+    case 'drift':
+      return <DriftVotingPower role={role} />
   }
 }
 
