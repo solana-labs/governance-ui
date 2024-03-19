f5cc814707999f75203a4e740c235017c4ee58f8
dankelleher <dankelleher@yahoo.com>
2024-03-19 16:15:32 +0100

    Fix delegation with plugins - currently TORs are created on voting, but VWRs are not. This means plugins with non-emphemeral VWRs will not work for delegates who have not themselves onboarded, in "batch voting" mode.
    After discussion, this will be solved by filtering those out of the voting batch, but that will be done in a separate PR. For now, batch voting mode is disabled for these plugins. Additionally, a bug is fixed that shows the combined votes on the UI when "Yourself only" is picked

--- components/ProposalVotingPower/PluginVotingPower.tsx
+++ components/ProposalVotingPower/PluginVotingPower.tsx
@@ -16,10 +16,45 @@ import Button from "@components/Button";
 import {sendTransaction} from "@utils/send";
 import {TokenDeposit} from "@components/TokenBalance/TokenDeposit";
 import {GoverningTokenRole} from "@solana/spl-governance";
+import {useSelectedDelegatorStore} from "../../stores/useSelectedDelegatorStore";
+import {GovernanceRole} from "../../@types/types";
+import {CalculatedWeight} from "../../VoterWeightPlugins/lib/types";
+import {DELEGATOR_BATCH_VOTE_SUPPORT_BY_PLUGIN} from "@constants/flags";
 
 interface Props {
   className?: string
-  role: 'community' | 'council'
+  role: GovernanceRole
+}
+
+const useVoterWeight = (role: GovernanceRole): CalculatedWeight | undefined => {
+  const wallet = useWalletOnePointOh();
+  // these hooks return different results depending on whether batch delegator voting is supported
+  // if batch is on, and these are undefined, it means "yourself + all delegators"
+  // if batch is off, and these are undefined, it means "yourself only"
+  // if batch is on, and yourself only is picked, the selectedDelegator will be the current logged-in wallet
+  const selectedCommunityDelegator = useSelectedDelegatorStore((s) => s.communityDelegator)
+  const selectedCouncilDelegator = useSelectedDelegatorStore((s) => s.councilDelegator)
+  const selectedDelegatorForRole = role === 'community' ? selectedCommunityDelegator : selectedCouncilDelegator;
+  const votingWallet = (role === 'community' ? selectedCommunityDelegator : selectedCouncilDelegator) ?? wallet?.publicKey
+
+  const { plugins, totalCalculatedVoterWeight, voterWeightForWallet } = useRealmVoterWeightPlugins(role)
+
+  // if the plugin supports delegator batch voting (or no plugins exist on the dao),
+  // and no delegator is selected, we can use totalCalculatedVoterWeight
+  // otherwise, use the voterWeightForWallet for the correct delegator or the wallet itself
+  const lastPlugin = plugins?.voterWeight[plugins.voterWeight.length - 1];
+  const supportsBatchVoting = !lastPlugin || DELEGATOR_BATCH_VOTE_SUPPORT_BY_PLUGIN[lastPlugin?.name]
+
+  // the user has selected "yourself + all delegators" and the plugin supports batch voting
+  if (supportsBatchVoting && !selectedDelegatorForRole) {
+    return totalCalculatedVoterWeight;
+  }
+
+  // there is no wallet to calculate voter weight for
+  if (!votingWallet) return undefined;
+
+  // the user has selected a specific delegator or "yourself only"
+  return voterWeightForWallet(votingWallet);
 }
 
 export default function PluginVotingPower({ role, className }: Props) {
@@ -30,19 +65,20 @@ export default function PluginVotingPower({ role, className }: Props) {
   const { userNeedsTokenOwnerRecord, userNeedsVoterWeightRecords, handleRegister } = useJoinRealm();
   const mintInfo = useMintInfoByPubkeyQuery(realm?.account.communityMint).data
     ?.result
-
   const isLoading = useDepositStore((s) => s.state.isLoading)
+  const voterWeight = useVoterWeight(role);
 
-  const { totalCalculatedVoterWeight, isReady } = useRealmVoterWeightPlugins(role)
+  const { isReady } = useRealmVoterWeightPlugins(role)
 
   const formattedTotal = useMemo(
-    () =>
-      mintInfo && totalCalculatedVoterWeight?.value
-        ? new BigNumber(totalCalculatedVoterWeight?.value.toString())
-            .shiftedBy(-mintInfo.decimals)
-            .toString()
-        : undefined,
-    [mintInfo, totalCalculatedVoterWeight?.value]
+    () => {
+      return mintInfo && voterWeight?.value
+          ? new BigNumber(voterWeight?.value.toString())
+              .shiftedBy(-mintInfo.decimals)
+              .toString()
+          : undefined;
+    },
+    [mintInfo, voterWeight?.value]
   )
   // There are two buttons available on this UI:
   // The Deposit button - available if you have tokens to deposit
