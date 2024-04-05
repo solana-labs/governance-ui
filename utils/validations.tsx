6b73b877bbda2355f660bef23a175991b4439bee
Utkarsh <83659045+0xShuk@users.noreply.github.com>
2024-04-05 16:48:08 +0530

    add burn instruction (#2224)

--- utils/validations.tsx
+++ utils/validations.tsx
@@ -981,6 +981,69 @@ export const getTokenTransferSchema = ({
   })
 }
 
+export const getBurnTokensSchema = ({
+  form,
+  tokenAmount,
+  mintDecimals,
+  nftMode,
+  ignoreAmount,
+}: {
+  form: any
+  tokenAmount?: BN
+  mintDecimals?: number
+  nftMode?: boolean
+  ignoreAmount?: boolean
+}) => {
+  const governedTokenAccount = form.governedTokenAccount as AssetAccount
+  return yup.object().shape({
+    governedTokenAccount: yup.object().required('Token account is required'),
+    amount: yup
+      .number()
+      .typeError('Amount is required')
+      .test(
+        'amount',
+        'Transfer amount must be less than the source account available amount',
+        async function (val: number) {
+          const isNft = nftMode || governedTokenAccount?.isNft
+          if (isNft || ignoreAmount) {
+            return true
+          }
+          if (val && !form.governedTokenAccount) {
+            return this.createError({
+              message: `Please select source account to validate the amount`,
+            })
+          }
+          if (
+            val &&
+            governedTokenAccount &&
+            governedTokenAccount?.extensions.mint
+          ) {
+            const mintValue = getMintNaturalAmountFromDecimalAsBN(
+              val,
+              typeof mintDecimals !== 'undefined'
+                ? mintDecimals
+                : governedTokenAccount?.extensions.mint.account.decimals
+            )
+            if (tokenAmount) {
+              return tokenAmount.gte(mintValue)
+            }
+            return !!(governedTokenAccount?.extensions.token?.publicKey &&
+            !governedTokenAccount.isSol
+              ? governedTokenAccount.extensions.token.account.amount.gte(
+                  mintValue
+                )
+              : new BN(
+                  governedTokenAccount.extensions.solAccount!.lamports
+                ).gte(mintValue))
+          }
+          return this.createError({
+            message: `Amount is required`,
+          })
+        }
+      ),
+  })
+}
+
 export const getMintSchema = ({ form, connection }) => {
   return yup.object().shape({
     amount: yup
