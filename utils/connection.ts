58ab605b6126568ac686f7689f34dc2cf45ff422
agrippa kellum <ask252@cornell.edu>
2024-04-26 15:29:58 -0400

    sync endpoint definitions

--- utils/connection.ts
+++ utils/connection.ts
@@ -1,6 +1,7 @@
 import type { EndpointTypes } from '@models/types'
 import { Connection } from '@solana/web3.js'
 import type { EndpointInfo } from '../@types/types'
+import { DEVNET_RPC, MAINNET_RPC } from '@constants/endpoints'
 
 export const BACKUP_CONNECTIONS = [
   new Connection('https://api.mngo.cloud/lite-rpc/v1/', 'recent'),
@@ -9,13 +10,11 @@ export const BACKUP_CONNECTIONS = [
 const ENDPOINTS: EndpointInfo[] = [
   {
     name: 'mainnet',
-    url:
-      process.env.MAINNET_RPC ||
-      'http://realms-realms-c335.mainnet.rpcpool.com/258d3727-bb96-409d-abea-0b1b4c48af29/',
+    url: MAINNET_RPC,
   },
   {
     name: 'devnet',
-    url: process.env.DEVNET_RPC || 'https://api.dao.devnet.solana.com/',
+    url: DEVNET_RPC,
   },
   {
     name: 'localnet',
