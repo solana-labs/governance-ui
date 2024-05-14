3031dd1559c2f8ece33ce27c06886010e6594d9b
agrippa kellum <ask252@cornell.edu>
2024-05-14 11:18:59 -0400

    remove triton token

--- hub/providers/Cluster/index.tsx
+++ hub/providers/Cluster/index.tsx
@@ -6,8 +6,7 @@ import { createContext, useEffect, useState } from 'react';
 const DEVNET_RPC_ENDPOINT =
   process.env.DEVNET_RPC || 'https://api.dao.devnet.solana.com/';
 const MAINNET_RPC_ENDPOINT =
-  process.env.MAINNET_RPC ||
-  'http://realms-realms-c335.mainnet.rpcpool.com/258d3727-bb96-409d-abea-0b1b4c48af29/';
+  process.env.MAINNET_RPC || 'http://realms-realms-c335.mainnet.rpcpool.com';
 const TESTNET_RPC_ENDPOINT = 'http://127.0.0.1:8899';
 
 export enum ClusterType {
