6580fb7ed6b950fc3d12e20f4977143c66e02d1d
agrippa kellum <ask252@cornell.edu>
2024-05-19 14:31:51 -0400

    change token-haver program id

--- TokenHaverPlugin/constants.ts
+++ TokenHaverPlugin/constants.ts
@@ -1,5 +1,5 @@
 import { PublicKey } from '@solana/web3.js'
 
 export const TOKEN_HAVER_PLUGIN = new PublicKey(
-  'vo65JQC6U8HCDPfoJxRrr17n7RZ5fKdfyjHm3erHJ2V'
+  '7gobfUihgoxA14RUnVaseoah89ggCgYAzgz1JoaPAXam'
 )
