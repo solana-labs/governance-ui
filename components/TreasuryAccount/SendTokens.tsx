cad2e4d45d84a14b2b7ddc73f84bcd95c834102d
0xShuk <shukarsh@gmail.com>
2024-06-23 16:37:33 +0530

    add: batch token transfer

--- components/TreasuryAccount/SendTokens.tsx
+++ components/TreasuryAccount/SendTokens.tsx
@@ -11,16 +11,16 @@ import {
 } from '@tools/sdk/units'
 import { tryParseKey } from '@tools/validators/pubkey'
 import { debounce } from '@utils/debounce'
-import { abbreviateAddress, precision } from '@utils/formatting'
+import { precision } from '@utils/formatting'
 import { TokenProgramAccount, tryGetTokenAccount } from '@utils/tokens'
 import {
   SendTokenCompactViewForm,
   UiInstruction,
 } from '@utils/uiTypes/proposalCreationTypes'
-import React, { useEffect, useState } from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import useTreasuryAccountStore from 'stores/useTreasuryAccountStore'
 
-import { getTokenTransferSchema } from '@utils/validations'
+import { getBatchTokenTransferSchema } from '@utils/validations'
 import {
   ArrowCircleDownIcon,
   ArrowCircleUpIcon,
@@ -37,8 +37,8 @@ import AccountLabel from './AccountHeader'
 import Tooltip from '@components/Tooltip'
 import useGovernanceAssets from '@hooks/useGovernanceAssets'
 import {
-  getSolTransferInstruction,
-  getTransferInstruction,
+  getBatchSolTransferInstruction,
+  getBatchTransferInstruction,
 } from '@utils/instructionTools'
 import VoteBySwitch from 'pages/dao/[symbol]/proposal/components/VoteBySwitch'
 import useCreateProposal from '@hooks/useCreateProposal'
@@ -48,6 +48,8 @@ import useLegacyConnectionContext from '@hooks/useLegacyConnectionContext'
 import { fetchJupiterPrice } from '@hooks/queries/jupiterPrice'
 import { useAsync } from 'react-async-hook'
 import {useVoteByCouncilToggle} from "@hooks/useVoteByCouncilToggle";
+import { AddAlt } from '@carbon/icons-react'
+import { StyledLabel } from '@components/inputs/styles'
 
 const SendTokens = () => {
   const currentAccount = useTreasuryAccountStore((s) => s.currentAccount)
@@ -63,8 +65,9 @@ const SendTokens = () => {
   const router = useRouter()
   const programId: PublicKey | undefined = realmInfo?.programId
   const [form, setForm] = useState<SendTokenCompactViewForm>({
-    destinationAccount: '',
-    amount: undefined,
+    destinationAccount: [''],
+    txDollarAmount: [undefined],
+    amount: [undefined],
     governedTokenAccount: undefined,
     programId: programId?.toString(),
     mintInfo: undefined,
@@ -76,12 +79,15 @@ const SendTokens = () => {
   const [
     destinationAccount,
     setDestinationAccount,
-  ] = useState<TokenProgramAccount<AccountInfo> | null>(null)
+  ] = useState<(TokenProgramAccount<AccountInfo> | null)[]>([null])
+
   const [isLoading, setIsLoading] = useState(false)
   const [formErrors, setFormErrors] = useState({})
-  const destinationAccountName =
-    destinationAccount?.publicKey &&
-    getAccountName(destinationAccount?.account.address)
+  const destinationAccountName = destinationAccount.map(acc => (
+    acc?.publicKey &&
+    getAccountName(acc?.account.address)
+  ))
+
   const mintMinAmount = form.governedTokenAccount?.extensions?.mint
     ? getMintMinAmountAsDecimal(
         form.governedTokenAccount.extensions.mint.account
@@ -93,28 +99,66 @@ const SendTokens = () => {
     setFormErrors({})
     setForm({ ...form, [propertyName]: value })
   }
-  const setAmount = (event) => {
+
+  const handleSetMultipleProps = (
+    {destinationAccount, amount, txDollarAmount} : 
+    {amount: any, txDollarAmount: any, destinationAccount?: any}
+  ) => {
+    setFormErrors({})
+    
+    setForm({ ...form,
+      destinationAccount : destinationAccount ? destinationAccount : form.destinationAccount,
+      amount,
+      txDollarAmount
+    })
+  }
+
+  const setAmount = (idx: number, event: ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value
-    handleSetForm({
-      value: value,
-      propertyName: 'amount',
-    })
-  }
-  const validateAmountOnBlur = () => {
-    const value = form.amount
+    const newAmounts: any[] = [...form.amount]
+    newAmounts[idx] = value
 
     handleSetForm({
-      value: parseFloat(
-        Math.max(
-          Number(mintMinAmount),
-          Math.min(Number(Number.MAX_SAFE_INTEGER), Number(value))
-        ).toFixed(currentPrecision)
-      ),
+      value: newAmounts,
       propertyName: 'amount',
     })
   }
 
-  async function getInstruction(): Promise<UiInstruction> {
+  const validateAmountOnBlur = async(idx: number) => {
+    const value = form.amount[idx]
+    const newAmounts = [...form.amount]
+    const newTxDollars = [...form.txDollarAmount]
+
+    const newVal = parseFloat(
+      Math.max(
+        Number(mintMinAmount),
+        Math.min(Number(Number.MAX_SAFE_INTEGER), Number(value))
+      ).toFixed(currentPrecision)
+    )
+    
+    console.log(newVal)
+
+    newAmounts[idx] = newVal
+
+    const mint = currentAccount?.extensions.mint?.publicKey
+    if (mint === undefined) {
+      newTxDollars[idx] = undefined
+    } else {
+      const priceData = await fetchJupiterPrice(mint)
+      const price = priceData.result?.price ?? 0
+  
+      const totalPrice = newVal * price
+      const totalPriceFormatted = newVal && price ? new BigNumber(totalPrice).toFormat(2) : ''  
+      newTxDollars[idx] = totalPriceFormatted
+    }
+    
+    handleSetMultipleProps({
+      amount: newAmounts,
+      txDollarAmount: newTxDollars
+    })
+  }
+
+  async function getInstruction(): Promise<UiInstruction[]> {
     const defaultProps = {
       schema,
       form,
@@ -125,34 +169,36 @@ const SendTokens = () => {
       setFormErrors,
     }
     if (isSol) {
-      return getSolTransferInstruction(defaultProps)
+      return getBatchSolTransferInstruction(defaultProps)
     }
-    return getTransferInstruction(defaultProps)
+    return getBatchTransferInstruction(defaultProps)
   }
 
   const handleProposeTransfer = async () => {
     setIsLoading(true)
-    const instruction: UiInstruction = await getInstruction()
-    if (instruction.isValid) {
+    const instruction: UiInstruction[] = await getInstruction()
+    
+    if (instruction.every(ix => ix.isValid)) {
       const governance = currentAccount?.governance
       let proposalAddress: PublicKey | null = null
       if (!realm) {
         setIsLoading(false)
         throw 'No realm selected'
       }
-      const instructionData = {
-        data: instruction.serializedInstruction
-          ? getInstructionDataFromBase64(instruction.serializedInstruction)
+      const instructionsData = instruction.map(ix => ({
+        data: ix.serializedInstruction
+          ? getInstructionDataFromBase64(ix.serializedInstruction)
           : null,
         holdUpTime: governance?.account?.config.minInstructionHoldUpTime,
-        prerequisiteInstructions: instruction.prerequisiteInstructions || [],
-      }
+        prerequisiteInstructions: ix.prerequisiteInstructions || [],
+      }))
+
       try {
         proposalAddress = await handleCreateProposal({
           title: form.title ? form.title : proposalTitle,
           description: form.description ? form.description : '',
           voteByCouncil,
-          instructionsData: [instructionData],
+          instructionsData,
           governance: governance!,
         })
         const url = fmtUrlWithCluster(
@@ -166,10 +212,10 @@ const SendTokens = () => {
     setIsLoading(false)
   }
 
-  const IsAmountNotHigherThenBalance = () => {
+  const IsAmountNotHigherThenBalance = (idx: number) => {
     try {
       const mintValue = getMintNaturalAmountFromDecimalAsBN(
-        form.amount!,
+        form.amount[idx]!,
         form.governedTokenAccount!.extensions.mint!.account.decimals
       )
       let gte: boolean | undefined = false
@@ -189,49 +235,80 @@ const SendTokens = () => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps -- TODO please fix, it can cause difficult bugs. You might wanna check out https://bobbyhadz.com/blog/react-hooks-exhaustive-deps for info. -@asktree
   }, [currentAccount])
-  useEffect(() => {
-    if (form.destinationAccount) {
-      debounce.debounceFcn(async () => {
-        const pubKey = tryParseKey(form.destinationAccount)
-        if (pubKey) {
-          const account = await tryGetTokenAccount(connection.current, pubKey)
-          setDestinationAccount(account ? account : null)
-        } else {
-          setDestinationAccount(null)
-        }
-      })
-    } else {
-      setDestinationAccount(null)
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- TODO please fix, it can cause difficult bugs. You might wanna check out https://bobbyhadz.com/blog/react-hooks-exhaustive-deps for info. -@asktree
-  }, [form.destinationAccount])
 
-  const schema = getTokenTransferSchema({ form, connection, nftMode: false })
+  const schema = getBatchTokenTransferSchema({ form, connection, nftMode: false })
 
-  const { result: transactionDolarAmount } = useAsync(async () => {
-    const mint = currentAccount?.extensions.mint?.publicKey
-    if (mint === undefined) return undefined
-    const amount = form.amount ?? 0
-    const priceData = await fetchJupiterPrice(mint)
-    const price = priceData.result?.price ?? 0
+  // const { result: transactionDolarAmount } = useAsync(async () => {
+  //   const mint = currentAccount?.extensions.mint?.publicKey
+  //   if (mint === undefined) return undefined
+  //   const amount = form.amount ?? 0
+  //   const priceData = await fetchJupiterPrice(mint)
+  //   const price = priceData.result?.price ?? 0
 
-    const totalPrice = amount * price
-    const totalPriceFormatted =
-      amount && price ? new BigNumber(totalPrice).toFormat(2) : ''
-    return totalPriceFormatted
-  }, [form.amount, currentAccount?.extensions.mint?.publicKey])
+  //   const totalPrice = amount * price
+  //   const totalPriceFormatted =
+  //     amount && price ? new BigNumber(totalPrice).toFormat(2) : ''
+  //   return totalPriceFormatted
+  // }, [form.amount, currentAccount?.extensions.mint?.publicKey])
 
-  const proposalTitle = `Pay ${form.amount}${
-    tokenInfo ? ` ${tokenInfo?.symbol} ` : ' '
-  }to ${
-    tryParseKey(form.destinationAccount)
-      ? abbreviateAddress(new PublicKey(form.destinationAccount))
-      : ''
-  }`
+  const proposalTitle = `Transfer tokens`
+  // ${
+  //   tokenInfo ? ` ${tokenInfo?.symbol} ` : ' '
+  // }to ${
+  //   tryParseKey(form.destinationAccount)
+  //     ? abbreviateAddress(new PublicKey(form.destinationAccount))
+  //     : ''
+  // }`
 
   if (!currentAccount) {
     return null
   }
+
+  const addRecepient = () => {
+    const newAddresses = [...form.destinationAccount]
+    const newAmounts = [...form.amount]
+    const newTxDollars = [...form.txDollarAmount]
+
+    newAddresses.push("")
+    newAmounts.push(undefined)
+    newTxDollars.push(undefined)
+
+    handleSetMultipleProps({
+      destinationAccount: newAddresses,
+      amount: newAmounts,
+      txDollarAmount: newTxDollars
+    })
+
+    const currentAccounts = [...destinationAccount]
+    currentAccounts.push(null)
+    setDestinationAccount(currentAccounts)
+  }
+
+  const setAddress = (idx: number, address: string) => {
+    const newAddresses = [...form.destinationAccount]
+    newAddresses[idx] = address
+
+    handleSetForm({
+      value: newAddresses,
+      propertyName: 'destinationAccount',
+    })
+
+    const currentAccounts = [...destinationAccount]
+
+    debounce.debounceFcn(async () => {
+      const pubKey = tryParseKey(address)
+      if (pubKey) {
+        const account = await tryGetTokenAccount(connection.current, pubKey)
+        currentAccounts[idx] = account ? account : null
+        setDestinationAccount(currentAccounts)
+      } else {
+        currentAccounts[idx] = null
+        setDestinationAccount(currentAccounts)
+      }
+    })
+  }
+
+  console.log(form)
 
   return (
     <>
@@ -240,53 +317,63 @@ const SendTokens = () => {
       </h3>
       <AccountLabel />
       <div className="space-y-4 w-full pb-4">
-        <Input
-          label="Destination account"
-          value={form.destinationAccount}
-          type="text"
-          onChange={(evt) =>
-            handleSetForm({
-              value: evt.target.value,
-              propertyName: 'destinationAccount',
-            })
-          }
-          noMaxWidth={true}
-          error={formErrors['destinationAccount']}
-        />
-        {destinationAccount && (
-          <div>
-            <div className="pb-0.5 text-fgd-3 text-xs">Account owner</div>
-            <div className="text-xs break-all">
-              {destinationAccount.account.owner.toString()}
-            </div>
-          </div>
-        )}
-        {destinationAccountName && (
-          <div>
-            <div className="pb-0.5 text-fgd-3 text-xs">Account name</div>
-            <div className="text-xs break-all">{destinationAccountName}</div>
-          </div>
-        )}
+        {form.destinationAccount.map((acc, idx) => (
+          <div className="flex flex-col gap-2" key={idx}>
+            <StyledLabel>Recepient {idx+1}</StyledLabel>
+            <Input
+              label="Destination account"
+              value={form.destinationAccount[idx]}
+              type="text"
+              onChange={e => setAddress(idx, e.target.value)}
+              noMaxWidth={true}
+              error={
+                formErrors['destinationAccount'] && formErrors['destinationAccount'][idx] ? 
+                formErrors['destinationAccount'][idx] : ""
+              }
+            />
+            {destinationAccount[idx] && (
+              <div>
+                <div className="pb-0.5 text-fgd-3 text-xs">Account owner</div>
+                <div className="text-xs break-all">
+                  {destinationAccount[idx]!.account.owner.toString()}
+                </div>
+              </div>
+            )}
+            {destinationAccountName[idx] && (
+              <div>
+                <div className="pb-0.5 text-fgd-3 text-xs">Account name</div>
+                <div className="text-xs break-all">{destinationAccountName[idx]}</div>
+              </div>
+            )}
 
-        <Input
-          min={mintMinAmount}
-          label={`Amount ${tokenInfo ? tokenInfo?.symbol : ''}`}
-          value={form.amount}
-          type="number"
-          onChange={setAmount}
-          step={mintMinAmount}
-          error={formErrors['amount']}
-          onBlur={validateAmountOnBlur}
-          noMaxWidth={true}
-        />
+            <Input
+              min={mintMinAmount}
+              label={`Amount ${tokenInfo ? tokenInfo?.symbol : ''}`}
+              value={form.amount[idx]}
+              type="number"
+              onChange={e => setAmount(idx, e)}
+              step={mintMinAmount}
+              error={formErrors['amount'] && formErrors['amount'][idx] ? formErrors['amount'][idx] : ""}
+              onBlur={() => validateAmountOnBlur(idx)}
+              noMaxWidth={true}
+            />
 
-        <small className="text-red">
-          {transactionDolarAmount
-            ? IsAmountNotHigherThenBalance()
-              ? `~$${transactionDolarAmount}`
-              : 'Insufficient balance'
-            : null}
-        </small>
+            <small className="text-red">
+              {form.txDollarAmount[idx]
+                ? IsAmountNotHigherThenBalance(idx)
+                  ? `~$${form.txDollarAmount[idx]}`
+                  : 'Insufficient balance'
+                : null}
+            </small>
+          </div>
+        ))}
+        <div 
+          className="flex gap-2 items-center justify-end cursor-pointer text-sm"
+          onClick={addRecepient}
+        >
+          <AddAlt className="text-green" />
+          <div>Add another recepient</div>
+        </div>
         <div
           className={'flex items-center hover:cursor-pointer w-24'}
           onClick={() => setShowOptions(!showOptions)}
