3d37655a579d336099facc475fa6f95d6b43cf52
cbergz <charlie.bergman@gmail.com>
2024-06-26 14:29:01 -0400

    fix tokenprice

--- utils/services/tokenPrice.tsx
+++ utils/services/tokenPrice.tsx
@@ -1,5 +1,5 @@
-// import axios from 'axios'
-// import { mergeDeepRight } from 'ramda'
+import axios from 'axios'
+import { mergeDeepRight } from 'ramda'
 
 import { notify } from '@utils/notifications'
 import { WSOL_MINT } from '@components/instructions/tools'
@@ -10,59 +10,59 @@ import { USDC_MINT } from '@blockworks-foundation/mango-v4'
 
 // //this service provide prices it is not recommended to get anything more from here besides token name or price.
 // //decimals from metadata can be different from the realm on chain one
-// const priceEndpoint = 'https://price.jup.ag/v4/price'
-// const tokenListUrl = 'https://token.jup.ag/strict'
+const priceEndpoint = 'https://price.jup.ag/v4/price'
+const tokenListUrl = 'https://token.jup.ag/strict'
 
-// export type TokenInfoWithoutDecimals = Omit<TokenInfo, 'decimals'>
+export type TokenInfoWithoutDecimals = Omit<TokenInfo, 'decimals'>
 
-// /** @deprecated */
-// class TokenPriceService {
-//   _tokenList: TokenInfo[]
-//   _tokenPriceToUSDlist: {
-//     [mintAddress: string]: Price
-//   }
-//   constructor() {
-//     this._tokenList = []
-//     this._tokenPriceToUSDlist = {}
-//   }
-//   async fetchSolanaTokenList() {
-//     try {
-//       const tokens = await axios.get(tokenListUrl)
-//       const tokenList = tokens.data as TokenInfo[]
-//       if (tokenList && tokenList.length) {
-//         this._tokenList = tokenList.map((token) => {
-//           const override = overrides[token.address]
+/** @deprecated */
+class TokenPriceService {
+  _tokenList: TokenInfo[]
+  _tokenPriceToUSDlist: {
+    [mintAddress: string]: Price
+  }
+  constructor() {
+    this._tokenList = []
+    this._tokenPriceToUSDlist = {}
+  }
+  async fetchSolanaTokenList() {
+    try {
+      const tokens = await axios.get(tokenListUrl)
+      const tokenList = tokens.data as TokenInfo[]
+      if (tokenList && tokenList.length) {
+        this._tokenList = tokenList.map((token) => {
+          const override = overrides[token.address]
 
-//           if (override) {
-//             return mergeDeepRight(token, override)
-//           }
+          if (override) {
+            return mergeDeepRight(token, override)
+          }
 
-//           return token
-//         })
-//       }
-//     } catch (e) {
-//       console.log(e)
-//       notify({
-//         type: 'error',
-//         message: 'unable to fetch token list',
-//       })
-//     }
-//   }
-//   async fetchTokenPrices(mintAddresses: string[]) {
-//     if (mintAddresses.length) {
-//       //can query only 100 at once
-//       const mintAddressesWithSol = chunks([...mintAddresses, WSOL_MINT], 100)
-//       for (const mintChunk of mintAddressesWithSol) {
-//         const symbols = mintChunk.join(',')
-//         try {
-//           const response = await axios.get(`${priceEndpoint}?ids=${symbols}`)
-//           const priceToUsd: Price[] = response?.data?.data
-//             ? Object.values(response.data.data)
-//             : []
-//           const keyValue = Object.fromEntries(
-//             // eslint-disable-next-line @typescript-eslint/no-unused-vars
-//             Object.entries(priceToUsd).map(([key, val]) => [val.id, val])
-//           )
+          return token
+        })
+      }
+    } catch (e) {
+      console.log(e)
+      notify({
+        type: 'error',
+        message: 'unable to fetch token list',
+      })
+    }
+  }
+  async fetchTokenPrices(mintAddresses: string[]) {
+    if (mintAddresses.length) {
+      //can query only 100 at once
+      const mintAddressesWithSol = chunks([...mintAddresses, WSOL_MINT], 100)
+      for (const mintChunk of mintAddressesWithSol) {
+        const symbols = mintChunk.join(',')
+        try {
+          const response = await axios.get(`${priceEndpoint}?ids=${symbols}`)
+          const priceToUsd: Price[] = response?.data?.data
+            ? Object.values(response.data.data)
+            : []
+          const keyValue = Object.fromEntries(
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            Object.entries(priceToUsd).map(([key, val]) => [val.id, val])
+          )
 
           this._tokenPriceToUSDlist = {
             ...this._tokenPriceToUSDlist,
@@ -86,47 +86,47 @@ import { USDC_MINT } from '@blockworks-foundation/mango-v4'
         }
       }
 
-//       //override chai price if its broken
-//       const chaiMint = '3jsFX1tx2Z8ewmamiwSU851GzyzM2DJMq7KWW5DM8Py3'
-//       const chaiData = this._tokenPriceToUSDlist[chaiMint]
+      //override chai price if its broken
+      const chaiMint = '3jsFX1tx2Z8ewmamiwSU851GzyzM2DJMq7KWW5DM8Py3'
+      const chaiData = this._tokenPriceToUSDlist[chaiMint]
 
-//       if (chaiData?.price && (chaiData.price > 1.3 || chaiData.price < 0.9)) {
-//         this._tokenPriceToUSDlist[chaiMint] = {
-//           ...chaiData,
-//           price: 1,
-//         }
-//       }
-//     }
-//   }
-//   /**
-//    * @deprecated
-//    * seriously do not use this. use fetchJupiterPrice
-//    */
-//   getUSDTokenPrice(mintAddress: string): number {
-//     return mintAddress ? this._tokenPriceToUSDlist[mintAddress]?.price || 0 : 0
-//   }
-//   /**
-//    * For decimals use on chain tryGetMint
-//    */
-//   getTokenInfo(mintAddress: string): TokenInfoWithoutDecimals | undefined {
-//     const tokenListRecord = this._tokenList?.find(
-//       (x) => x.address === mintAddress
-//     )
-//     return tokenListRecord
-//   }
-//   /**
-//    * For decimals use on chain tryGetMint
-//    */
-//   getTokenInfoFromCoingeckoId(
-//     coingeckoId: string
-//   ): TokenInfoWithoutDecimals | undefined {
-//     const tokenListRecord = this._tokenList?.find(
-//       (x) => x.extensions?.coingeckoId === coingeckoId
-//     )
-//     return tokenListRecord
-//   }
-// }
+      if (chaiData?.price && (chaiData.price > 1.3 || chaiData.price < 0.9)) {
+        this._tokenPriceToUSDlist[chaiMint] = {
+          ...chaiData,
+          price: 1,
+        }
+      }
+    }
+  }
+  /**
+   * @deprecated
+   * seriously do not use this. use fetchJupiterPrice
+   */
+  getUSDTokenPrice(mintAddress: string): number {
+    return mintAddress ? this._tokenPriceToUSDlist[mintAddress]?.price || 0 : 0
+  }
+  /**
+   * For decimals use on chain tryGetMint
+   */
+  getTokenInfo(mintAddress: string): TokenInfoWithoutDecimals | undefined {
+    const tokenListRecord = this._tokenList?.find(
+      (x) => x.address === mintAddress
+    )
+    return tokenListRecord
+  }
+  /**
+   * For decimals use on chain tryGetMint
+   */
+  getTokenInfoFromCoingeckoId(
+    coingeckoId: string
+  ): TokenInfoWithoutDecimals | undefined {
+    const tokenListRecord = this._tokenList?.find(
+      (x) => x.extensions?.coingeckoId === coingeckoId
+    )
+    return tokenListRecord
+  }
+}
 
-// const tokenPriceService = new TokenPriceService()
+const tokenPriceService = new TokenPriceService()
 
-// export default tokenPriceService
+export default tokenPriceService
